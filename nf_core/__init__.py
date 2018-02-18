c3056d67ef67d4f97789d756e291b4c223e27154
sven <sven.fillinger@mailbox.org>
2018-02-18 10:34:40 +0100

    Revert "Handle version exception for unittest import"
    This reverts commit 3bcff4645d75db8dd98cf28a036d699350da62e1.

--- nf_core/__init__.py
+++ nf_core/__init__.py
@@ -6,8 +6,4 @@ Shouldn't do much, as everything is under subcommands.
 
 import pkg_resources
 
-try:
-    __version__ = pkg_resources.get_distribution("nf_core").version
-except:
-    # Needed for loading from unittest
-    __version__ = "unknown version"
+__version__ = pkg_resources.get_distribution("nf_core").version
