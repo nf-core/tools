b94951635d797a5385116365d353f2312d081c27
Andreas WILM <wilma@gis.a-star.edu.sg>
2018-02-28 21:10:02 +0800

    Addressing code review comments round 1

--- nf_core/lint.py
+++ nf_core/lint.py
@@ -108,7 +108,7 @@ class PipelineLint(object):
         """minimal tests only"""
         logging.debug('Checking Dockerfile')
         fn = os.path.join(self.path, "Dockerfile")
-        content = open(fn).read()
+        with open(fn, 'r') as fh: content = fh.read()
 
         # Implicitely also checks if empty.
         if 'FROM ' in content:
@@ -123,7 +123,7 @@ class PipelineLint(object):
         for l in ['LICENSE', 'LICENSE.md', 'LICENCE', 'LICENCE.md']:
             fn = os.path.join(self.path, l)
             if os.path.isfile(fn):
-                content = open(fn).read()
+                with open(fn, 'r') as fh: content = fh.read()
 
                 # needs at least copyright, permission, notice and "as-is" lines
                 nl = content.count("\n")
