c705def1857429d9a945e04aec1cf001885dccf0
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-03-22 15:48:43 +0100

    Adjust tests

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -34,7 +34,7 @@ PATHS_WRONG_LICENSE_EXAMPLE = [pf(WD, 'lint_examples/wrong_license_example'),
     pf(WD, 'lint_examples/license_incomplete_example')]
 
 # The maximum sum of passed tests currently possible
-MAX_PASS_CHECKS = 36
+MAX_PASS_CHECKS = 37
 # The additional tests passed for releases
 ADD_PASS_RELEASE = 1
 
@@ -83,7 +83,7 @@ class TestLint(unittest.TestCase):
         """Tests for missing files like Dockerfile or LICENSE"""
         lint_obj = nf_core.lint.PipelineLint(PATH_FAILING_EXAMPLE)
         lint_obj.check_files_exist()
-        expectations = {"failed": 4, "warned": 2, "passed": len(listfiles(PATH_WORKING_EXAMPLE)) - 4 - 1}
+        expectations = {"failed": 4, "warned": 2, "passed": len(listfiles(PATH_WORKING_EXAMPLE)) - 4 - 2}
         self.assess_lint_status(lint_obj, **expectations)
 
     def test_mit_licence_example_pass(self):
@@ -104,14 +104,14 @@ class TestLint(unittest.TestCase):
         """Tests that config variable existence test works with good pipeline example"""
         good_lint_obj = nf_core.lint.PipelineLint(PATH_WORKING_EXAMPLE)
         good_lint_obj.check_config_vars()
-        expectations = {"failed": 0, "warned": 0, "passed": 18}
+        expectations = {"failed": 0, "warned": 0, "passed": 19}
         self.assess_lint_status(good_lint_obj, **expectations)
 
     def test_config_variable_example_with_failed(self):
         """Tests that config variable existence test works with bad pipeline example"""
         bad_lint_obj = nf_core.lint.PipelineLint(PATH_FAILING_EXAMPLE)
         bad_lint_obj.check_config_vars()
-        expectations = {"failed": 11, "warned": 7, "passed": 0}
+        expectations = {"failed": 11, "warned": 8, "passed": 0}
         self.assess_lint_status(bad_lint_obj, **expectations)
 
     def test_ci_conf_pass(self):
