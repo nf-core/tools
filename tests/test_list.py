8cd1e07656924d3caf0f8f9424807d5125e2fa35
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-04-20 22:12:12 +0200

    Increase code coverage

--- tests/test_list.py
+++ tests/test_list.py
@@ -2,9 +2,11 @@
 """ Tests covering the workflow listing code.
 """
 import os
+import time
 import pytest
 import unittest
 import nf_core.list
+from datetime import datetime
 
 def test_working_listcall():
     """ Test that listing pipelines works """
@@ -13,3 +15,10 @@ def test_working_listcall():
 def test_working_listcall_json():
     """ Test that listing pipelines with JSON works """
     nf_core.list.list_workflows(True)
+
+def test_pretty_datetime():
+    """ Test that the pretty datetime function works """
+    now = datetime.now()
+    nf_core.list.pretty_date(now)
+    now_ts = time.mktime(now.timetuple())
+    nf_core.list.pretty_date(now_ts)
