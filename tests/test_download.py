fd62355b8c38a16838ed4a5b3ad665559c93d1a4
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-06-09 14:58:59 +0200

    Test devel version case and unknown release

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,21 +6,45 @@ import nf_core.list
 from nf_core.download import DownloadWorkflow
 
 import mock
+import pytest
 import unittest
 
 class DownloadTest(unittest.TestCase):
 
     @mock.patch('nf_core.list.RemoteWorkflow')
     @mock.patch('nf_core.list.Workflows')
-    def test_fetch_workflow_details_of_release(self, mock_workflows, mock_workflow):
+    def test_fetch_workflow_details_for_release(self, mock_workflows, mock_workflow):
         download_obj = DownloadWorkflow(
             pipeline = "dummy",
-            release="1.0.0")
+            release="1.0.0"
+            )
         mock_workflow.name = "dummy"
         mock_workflow.releases = [{"tag_name": "1.0.0", "tag_sha": "n3v3rl4nd"}]
         mock_workflows.remote_workflows = [mock_workflow]
         
         download_obj.fetch_workflow_details(mock_workflows)
+    
+    @mock.patch('nf_core.list.RemoteWorkflow')
+    @mock.patch('nf_core.list.Workflows')
+    def test_fetch_workflow_details_for_dev_version(self, mock_workflows, mock_workflow):
+        download_obj = DownloadWorkflow(pipeline = "dummy")
+        mock_workflow.name = "dummy"
+        mock_workflow.releases = [{"tag_name": "1.0.0", "tag_sha": "n3v3rl4nd"}]
+        mock_workflows.remote_workflows = [mock_workflow]
         
+        download_obj.fetch_workflow_details(mock_workflows)
 
+    @mock.patch('nf_core.list.RemoteWorkflow')
+    @mock.patch('nf_core.list.Workflows')
+    @pytest.mark.xfail(raises=LookupError)
+    def test_fetch_workflow_details_for_unknown_release(self, mock_workflows, mock_workflow):
+        download_obj = DownloadWorkflow(
+            pipeline = "dummy",
+            release = "1.2.0"
+            )
+        mock_workflow.name = "dummy"
+        mock_workflow.releases = [{"tag_name": "1.0.0", "tag_sha": "n3v3rl4nd"}]
+        mock_workflows.remote_workflows = [mock_workflow]
+        
+        download_obj.fetch_workflow_details(mock_workflows)
 
