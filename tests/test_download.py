07b681fea98288d4f50946ad9454e006ea3be083
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-06-09 15:18:02 +0200

    Test for autoset of release tag

--- tests/test_download.py
+++ tests/test_download.py
@@ -36,6 +36,17 @@ class DownloadTest(unittest.TestCase):
 
     @mock.patch('nf_core.list.RemoteWorkflow')
     @mock.patch('nf_core.list.Workflows')
+    def test_fetch_workflow_details_and_autoset_release(self, mock_workflows, mock_workflow):
+        download_obj = DownloadWorkflow(pipeline = "dummy")
+        mock_workflow.name = "dummy"
+        mock_workflow.releases = [{"tag_name": "1.0.0", "tag_sha": "n3v3rl4nd"}]
+        mock_workflows.remote_workflows = [mock_workflow]
+        
+        download_obj.fetch_workflow_details(mock_workflows)
+        assert download_obj.release == "1.0.0"
+
+    @mock.patch('nf_core.list.RemoteWorkflow')
+    @mock.patch('nf_core.list.Workflows')
     @pytest.mark.xfail(raises=LookupError)
     def test_fetch_workflow_details_for_unknown_release(self, mock_workflows, mock_workflow):
         download_obj = DownloadWorkflow(
