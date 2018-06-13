854c56165dff547410af0e2b6da8a3ddce21d983
Phil Ewels <phil.ewels@scilifelab.se>
2018-06-13 11:04:13 +0200

    Tidying up licences code
    * Normalise licence text
    * Print results sorted by licence, then name
    * Separate package name and version in output
    * Add JSON output

--- nf_core/licences.py
+++ nf_core/licences.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import logging
 import json
+import re
 import requests
 import sys
 import tabulate
@@ -15,10 +16,10 @@ import nf_core.lint
 class WorkflowLicences():
     """ Class to hold all licence info """
 
-    def __init__(self, pipeline):
+    def __init__(self, pipeline, json=False):
         """ Set class variables """
         self.pipeline = pipeline
-        self.pipeline_licence = 'MIT'
+        self.json = json
         self.conda_package_licences = {}
 
     def fetch_conda_licences(self):
@@ -29,36 +30,67 @@ class WorkflowLicences():
         lint_obj.conda_config = yaml.load(response.text)
         # Check conda dependency list
         for dep in lint_obj.conda_config.get('dependencies', []):
-            if isinstance(dep, str):
-                try:
+            try:
+                if isinstance(dep, str):
                     lint_obj.check_anaconda_package(dep)
-                except ValueError:
-                    print("Couldn't get {}".format(dep))
-            elif isinstance(dep, dict):
-                try:
+                elif isinstance(dep, dict):
                     lint_obj.check_pip_package(dep)
-                except ValueError:
-                    print("Couldn't get {}".format(dep))
+            except ValueError:
+                logging.error("Couldn't get licence information for {}".format(dep))
 
         for dep, data in lint_obj.conda_package_info.items():
             try:
-                self.conda_package_licences[dep] = data['license']
-                if not isinstance(data['license'], basestring):
-                    licences = set()
-                    for f in data['files']:
+                depname, depver = dep.split('=', 1)
+                licences = set()
+                # Licence for each version
+                for f in data['files']:
+                    if not depver or depver == f.get('version'):
                         try:
                             licences.add(f['attrs']['license'])
                         except KeyError:
                             pass
-                    self.conda_package_licences[dep] = ', '.join(list(licences))
+                # Main licence field
+                if len(list(licences)) == 0 and isinstance(data['license'], basestring):
+                    licences.add(data['license'])
+                self.conda_package_licences[dep] = self.clean_licence_names(list(licences))
             except KeyError:
                 pass
 
+    def clean_licence_names(self, licences):
+        """ Normalise varying licence names """
+        clean_licences = []
+        for l in licences:
+            l = re.sub(r'GNU General Public License v\d \(([^\)]+)\)', r'\1', l)
+            l = re.sub(r'GNU GENERAL PUBLIC LICENSE', 'GPL', l, flags=re.IGNORECASE)
+            l = l.replace('GPL-', 'GPLv')
+            l = re.sub(r'GPL(\d)', r'GPLv\1', l)
+            l = re.sub(r'GPL \(([^\)]+)\)', r'GPL \1', l)
+            l = re.sub(r'GPL\s*v', 'GPLv', l)
+            l = re.sub(r'\s*(>=?)\s*(\d)', r' \1\2', l)
+            clean_licences.append(l)
+        return clean_licences
+
     def print_licences(self):
         """ Print the fetched information """
+
         logging.info("""Warning: This tool only prints licence information for the software tools packaged using conda.
         The pipeline may use other software and dependencies not described here. """)
-        # Print summary table
-        print("", file=sys.stderr)
-        print(tabulate.tabulate([[d, l] for d, l in self.conda_package_licences.items()], headers=['Package Name', 'Licence']))
-        print("", file=sys.stderr)
+
+        if self.json:
+            print(json.dumps(self.conda_package_licences, indent=4))
+
+        else:
+            licence_list = []
+            for dep, licences in self.conda_package_licences.items():
+                depname, depver = dep.split('=', 1)
+                try:
+                    depname = depname.split('::')[1]
+                except IndexError:
+                    pass
+                licence_list.append([depname, depver, ', '.join(licences)])
+            # Sort by licence, then package name
+            licence_list = sorted(sorted(licence_list), key=lambda x: x[2])
+            # Print summary table
+            print("", file=sys.stderr)
+            print(tabulate.tabulate(licence_list, headers=['Package Name', 'Version', 'Licence']))
+            print("", file=sys.stderr)
