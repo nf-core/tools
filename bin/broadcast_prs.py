d495cc071a1cca1e5a160480169d277b43289721
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-08-09 13:59:57 +0200

    Refactor a lot of stuff

--- bin/broadcast_prs.py
+++ bin/broadcast_prs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 from cookiecutter.main import cookiecutter
+import git
 import json
 import os
 import requests
 from requests.auth import HTTPBasicAuth
 import sys
 import subprocess
+import tempfile
+import utils
+
+#run_cookiecutter(self.pipeline_name, self.pipeline_description, self.pipeline_version)
 
 # The GitHub base url or the nf-core project
 GH_BASE_URL = "https://github.com/nf-core"
 # The current cookiecutter template url for nf-core pipelines
-NF_CORE_TEMPLATE = "https://github.com/nf-core/cookiecutter.git"
+NF_CORE_TEMPLATE = ""
 # The JSON file is updated on every push event on the nf-core GitHub project
 NF_CORE_PIPELINE_INFO = "http://nf-co.re/pipelines.json"
 # The API endpoint for creating pull requests
@@ -27,43 +32,57 @@ class UpdateTemplate:
           - branch: The template branch name, default=`TEMPLATE`
     """
 
-    def __init__(self, pipeline, context, branch='TEMPLATE'):
+    def __init__(self, pipeline, branch='TEMPLATE'):
         """Basic constructor
         """
         self.pipeline = pipeline
         self.repo_url = "{base_url}/{pipeline}".format(
                     base_url=GH_BASE_URL,
                     pipeline=pipeline)
-        self.context = context
         self.branch = branch
+        self.tmpdir = tempfile.mkdtemp()
+        self.repo = None
     
     def run(self):
         """Execute the template update.
         """
-        self._clone_repo()
-        self._apply_changes()
+        config = self._clone_repo()
+        assert self.repo
+        context = utils.create_context(config)
+        self._apply_changes(context)
         self._commit_changes()
+        self._push_changes()
+
     
     def _clone_repo(self):
         """Clone the repo and switch to the configured branch.
         """
-        subprocess.run(["git", "clone", self.repo_url, "-b", self.branch, self.pipeline])
+        self.repo = git.Repo.clone_from(self.repo_url, self.tmpdir)
+        config = utils.fetch_wf_config(wf_path=self.tmpdir)
+        print(self.branch)
+        self.repo.git.checkout("origin/{branch}".format(branch=self.branch),
+            b="{branch}".format(branch=self.branch))
+        return config
 
-    def _apply_changes(self):
+    def _apply_changes(self, context):
         """Apply the changes of the cookiecutter template
         to the pipelines template branch.
         """
         cookiecutter(NF_CORE_TEMPLATE,
                      no_input=True,
-                     extra_context=None,
+                     extra_context=context,
                      overwrite_if_exists=True,
-                     output_dir=self.pipeline)
+                     output_dir=self.tmpdir)
     
     def _commit_changes(self):
         """Commits the changes of the new template to the current branch.
         """
-        subprocess.run(["git", "add", "-A", "."], cwd=self.pipeline)
-        subprocess.run(["git", "commit", "-m", "Update nf-core template"], cwd=self.pipeline)
+        self.repo.git.add(A=True)
+        self.repo.index.commit("Update nf-core pipeline template.")
+
+    def _push_changes(self):
+        print("Push it...")
+        #self.repo.git.push() # we need a different way to push otherwise it will ask for credentials
         
 
 def create_pullrequest(pipeline, origin="master", template="TEMPLATE", token="", user="nf-core"):
@@ -74,39 +93,31 @@ def create_pullrequest(pipeline, origin="master", template="TEMPLATE", token="",
     """
     content = {}
     content['title'] = "Important pipeline nf-core update!"
-    content['body'] = "Some important changes have been made in the nf-core pipelines templates.\n" +
+    content['body'] = "Some important changes have been made in the nf-core pipelines templates.\n" \
     "Please make sure to merge this in ASAP and make a new minor release of your pipeline."
-    content['head'] = "{}:{}".format(pipeline, template)
-    content['base'] = master
+    content['head'] = "{}".format(template)
+    content['base'] = origin
     return requests.post(url=GITHUB_PR_URL_TEMPL.format(pipeline=pipeline),
-                         data=json.dumps(content)
+                         data=json.dumps(content),
                          auth=HTTPBasicAuth(user, token))
-
-def get_context(pipeline):
-    """Get the template context for a given pipeline.
-    
-    Returns: A context dictionary
-    """
-    pass
 
 def main():
     res = requests.get(NF_CORE_PIPELINE_INFO)
     pipelines = json.loads(res.content).get('remote_workflows')
     if not pipelines:
         print("Pipeline information was empty!")
+    pipelines = [{"name":"hlatyping"}] # just for testing
     for pipeline in pipelines:
-        # Get context from pipeline and load it into a dictionary
-        # context = load_context(pipeline)
-        print(pipeline['name']) # Just for testing, can be safely deleted
-        ut.UpdateTemplate(pipeline['name'], context)
+        UpdateTemplate(pipeline['name']).run()
     
     for pipeline in pipelines:
         print("Trying to open pull request for pipeline {}...".format(pipeline['name']))
-        response = create_pullrequest(pipeline['name'])
+        response = create_pullrequest(pipeline['name'], token="117962f70c156268d02a8b8f42be04bf7676141e")
         if response.status_code != 201:
             print("Pull-request for pipeline \'{pipeline}\' failed," 
             " got return code {return_code}."
-            .format(pipeline=pipeline, return_code=response.status_code))
+            .format(pipeline=pipeline["name"], return_code=response.status_code))
+            print(response.content)
         else:
             print("Created pull-request for pipeline \'{pipeline}\' successfully.".format(pipeline=pipeline))
             
