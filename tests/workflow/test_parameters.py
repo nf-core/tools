0d09ce9214b51e8e39520487999ced793ad6f7d7
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-01-08 16:35:12 +0100

    Tests params dump

--- tests/workflow/test_parameters.py
+++ tests/workflow/test_parameters.py
@@ -11,9 +11,28 @@ import nf_core.workflow.parameters as pms
 WD = os.path.dirname(__file__)
 PATH_WORKING_EXAMPLE = os.path.join(WD, 'example.json')
 
-def test_creating_params_from_json():
-    """Tests parsing of a parameter json."""
+@pytest.fixture(scope="class")
+def example_json():
     assert os.path.isfile(PATH_WORKING_EXAMPLE)
     with open(PATH_WORKING_EXAMPLE) as fp:
-        result = pms.Parameters.create_from_json(fp.read())
+        content = fp.read()
+    return content
+
+def test_creating_params_from_json(example_json):
+    """Tests parsing of a parameter json."""
+    result = pms.Parameters.create_from_json(example_json)
     assert len(result) == 2
+
+def test_params_as_json_dump(example_json):
+    """Tests the JSON dump that can be consumed by Nextflow."""
+    result = pms.Parameters.create_from_json(example_json)
+    parameter = result[0]
+    assert parameter.name == "reads"
+    expected_output = """
+    {
+        "reads": "path/to/reads.fastq.gz"
+    }"""
+    parsed_output = json.loads(expected_output)
+    assert len(parsed_output.keys()) == 1
+    assert parameter.name in parsed_output.keys()
+    assert parameter.default_value == parsed_output[parameter.name]
