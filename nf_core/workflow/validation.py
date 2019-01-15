41ed743eec4dba35e131a027c68e7a74d9a761b1
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-01-15 15:25:14 +0100

    Adds docstrings

--- nf_core/workflow/validation.py
+++ nf_core/workflow/validation.py
@@ -2,14 +2,23 @@ import abc
 from nf_core.workflow.parameters import Parameter
 
 class Validators(object):
-    """
+    """Gives access to a factory method for objects of instance 
+    :class:`Validator` which returns the correct Validator for a 
+    given parameter type.
     """
     def __init__(self):
         pass
     
     @staticmethod
     def get_validator_for_param(parameter):
-        """Returns a validator object for a given parameter.
+        """Determines matching :class:`Validator` instance for a given parameter.
+
+        Returns:
+            Validator: Matching validator for a given :class:`Parameter`.
+        
+        Raises:
+            LookupError: In case no matching validator for a given parameter type
+                can be determined.
         """
         if parameter.type == "integer":
             return IntegerValidator(parameter)
@@ -35,12 +44,26 @@ class Validator(abc.ABC):
 
 
 class IntegerValidator(Validator):
-    """Implementation for parameters of type integer."""
+    """Implementation for parameters of type integer.
+    
+    Args:
+        parameter (:class:`Parameter`): A Parameter object.
+    
+    Raises:
+        AttributeError: In case the argument is not of instance :class:`Parameter`.
 
-    def __init__(self, params):
-        super(IntegerValidator, self).__init__(params)
+    """
+
+    def __init__(self, parameter):
+        super(IntegerValidator, self).__init__(parameter)
 
     def validate(self):
+        """Validates an parameter integer value against a given range (choices).
+        If the value is valid, no error is risen.
+
+        Raises:
+            AtrributeError: Description of the value error.
+        """
         value = int(self._param.value)
         choices = sorted([int(x) for x in self._param.choices])
         if not choices:
@@ -49,7 +72,7 @@ class IntegerValidator(Validator):
             raise AttributeError("The property 'choices' must have at least two entries.")
         if not value >= choices[0] and value <= choices[-1]:
             raise AttributeError("The value for parameter '{}' must be within range of [{},{}]"
-                .format(self.__param.name, choices[0], choices[-1]))
+                .format(self._param.name, choices[0], choices[-1]))
 
 
 
