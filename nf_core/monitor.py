55800358b05374a0905b78a16dcc180d2d9780d1
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-12 18:23:38 +0200

    Add a temporary database to monitor

--- nf_core/monitor.py
+++ nf_core/monitor.py
@@ -5,11 +5,14 @@ Starts an OpenAPI server which handles weblog messages from nextflow.
 """
 
 import os
+import tempfile
+import pickle
 import logging
 
 import connexion
+from flask import g
 
-from nf_core.openapi_server import encoder
+from nf_core.openapi_server import encoder, db
 
 def start_monitor(port=8080):
     """Starts the OpenAPI server with specified port """
@@ -20,4 +23,15 @@ def start_monitor(port=8080):
     app.add_api('openapi.yaml',
                 arguments={'title': 'nextflow monitor'},
                 pythonic_params=True)
+
+    with app.app.app_context(): # create a default temporary database
+        db.get_db()
+
+    @app.app.teardown_appcontext
+    def close_db(e=None): # tears down g.db from application context
+        database = g.pop('db', None)
+
+        if database is not None:
+            pickle.dump(database, open(os.path.join(tempfile.gettempdir(), 'nf_core.db'), 'wb'))
+    
     app.run(port=port)
--- nf_core/openapi_server/db.py
+++ nf_core/openapi_server/db.py
@@ -1,11 +1,20 @@
+import os
+import pickle
+import tempfile
+
 from flask import g
 
 def get_db():
     """ Returns a database for the current session """
 
     if 'db' not in g:
-        g.db = {}
-        g.db['events'] = []
-        g.db['runs'] = []
+        temporaryDatabase = os.path.join(tempfile.gettempdir(), 'nf_core.db')
+        # A temporary database under the /var directory
+        if os.path.isfile(temporaryDatabase):
+            g.db = pickle.load(open(temporaryDatabase, 'rb'))
+        else:
+            g.db = {}
+            g.db['events'] = []
+            g.db['runs'] = []
 
     return g.db
