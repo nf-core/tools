0dfe5f971ea7c8607c11721fb9498ca68fcbea4d
Alexander Peltzer <alex.peltzer@gmail.com>
2019-09-18 14:31:45 +0200

    Fix parameters schema URL

--- tests/workflow/test_parameters.py
+++ tests/workflow/test_parameters.py
@@ -13,7 +13,7 @@ from nf_core.workflow import parameters as pms
 
 WD = os.path.dirname(__file__)
 PATH_WORKING_EXAMPLE = os.path.join(WD, 'example.json')
-SCHEMA_URI = "https://nf-co.re/parameters.schema.json"
+SCHEMA_URI = "https://nf-co.re/params/0.2.0dev/parameters.schema.json"
 
 @pytest.fixture(scope="class")
 def schema():
