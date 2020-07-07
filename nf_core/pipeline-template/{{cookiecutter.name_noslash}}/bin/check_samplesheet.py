06e665b3922f7f2833bfe4551b5427f8ec9d2b99
drpatelh <drhpatel@gmail.com>
2020-07-07 15:52:47 +0100

    Change variable case

--- nf_core/pipeline-template/{{cookiecutter.name_noslash}}/bin/check_samplesheet.py
+++ nf_core/pipeline-template/{{cookiecutter.name_noslash}}/bin/check_samplesheet.py
@@ -7,7 +7,7 @@ import argparse
 
 def parse_args(args=None):
     Description = 'Reformat {{ cookiecutter.name }} samplesheet file and check its contents.'
-    Epilog = """Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"""
+    Epilog = 'Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>'
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument('FILE_IN', help="Input samplesheet file.")
@@ -29,16 +29,16 @@ def print_error(error,line):
     sys.exit(1)
 
 
-def check_samplesheet(FileIn,FileOut):
+def check_samplesheet(file_in,file_out):
     ## Check header
     HEADER = ['sample', 'fastq_1', 'fastq_2']
-    fin = open(FileIn,'r')
+    fin = open(file_in,'r')
     header = fin.readline().strip().split(',')
     if header != HEADER:
         print("ERROR: Please check samplesheet header -> {} != {}".format(','.join(header),','.join(HEADER)))
         sys.exit(1)
 
-    sampleRunDict = {}
+    sample_run_dict = {}
     while True:
         line = fin.readline()
         if line:
@@ -48,12 +48,12 @@ def check_samplesheet(FileIn,FileOut):
             if len(lspl) != len(header):
                 print_error("Invalid number of columns (minimum = {})!".format(len(header)),line)
 
-            numCols = len([x for x in lspl if x])
-            if numCols < 2:
+            num_cols = len([x for x in lspl if x])
+            if num_cols < 2:
                 print_error("Invalid number of populated columns (minimum = 2)!".format(line))
 
             ## Check sample name entries
-            sample,fastQFiles = lspl[0],lspl[1:]
+            sample,fastq_files = lspl[0],lspl[1:]
             if sample:
                 if sample.find(' ') != -1:
                     print_error("Sample entry contains spaces!",line)
@@ -61,7 +61,7 @@ def check_samplesheet(FileIn,FileOut):
                 print_error("Sample entry has not been specified!",line)
 
             ## Check FastQ file extension
-            for fastq in fastQFiles:
+            for fastq in fastq_files:
                 if fastq:
                     if fastq.find(' ') != -1:
                         print_error("FastQ file contains spaces!",line)
@@ -70,7 +70,7 @@ def check_samplesheet(FileIn,FileOut):
 
             ## Auto-detect paired-end/single-end
             sample_info = []                             ## [single_end, fastq_1, fastq_2]
-            fastq_1,fastq_2 = fastQFiles
+            fastq_1,fastq_2 = fastq_files
             if sample and fastq_1 and fastq_2:           ## Paired-end short reads
                 sample_info = ['0', fastq_1, fastq_2]
             elif sample and fastq_1 and not fastq_2:     ## Single-end short reads
@@ -78,30 +78,30 @@ def check_samplesheet(FileIn,FileOut):
             else:
                 print_error("Invalid combination of columns provided!",line)
 
-            if sample not in sampleRunDict:
-                sampleRunDict[sample] = [sample_info]
+            if sample not in sample_run_dict:
+                sample_run_dict[sample] = [sample_info]
             else:
-                if sample_info in sampleRunDict[sample]:
+                if sample_info in sample_run_dict[sample]:
                     print_error("Samplesheet contains duplicate rows!",line)
                 else:
-                    sampleRunDict[sample].append(sample_info)
+                    sample_run_dict[sample].append(sample_info)
         else:
             fin.close()
             break
 
     ## Write validated samplesheet with appropriate columns
-    if len(sampleRunDict) > 0:
-        OutDir = os.path.dirname(FileOut)
-        make_dir(OutDir)
-        fout = open(FileOut,'w')
+    if len(sample_run_dict) > 0:
+        out_dir = os.path.dirname(file_out)
+        make_dir(out_dir)
+        fout = open(file_out,'w')
         fout.write(','.join(['sample', 'single_end', 'fastq_1', 'fastq_2']) + '\n')
-        for sample in sorted(sampleRunDict.keys()):
+        for sample in sorted(sample_run_dict.keys()):
 
             ## Check that multiple runs of the same sample are of the same datatype
-            if not all(x[0] == sampleRunDict[sample][0][0] for x in sampleRunDict[sample]):
+            if not all(x[0] == sample_run_dict[sample][0][0] for x in sample_run_dict[sample]):
                 print_error("Multiple runs of a sample must be of the same datatype","Sample: {}".format(sample))
 
-            for idx,val in enumerate(sampleRunDict[sample]):
+            for idx,val in enumerate(sample_run_dict[sample]):
                 fout.write(','.join(["{}_T{}".format(sample,idx+1)] + val) + '\n')
         fout.close()
 
