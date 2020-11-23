316df0152598fc8c321e6b18579946ae669ca7cd
drpatelh <drhpatel@gmail.com>
2020-11-23 17:23:19 +0000

    Update software scraping

--- nf_core/pipeline-template/{{cookiecutter.name_noslash}}/bin/scrape_software_versions.py
+++ nf_core/pipeline-template/{{cookiecutter.name_noslash}}/bin/scrape_software_versions.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from collections import OrderedDict
-import re
+import os
 
-# TODO nf-core: Add additional regexes for new tools in process get_software_versions
-regexes = {
-    "{{ cookiecutter.name }}": ["v_pipeline.txt", r"(\S+)"],
-    "Nextflow": ["v_nextflow.txt", r"(\S+)"],
-    "FastQC": ["v_fastqc.txt", r"FastQC v(\S+)"],
-    "MultiQC": ["v_multiqc.txt", r"multiqc, version (\S+)"],
-}
-results = OrderedDict()
-results["{{ cookiecutter.name }}"] = '<span style="color:#999999;">N/A</span>'
-results["Nextflow"] = '<span style="color:#999999;">N/A</span>'
-results["FastQC"] = '<span style="color:#999999;">N/A</span>'
-results["MultiQC"] = '<span style="color:#999999;">N/A</span>'
+results = {}
+version_files = [x for x in os.listdir('.') if x.endswith('.version.txt')]
+for version_file in version_files:
 
-# Search each file using its regex
-for k, v in regexes.items():
-    try:
-        with open(v[0]) as x:
-            versions = x.read()
-            match = re.search(v[1], versions)
-            if match:
-                results[k] = "v{}".format(match.group(1))
-    except IOError:
-        results[k] = False
+    software = version_file.replace('.version.txt','')
+    if software == 'pipeline':
+        software = '{{ cookiecutter.name }}'
 
-# Remove software set to false in results
-for k in list(results):
-    if not results[k]:
-        del results[k]
+    with open(version_file) as fin:
+        version = fin.read().strip()
+    results[software] = version
 
 # Dump to YAML
-print(
-    """
+print ('''
 id: 'software_versions'
 section_name: '{{ cookiecutter.name }} Software Versions'
 section_href: 'https://github.com/{{ cookiecutter.name }}'
@@ -42,13 +23,12 @@ plot_type: 'html'
 description: 'are collected at run time from the software output.'
 data: |
     <dl class="dl-horizontal">
-"""
-)
-for k, v in results.items():
-    print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k, v))
-print("    </dl>")
+''')
+for k,v in sorted(results.items()):
+    print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k,v))
+print ("    </dl>")
 
 # Write out regexes as csv file:
-with open("software_versions.csv", "w") as f:
-    for k, v in results.items():
-        f.write("{}\t{}\n".format(k, v))
+with open('software_versions.csv', 'w') as f:
+    for k,v in sorted(results.items()):
+        f.write("{}\t{}\n".format(k,v))
