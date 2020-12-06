cc43e6ad54b918d381f2f80ccd3a0f1e84cd7734
Phil Ewels <phil.ewels@scilifelab.se>
2020-12-07 00:54:30 +0100

    Added documentation to function docstrings for awstest lint tests

--- nf_core/lint/actions_awsfulltest.py
+++ nf_core/lint/actions_awsfulltest.py
@@ -7,7 +7,24 @@ import yaml
 def actions_awsfulltest(self):
     """Checks the GitHub Actions awsfulltest is valid.
 
-    Makes sure it is triggered only on ``release`` and workflow_dispatch.
+    In addition to small test datasets run on GitHub Actions, we provide the possibility of testing the pipeline on full size datasets on AWS.
+    This should ensure that the pipeline runs as expected on AWS and provide a resource estimation.
+
+    The GitHub Actions workflow is called ``awsfulltest.yml``, and it can be found in the ``.github/workflows/`` directory.
+
+    .. warning::  This workflow incurs AWS costs, therefore it should only be triggered for pipeline releases:
+                  ``workflow_run`` (after the docker hub release workflow) and ``workflow_dispatch``.
+
+    .. seealso::  You can manually trigger the AWS tests by going to the `Actions` tab on the pipeline GitHub repository and selecting the
+                  `nf-core AWS full size tests` workflow on the left.
+
+    .. seealso::  For tests on full data prior to release, `Nextflow Tower <https://tower.nf>`_ launch feature can be employed.
+
+    The ``.github/workflows/awsfulltest.yml`` file is tested for the following:
+
+    * Must be turned on ``workflow_dispatch``.
+    * Must be turned on for ``workflow_run`` with ``workflows: ["nf-core Docker push (release)"]`` and ``types: [completed]``
+    * Should run the profile ``test_full`` that should be edited to provide the links to full-size datasets. If it runs the profile ``test``, a warning is given.
     """
     passed = []
     warned = []
--- nf_core/lint/actions_awstest.py
+++ nf_core/lint/actions_awstest.py
@@ -7,7 +7,21 @@ import yaml
 def actions_awstest(self):
     """Checks the GitHub Actions awstest is valid.
 
-    Makes sure it is triggered only on ``push`` to ``master``.
+    In addition to small test datasets run on GitHub Actions, we provide the possibility of testing the pipeline on AWS.
+    This should ensure that the pipeline runs as expected on AWS (which often has its own unique edge cases).
+
+    .. warning:: Running tests on AWS incurs costs, so these tests are not triggered automatically.
+                 Instead, they use the ``workflow_dispatch`` trigger, which allows for manual triggering
+                 of the workflow when testing on AWS is desired.
+
+    .. seealso::  You can trigger the tests by going to the `Actions` tab on the pipeline GitHub repository
+                  and selecting the `nf-core AWS test` workflow on the left.
+
+    The ``.github/workflows/awstest.yml`` file is tested for the following:
+
+    * Must *not* be turned on for ``push`` or ``pull_request``.
+    * Must be turned on for ``workflow_dispatch``.
+
     """
     fn = os.path.join(self.wf_path, ".github", "workflows", "awstest.yml")
     if not os.path.isfile(fn):
