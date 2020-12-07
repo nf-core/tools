7fddb2585567f0dbf569219af4a412a9a6d154c3
Phil Ewels <phil.ewels@scilifelab.se>
2020-12-07 22:07:44 +0100

    actions_ci lint - return ignored message if file not found

--- nf_core/lint/actions_ci.py
+++ nf_core/lint/actions_ci.py
@@ -11,67 +11,68 @@ def actions_ci(self):
     Makes sure tests run with the required nextflow version.
     """
     passed = []
-    warned = []
     failed = []
     fn = os.path.join(self.wf_path, ".github", "workflows", "ci.yml")
-    if os.path.isfile(fn):
-        with open(fn, "r") as fh:
-            ciwf = yaml.safe_load(fh)
+    if not os.path.isfile(fn):
+        return {"ignored": ["'.github/workflows/ci.yml' not found"]}
 
-        # Check that the action is turned on for the correct events
+    with open(fn, "r") as fh:
+        ciwf = yaml.safe_load(fh)
+
+    # Check that the action is turned on for the correct events
+    try:
+        expected = {"push": {"branches": ["dev"]}, "pull_request": None, "release": {"types": ["published"]}}
+        # NB: YAML dict key 'on' is evaluated to a Python dict key True
+        assert ciwf[True] == expected
+    except (AssertionError, KeyError, TypeError):
+        failed.append("GitHub Actions CI is not triggered on expected events: `{}`".format(fn))
+    else:
+        passed.append("GitHub Actions CI is triggered on expected events: `{}`".format(fn))
+
+    # Check that we're pulling the right docker image and tagging it properly
+    if self.nf_config.get("process.container", ""):
+        docker_notag = re.sub(r":(?:[\.\d]+|dev)$", "", self.nf_config.get("process.container", "").strip("\"'"))
+        docker_withtag = self.nf_config.get("process.container", "").strip("\"'")
+
+        # docker build
+        docker_build_cmd = "docker build --no-cache . -t {}".format(docker_withtag)
         try:
-            expected = {"push": {"branches": ["dev"]}, "pull_request": None, "release": {"types": ["published"]}}
-            # NB: YAML dict key 'on' is evaluated to a Python dict key True
-            assert ciwf[True] == expected
+            steps = ciwf["jobs"]["test"]["steps"]
+            assert any([docker_build_cmd in step["run"] for step in steps if "run" in step.keys()])
         except (AssertionError, KeyError, TypeError):
-            failed.append("GitHub Actions CI is not triggered on expected events: `{}`".format(fn))
+            failed.append("CI is not building the correct docker image. Should be: `{}`".format(docker_build_cmd))
         else:
-            passed.append("GitHub Actions CI is triggered on expected events: `{}`".format(fn))
+            passed.append("CI is building the correct docker image: `{}`".format(docker_build_cmd))
 
-        # Check that we're pulling the right docker image and tagging it properly
-        if self.nf_config.get("process.container", ""):
-            docker_notag = re.sub(r":(?:[\.\d]+|dev)$", "", self.nf_config.get("process.container", "").strip("\"'"))
-            docker_withtag = self.nf_config.get("process.container", "").strip("\"'")
-
-            # docker build
-            docker_build_cmd = "docker build --no-cache . -t {}".format(docker_withtag)
-            try:
-                steps = ciwf["jobs"]["test"]["steps"]
-                assert any([docker_build_cmd in step["run"] for step in steps if "run" in step.keys()])
-            except (AssertionError, KeyError, TypeError):
-                failed.append("CI is not building the correct docker image. Should be: `{}`".format(docker_build_cmd))
-            else:
-                passed.append("CI is building the correct docker image: `{}`".format(docker_build_cmd))
-
-            # docker pull
-            docker_pull_cmd = "docker pull {}:dev".format(docker_notag)
-            try:
-                steps = ciwf["jobs"]["test"]["steps"]
-                assert any([docker_pull_cmd in step["run"] for step in steps if "run" in step.keys()])
-            except (AssertionError, KeyError, TypeError):
-                failed.append("CI is not pulling the correct docker image. Should be: `{}`".format(docker_pull_cmd))
-            else:
-                passed.append("CI is pulling the correct docker image: {}".format(docker_pull_cmd))
-
-            # docker tag
-            docker_tag_cmd = "docker tag {}:dev {}".format(docker_notag, docker_withtag)
-            try:
-                steps = ciwf["jobs"]["test"]["steps"]
-                assert any([docker_tag_cmd in step["run"] for step in steps if "run" in step.keys()])
-            except (AssertionError, KeyError, TypeError):
-                failed.append("CI is not tagging docker image correctly. Should be: `{}`".format(docker_tag_cmd))
-            else:
-                passed.append("CI is tagging docker image correctly: {}".format(docker_tag_cmd))
-
-        # Check that we are testing the minimum nextflow version
+        # docker pull
+        docker_pull_cmd = "docker pull {}:dev".format(docker_notag)
         try:
-            matrix = ciwf["jobs"]["test"]["strategy"]["matrix"]["nxf_ver"]
-            assert any([self.minNextflowVersion in matrix])
-        except (KeyError, TypeError):
-            failed.append("Continuous integration does not check minimum NF version: `{}`".format(fn))
-        except AssertionError:
-            failed.append("Minimum NF version different in CI and pipelines manifest: `{}`".format(fn))
+            steps = ciwf["jobs"]["test"]["steps"]
+            assert any([docker_pull_cmd in step["run"] for step in steps if "run" in step.keys()])
+        except (AssertionError, KeyError, TypeError):
+            failed.append("CI is not pulling the correct docker image. Should be: `{}`".format(docker_pull_cmd))
         else:
-            passed.append("Continuous integration checks minimum NF version: `{}`".format(fn))
+            passed.append("CI is pulling the correct docker image: {}".format(docker_pull_cmd))
 
-    return {"passed": passed, "warned": warned, "failed": failed}
+        # docker tag
+        docker_tag_cmd = "docker tag {}:dev {}".format(docker_notag, docker_withtag)
+        try:
+            steps = ciwf["jobs"]["test"]["steps"]
+            assert any([docker_tag_cmd in step["run"] for step in steps if "run" in step.keys()])
+        except (AssertionError, KeyError, TypeError):
+            failed.append("CI is not tagging docker image correctly. Should be: `{}`".format(docker_tag_cmd))
+        else:
+            passed.append("CI is tagging docker image correctly: {}".format(docker_tag_cmd))
+
+    # Check that we are testing the minimum nextflow version
+    try:
+        matrix = ciwf["jobs"]["test"]["strategy"]["matrix"]["nxf_ver"]
+        assert any([self.minNextflowVersion in matrix])
+    except (KeyError, TypeError):
+        failed.append("Continuous integration does not check minimum NF version: `{}`".format(fn))
+    except AssertionError:
+        failed.append("Minimum NF version different in CI and pipelines manifest: `{}`".format(fn))
+    else:
+        passed.append("Continuous integration checks minimum NF version: `{}`".format(fn))
+
+    return {"passed": passed, "failed": failed}
