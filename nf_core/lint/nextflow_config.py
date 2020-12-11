1a8b4b446f92f50fddfca957089a173a0c0e8ebe
Phil Ewels <phil.ewels@scilifelab.se>
2020-12-11 02:03:09 +0100

    Errors to docstring - nextflow config

--- nf_core/lint/nextflow_config.py
+++ nf_core/lint/nextflow_config.py
@@ -4,14 +4,79 @@ import re
 
 
 def nextflow_config(self):
-    """Checks a given pipeline for required config variables.
+    """Checks the pipeline configuration for required variables.
 
-    At least one string in each list must be present for fail and warn.
-    Any config in config_fail_ifdefined results in a failure.
+    All nf-core pipelines are required to be configured with a minimal set of variable
+    names. This test fails or throws warnings if required variables are not set.
 
-    Uses ``nextflow config -flat`` to parse pipeline ``nextflow.config``
-    and print all config variables.
-    NB: Does NOT parse contents of main.nf / nextflow script
+    .. note:: These config variables must be set in ``nextflow.config`` or another config
+              file imported from there. Any variables set in nextflow script files (eg. ``main.nf``)
+              are not checked and will be assumed to be missing.
+
+    **The following variables fail the test if missing:**
+
+    * ``params.outdir``: A directory in which all pipeline results should be saved
+    * ``manifest.name``: The pipeline name. Should begin with ``nf-core/``
+    * ``manifest.description``: A description of the pipeline
+    * ``manifest.version``
+
+      * The version of this pipeline. This should correspond to a `GitHub release <https://help.github.com/articles/creating-releases/>`_.
+      * If ``--release`` is set when running ``nf-core lint``, the version number must not contain the string ``dev``
+      * If ``--release`` is _not_ set, the version should end in ``dev`` (warning triggered if not)
+
+    * ``manifest.nextflowVersion``
+
+      * The minimum version of Nextflow required to run the pipeline.
+      * Should be ``>=`` or ``!>=`` and a version number, eg. ``manifest.nextflowVersion = '>=0.31.0'`` (see `Nextflow documentation <https://www.nextflow.io/docs/latest/config.html#scope-manifest>`_)
+      * ``>=`` warns about old versions but tries to run anyway, ``!>=`` fails for old versions. Only use the latter if you *know* that the pipeline will certainly fail before this version.
+      * This should correspond to the ``NXF_VER`` version tested by GitHub Actions.
+
+    * ``manifest.homePage``
+
+      * The homepage for the pipeline. Should be the nf-core GitHub repository URL,
+        so beginning with ``https://github.com/nf-core/``
+
+    * ``timeline.enabled``, ``trace.enabled``, ``report.enabled``, ``dag.enabled``
+
+      * The nextflow timeline, trace, report and DAG should be enabled by default (set to ``true``)
+
+    * ``process.cpus``, ``process.memory``, ``process.time``
+
+      * Default CPUs, memory and time limits for tasks
+
+    * ``params.input``
+
+      * Input parameter to specify input data, specify this to avoid a warning
+      * Typical usage:
+
+        * ``params.input``: Input data that is not NGS sequencing data
+
+    **The following variables throw warnings if missing:**
+
+    * ``manifest.mainScript``: The filename of the main pipeline script (should be ``main.nf``)
+    * ``timeline.file``, ``trace.file``, ``report.file``, ``dag.file``
+
+      * Default filenames for the timeline, trace and report
+      * The DAG file path should end with ``.svg`` (If Graphviz is not installed, Nextflow will generate a ``.dot`` file instead)
+
+    * ``process.container``
+
+      * Docker Hub handle for a single default container for use by all processes.
+      * Must specify a tag that matches the pipeline version number if set.
+      * If the pipeline version number contains the string ``dev``, the DockerHub tag must be ``:dev``
+
+    **The following variables are depreciated and fail the test if they are still present:**
+
+    * ``params.version``: The old method for specifying the pipeline version. Replaced by ``manifest.version``
+    * ``params.nf_required_version``: The old method for specifying the minimum Nextflow version. Replaced by ``manifest.nextflowVersion``
+    * ``params.container``: The old method for specifying the dockerhub container address. Replaced by ``process.container``
+    * ``igenomesIgnore``: Changed to ``igenomes_ignore``
+
+        .. tip:: The ``snake_case`` convention should now be used when defining pipeline parameters
+
+    **The following Nextflow syntax is depreciated and fails the test if present:**
+
+    * Process-level configuration syntax still using the old Nextflow syntax, for example: ``process.$fastqc`` instead of ``process withName:'fastqc'``.
     """
     passed = []
     warned = []
@@ -98,48 +163,48 @@ def nextflow_config(self):
     # Check the variables that should be set to 'true'
     for k in ["timeline.enabled", "report.enabled", "trace.enabled", "dag.enabled"]:
         if self.nf_config.get(k) == "true":
-            passed.append("Config `{}` had correct value: `{}`".format(k, self.nf_config.get(k)))
+            passed.append("Config ``{}`` had correct value: ``{}``".format(k, self.nf_config.get(k)))
         else:
-            failed.append("Config `{}` did not have correct value: `{}`".format(k, self.nf_config.get(k)))
+            failed.append("Config ``{}`` did not have correct value: ``{}``".format(k, self.nf_config.get(k)))
 
     # Check that the pipeline name starts with nf-core
     try:
         assert self.nf_config.get("manifest.name", "").strip("'\"").startswith("nf-core/")
     except (AssertionError, IndexError):
         failed.append(
-            "Config `manifest.name` did not begin with `nf-core/`:\n    {}".format(
+            "Config ``manifest.name`` did not begin with ``nf-core/``:\n    {}".format(
                 self.nf_config.get("manifest.name", "").strip("'\"")
             )
         )
     else:
-        passed.append("Config `manifest.name` began with `nf-core/`")
+        passed.append("Config ``manifest.name`` began with ``nf-core/``")
 
     # Check that the homePage is set to the GitHub URL
     try:
         assert self.nf_config.get("manifest.homePage", "").strip("'\"").startswith("https://github.com/nf-core/")
     except (AssertionError, IndexError):
         failed.append(
-            "Config variable `manifest.homePage` did not begin with https://github.com/nf-core/:\n    {}".format(
+            "Config variable ``manifest.homePage`` did not begin with https://github.com/nf-core/:\n    {}".format(
                 self.nf_config.get("manifest.homePage", "").strip("'\"")
             )
         )
     else:
-        passed.append("Config variable `manifest.homePage` began with https://github.com/nf-core/")
+        passed.append("Config variable ``manifest.homePage`` began with https://github.com/nf-core/")
 
-    # Check that the DAG filename ends in `.svg`
+    # Check that the DAG filename ends in ``.svg``
     if "dag.file" in self.nf_config:
         if self.nf_config["dag.file"].strip("'\"").endswith(".svg"):
-            passed.append("Config `dag.file` ended with `.svg`")
+            passed.append("Config ``dag.file`` ended with ``.svg``")
         else:
-            failed.append("Config `dag.file` did not end with `.svg`")
+            failed.append("Config ``dag.file`` did not end with ``.svg``")
 
     # Check that the minimum nextflowVersion is set properly
     if "manifest.nextflowVersion" in self.nf_config:
         if self.nf_config.get("manifest.nextflowVersion", "").strip("\"'").lstrip("!").startswith(">="):
-            passed.append("Config variable `manifest.nextflowVersion` started with >= or !>=")
+            passed.append("Config variable ``manifest.nextflowVersion`` started with >= or !>=")
         else:
             failed.append(
-                "Config `manifest.nextflowVersion` did not start with `>=` or `!>=` : `{}`".format(
+                "Config ``manifest.nextflowVersion`` did not start with ``>=`` or ``!>=`` : ``{}``".format(
                     self.nf_config.get("manifest.nextflowVersion", "")
                 ).strip("\"'")
             )
@@ -159,37 +224,39 @@ def nextflow_config(self):
         except AssertionError:
             if self.release_mode:
                 failed.append(
-                    "Config `process.container` looks wrong. Should be `{}` but is `{}`".format(
+                    "Config ``process.container`` looks wrong. Should be ``{}`` but is ``{}``".format(
                         container_name, self.nf_config.get("process.container", "").strip("'")
                     )
                 )
             else:
                 warned.append(
-                    "Config `process.container` looks wrong. Should be `{}` but is `{}`".format(
+                    "Config ``process.container`` looks wrong. Should be ``{}`` but is ``{}``".format(
                         container_name, self.nf_config.get("process.container", "").strip("'")
                     )
                 )
         else:
-            passed.append("Config `process.container` looks correct: `{}`".format(container_name))
+            passed.append("Config ``process.container`` looks correct: ``{}``".format(container_name))
 
-    # Check that the pipeline version contains `dev`
+    # Check that the pipeline version contains ``dev``
     if not self.release_mode and "manifest.version" in self.nf_config:
         if self.nf_config["manifest.version"].strip(" '\"").endswith("dev"):
-            passed.append("Config `manifest.version` ends in `dev`: `{}`".format(self.nf_config["manifest.version"]))
+            passed.append(
+                "Config ``manifest.version`` ends in ``dev``: ``{}``".format(self.nf_config["manifest.version"])
+            )
         else:
             warned.append(
-                "Config `manifest.version` should end in `dev`: `{}`".format(self.nf_config["manifest.version"])
+                "Config ``manifest.version`` should end in ``dev``: ``{}``".format(self.nf_config["manifest.version"])
             )
     elif "manifest.version" in self.nf_config:
         if "dev" in self.nf_config["manifest.version"]:
             failed.append(
-                "Config `manifest.version` should not contain `dev` for a release: `{}`".format(
+                "Config ``manifest.version`` should not contain ``dev`` for a release: ``{}``".format(
                     self.nf_config["manifest.version"]
                 )
             )
         else:
             passed.append(
-                "Config `manifest.version` does not contain `dev` for release: `{}`".format(
+                "Config ``manifest.version`` does not contain ``dev`` for release: ``{}``".format(
                     self.nf_config["manifest.version"]
                 )
             )
