e4686d955acdcf3166c4aa955689349ad8e95e87
kevinmenden <kevin.menden@t-online.de>
2021-01-05 13:14:02 +0100

    only validating .yaml/.yml files

--- nf_core/lint/actions_schema_validation.py
+++ nf_core/lint/actions_schema_validation.py
@@ -5,6 +5,7 @@ import yaml
 import json
 import jsonschema
 import os
+import glob
 import requests
 
 
@@ -17,23 +18,22 @@ def actions_schema_validation(self):
     to verify that all workflow yml files adhere to the correct Schema
     """
     passed = []
-    warned = []
     failed = []
 
     # Only show error messages from schema
     logging.getLogger("nf_core.schema").setLevel(logging.ERROR)
 
     # Get all workflow files
-    action_workflows = os.listdir(os.path.join(self.wf_path, ".github/workflows"))
+    action_workflows = glob.glob(os.path.join(self.wf_path, ".github/workflows/*.y*ml"))
 
     # Load the GitHub workflow schema
     r = requests.get("https://json.schemastore.org/github-workflow", allow_redirects=True)
     schema = r.json()
 
     # Validate all workflows against the schema
-    for wf in action_workflows:
+    for wf_path in action_workflows:
         # load workflow
-        wf_path = os.path.join(self.wf_path, ".github/workflows", wf)
+        wf = os.path.basename(wf_path)
         with open(wf_path, "r") as fh:
             wf_json = yaml.safe_load(fh)
 
@@ -50,4 +50,4 @@ def actions_schema_validation(self):
         except Exception as e:
             failed.append("Workflow validation failed for {}: {}".format(wf, e))
 
-    return {"passed": passed, "warned": warned, "failed": failed}
+    return {"passed": passed, "failed": failed}
