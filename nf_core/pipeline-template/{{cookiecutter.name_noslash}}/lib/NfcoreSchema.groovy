9db29050565567672c2c019d63c26e86d8198c7c
kevinmenden <kevin.menden@t-online.de>
2021-02-18 13:25:53 +0100

    removed multiqc function from nfcoreschema

--- nf_core/pipeline-template/{{cookiecutter.name_noslash}}/lib/NfcoreSchema.groovy
+++ nf_core/pipeline-template/{{cookiecutter.name_noslash}}/lib/NfcoreSchema.groovy
@@ -422,28 +422,4 @@ class NfcoreSchema {
         return output
     }
 
-    static String params_summary_multiqc(workflow, summary) {
-        String summary_section = ''
-        for (group in summary.keySet()) {
-            def group_params = summary.get(group)  // This gets the parameters of that particular group
-            if (group_params) {
-                summary_section += "    <p style=\"font-size:110%\"><b>$group</b></p>\n"
-                summary_section += "    <dl class=\"dl-horizontal\">\n"
-                for (param in group_params.keySet()) {
-                    summary_section += "        <dt>$param</dt><dd><samp>${group_params.get(param) ?: '<span style=\"color:#999999;\">N/A</a>'}</samp></dd>\n"
-                }
-                summary_section += '    </dl>\n'
-            }
-        }
-
-        String yaml_file_text  = "id: '${workflow.manifest.name.replace('/', '-')}-summary'\n"
-        yaml_file_text        += "description: ' - this information is collected when the pipeline is started.'\n"
-        yaml_file_text        += "section_name: '${workflow.manifest.name} Workflow Summary'\n"
-        yaml_file_text        += "section_href: 'https://github.com/${workflow.manifest.name}'\n"
-        yaml_file_text        += "plot_type: 'html'\n"
-        yaml_file_text        += 'data: |\n'
-        yaml_file_text        += "${summary_section}"
-        return yaml_file_text
-    }
-
 }
