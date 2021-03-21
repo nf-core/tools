c7e1b6f085c4c7102bb04991902e3c708e1e2786
drpatelh <drhpatel@gmail.com>
2021-03-21 18:07:07 +0000

    Move workflow checks to Workflow.groovy

--- nf_core/pipeline-template/lib/Checks.groovy
+++ nf_core/pipeline-template/lib/Checks.groovy
@@ -63,38 +63,4 @@ class Checks {
             }
         }
     }
-
-    // Citation string
-    private static String citation(workflow) {
-        return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-               "* The pipeline\n" + 
-               "  https://doi.org/10.5281/zenodo.1400710\n\n" +
-               "* The nf-core framework\n" +
-               "  https://dx.doi.org/10.1038/s41587-020-0439-x\n" +
-               "* Software dependencies\n" +
-               "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
-    }
-
-    // Exit pipeline if incorrect --genome key provided
-    static void genome_exists(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "=============================================================================\n" +
-                      "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                      "  Currently, the available genome keys are:\n" +
-                      "  ${params.genomes.keySet().join(", ")}\n" +
-                      "============================================================================="
-            System.exit(0)
-        }
-    }
-
-    // Get attribute from genome config file e.g. fasta
-    static String get_genome_attribute(params, attribute) {
-        def val = ''
-        if (params.genomes && params.genome && params.genomes.containsKey(params.genome)) {
-            if (params.genomes[ params.genome ].containsKey(attribute)) {
-                val = params.genomes[ params.genome ][ attribute ]
-            }
-        }
-        return val
-    }    
 }
