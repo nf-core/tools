8982dcd80c03740f0a97ee1d0d0898c42fb1e91f
drpatelh <drhpatel@gmail.com>
2021-03-21 14:13:25 +0000

    Update input samplesheet format

--- nf_core/pipeline-template/bin/check_samplesheet.py
+++ nf_core/pipeline-template/bin/check_samplesheet.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 # TODO nf-core: Update the script to check the samplesheet
-# This script is based on the example at: https://raw.githubusercontent.com/nf-core/test-datasets/atacseq/design.csv
-
+# This script is based on the example at: https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
 
 import os
 import sys
 import errno
 import argparse
-
 
 def parse_args(args=None):
     Description = "Reformat {{ name }} samplesheet file and check its contents."
@@ -19,7 +17,6 @@ def parse_args(args=None):
     parser.add_argument("FILE_OUT", help="Output file.")
     return parser.parse_args(args)
 
-
 def make_dir(path):
     if len(path) > 0:
         try:
@@ -27,7 +24,6 @@ def make_dir(path):
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise exception
-
 
 def print_error(error, context="Line", context_str=""):
     error_str = "ERROR: Please check samplesheet -> {}".format(error)
@@ -38,26 +34,27 @@ def print_error(error, context="Line", context_str=""):
     print(error_str)
     sys.exit(1)
 
-
 # TODO nf-core: Update the check_samplesheet function
 def check_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
 
-    group,replicate,fastq_1,fastq_2
-    WT,1,WT_LIB1_REP1_1.fastq.gz,WT_LIB1_REP1_2.fastq.gz
-    WT,1,WT_LIB2_REP1_1.fastq.gz,WT_LIB2_REP1_2.fastq.gz
-    WT,2,WT_LIB1_REP2_1.fastq.gz,WT_LIB1_REP2_2.fastq.gz
-    KO,1,KO_LIB1_REP1_1.fastq.gz,KO_LIB1_REP1_2.fastq.gz
+    sample,fastq_1,fastq_2
+    SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz
+    SAMPLE_PE,SAMPLE_PE_RUN2_1.fastq.gz,SAMPLE_PE_RUN2_2.fastq.gz
+    SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,
+
+    For an example see:
+    https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
     """
 
-    sample_run_dict = {}
+    sample_mapping_dict = {}
     with open(file_in, "r") as fin:
 
         ## Check header
-        MIN_COLS = 3
+        MIN_COLS = 2
         # TODO nf-core: Update the column names for the input samplesheet
-        HEADER = ["group", "replicate", "fastq_1", "fastq_2"]
+        HEADER = ['sample', 'fastq_1', 'fastq_2']
         header = [x.strip('"') for x in fin.readline().strip().split(",")]
         if header[: len(HEADER)] != HEADER:
             print("ERROR: Please check samplesheet header -> {} != {}".format(",".join(header), ",".join(HEADER)))
@@ -67,7 +64,7 @@ def check_samplesheet(file_in, file_out):
         for line in fin:
             lspl = [x.strip().strip('"') for x in line.strip().split(",")]
 
-            ## Check valid number of columns per row
+            # Check valid number of columns per row
             if len(lspl) < len(HEADER):
                 print_error(
                     "Invalid number of columns (minimum = {})!".format(len(HEADER)),
@@ -83,17 +80,12 @@ def check_samplesheet(file_in, file_out):
                 )
 
             ## Check sample name entries
-            sample, replicate, fastq_1, fastq_2 = lspl[: len(HEADER)]
+            sample, fastq_1, fastq_2 = lspl[: len(HEADER)]
             if sample:
                 if sample.find(" ") != -1:
-                    print_error("Group entry contains spaces!", "Line", line)
+                    print_error("Sample entry contains spaces!", "Line", line)
             else:
-                print_error("Group entry has not been specified!", "Line", line)
-
-            ## Check replicate entry is integer
-            if not replicate.isdigit():
-                print_error("Replicate id not an integer!", "Line", line)
-            replicate = int(replicate)
+                print_error("Sample entry has not been specified!", "Line", line)
 
             ## Check FastQ file extension
             for fastq in [fastq_1, fastq_2]:
@@ -110,60 +102,41 @@ def check_samplesheet(file_in, file_out):
             ## Auto-detect paired-end/single-end
             sample_info = []  ## [single_end, fastq_1, fastq_2]
             if sample and fastq_1 and fastq_2:  ## Paired-end short reads
-                sample_info = ["0", fastq_1, fastq_2]
+                sample_info = ['0', fastq_1, fastq_2]
             elif sample and fastq_1 and not fastq_2:  ## Single-end short reads
-                sample_info = ["1", fastq_1, fastq_2]
+                sample_info = ['1', fastq_1, fastq_2]
             else:
                 print_error("Invalid combination of columns provided!", "Line", line)
-            ## Create sample mapping dictionary = {sample: {replicate : [ single_end, fastq_1, fastq_2 ]}}
-            if sample not in sample_run_dict:
-                sample_run_dict[sample] = {}
-            if replicate not in sample_run_dict[sample]:
-                sample_run_dict[sample][replicate] = [sample_info]
+
+            ## Create sample mapping dictionary = { sample: [ single_end, fastq_1, fastq_2 ] }
+            if sample not in sample_mapping_dict:
+                sample_mapping_dict[sample] = [sample_info]
             else:
-                if sample_info in sample_run_dict[sample][replicate]:
+                if sample_info in sample_mapping_dict[sample]:
                     print_error("Samplesheet contains duplicate rows!", "Line", line)
                 else:
-                    sample_run_dict[sample][replicate].append(sample_info)
+                    sample_mapping_dict[sample].append(sample_info)
 
     ## Write validated samplesheet with appropriate columns
-    if len(sample_run_dict) > 0:
+    if len(sample_mapping_dict) > 0:
         out_dir = os.path.dirname(file_out)
         make_dir(out_dir)
         with open(file_out, "w") as fout:
-
             fout.write(",".join(["sample", "single_end", "fastq_1", "fastq_2"]) + "\n")
-            for sample in sorted(sample_run_dict.keys()):
+            for sample in sorted(sample_mapping_dict.keys()):
 
-                ## Check that replicate ids are in format 1..<NUM_REPS>
-                uniq_rep_ids = set(sample_run_dict[sample].keys())
-                if len(uniq_rep_ids) != max(uniq_rep_ids):
-                    print_error(
-                        "Replicate ids must start with 1..<num_replicates>!",
-                        "Group",
-                        sample,
-                    )
-                for replicate in sorted(sample_run_dict[sample].keys()):
+                ## Check that multiple runs of the same sample are of the same datatype
+                if not all(x[0] == sample_mapping_dict[sample][0][0] for x in sample_mapping_dict[sample]):
+                    print_error("Multiple runs of a sample must be of the same datatype!","Sample: {}".format(sample))
 
-                    ## Check that multiple runs of the same sample are of the same datatype
-                    if not all(
-                        x[0] == sample_run_dict[sample][replicate][0][0] for x in sample_run_dict[sample][replicate]
-                    ):
-                        print_error(
-                            "Multiple runs of a sample must be of the same datatype!",
-                            "Group",
-                            sample,
-                        )
-                    ## Write to file
-                    for idx, sample_info in enumerate(sample_run_dict[sample][replicate]):
-                        sample_id = "{}_R{}_T{}".format(sample, replicate, idx + 1)
-                        fout.write(",".join([sample_id] + sample_info) + "\n")
-
+                for idx,val in enumerate(sample_mapping_dict[sample]):
+                    fout.write(','.join(["{}_T{}".format(sample,idx+1)] + val) + '\n')
+    else:
+        print_error("No entries to process!","Samplesheet: {}".format(file_in))
 
 def main(args=None):
     args = parse_args(args)
     check_samplesheet(args.FILE_IN, args.FILE_OUT)
-
-
+    
 if __name__ == "__main__":
     sys.exit(main())
