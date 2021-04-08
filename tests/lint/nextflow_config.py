81304172ca1ebe2ede2c91587bc4dfbdde9adbea
Erik Danielsson <danielsson.erik.0@gmail.com>
2021-04-08 11:23:14 +0200

    black

--- tests/lint/nextflow_config.py
+++ tests/lint/nextflow_config.py
@@ -28,26 +28,27 @@ def test_nextflow_config_bad_name_fail(self):
     assert len(result["failed"]) == 1
     assert len(result["warned"]) == 0
 
+
 def test_nextflow_config_bad_container_name_failed(self):
     """Tests that config variable existence test fails with bad container name"""
     new_pipeline = self._make_pipeline_copy()
     lint_obj = nf_core.lint.PipelineLint(new_pipeline)
     lint_obj._load_pipeline_config()
-    
+
     lint_obj.nf_config["process.container"] = "bad_name"
     result = lint_obj.nextflow_config()
     assert len(result["failed"]) == 0
     assert len(result["warned"]) == 1
+
 
 def test_nextflow_config_dev_in_release_mode_failed(self):
     """Tests that config variable existence test fails with dev version in release mode"""
     new_pipeline = self._make_pipeline_copy()
     lint_obj = nf_core.lint.PipelineLint(new_pipeline)
     lint_obj._load_pipeline_config()
-     
+
     lint_obj.release_mode = True
     lint_obj.nf_config["manifest.version"] = "dev_is_bad_name"
     result = lint_obj.nextflow_config()
     assert len(result["failed"]) == 1
     assert len(result["warned"]) == 0
-
