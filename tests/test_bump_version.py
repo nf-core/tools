44c1d0eec64335e04c66f1cf50eaf27df02330fa
Harshil Patel <drpatelhh@gmail.com>
2021-05-25 13:57:10 +0100

    Fix bump version tests

--- tests/test_bump_version.py
+++ tests/test_bump_version.py
@@ -28,24 +28,12 @@ def test_bump_pipeline_version(datafiles):
     # Check nextflow.config
     new_pipeline_obj._load_pipeline_config()
     assert new_pipeline_obj.nf_config["manifest.version"].strip("'\"") == "1.1"
-    assert new_pipeline_obj.nf_config["process.container"].strip("'\"") == "nfcore/testpipeline:1.1"
 
     # Check .github/workflows/ci.yml
     with open(new_pipeline_obj._fp(".github/workflows/ci.yml")) as fh:
         ci_yaml = yaml.safe_load(fh)
     assert ci_yaml["jobs"]["test"]["steps"][2]["run"] == "docker build --no-cache . -t nfcore/testpipeline:1.1"
     assert "docker tag nfcore/testpipeline:dev nfcore/testpipeline:1.1" in ci_yaml["jobs"]["test"]["steps"][3]["run"]
-
-    # Check environment.yml
-    with open(new_pipeline_obj._fp("environment.yml")) as fh:
-        conda_env = yaml.safe_load(fh)
-    assert conda_env["name"] == "nf-core-testpipeline-1.1"
-
-    # Check Dockerfile
-    with open(new_pipeline_obj._fp("Dockerfile")) as fh:
-        dockerfile = fh.read().splitlines()
-    assert "ENV PATH /opt/conda/envs/nf-core-testpipeline-1.1/bin:$PATH" in dockerfile
-    assert "RUN conda env export --name nf-core-testpipeline-1.1 > nf-core-testpipeline-1.1.yml" in dockerfile
 
 
 def test_dev_bump_pipeline_version(datafiles):
@@ -66,7 +54,6 @@ def test_dev_bump_pipeline_version(datafiles):
     # Check the pipeline config
     new_pipeline_obj._load_pipeline_config()
     assert new_pipeline_obj.nf_config["manifest.version"].strip("'\"") == "1.2dev"
-    assert new_pipeline_obj.nf_config["process.container"].strip("'\"") == "nfcore/testpipeline:dev"
 
 
 def test_bump_nextflow_version(datafiles):
@@ -85,7 +72,7 @@ def test_bump_nextflow_version(datafiles):
 
     # Check nextflow.config
     new_pipeline_obj._load_pipeline_config()
-    assert new_pipeline_obj.nf_config["manifest.nextflowVersion"].strip("'\"") == ">=19.10.3-edge"
+    assert new_pipeline_obj.nf_config["manifest.nextflowVersion"].strip("'\"") == "!>=21.04.0"
 
     # Check .github/workflows/ci.yml
     with open(new_pipeline_obj._fp(".github/workflows/ci.yml")) as fh:
