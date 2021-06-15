efcd6e2a13e6a9a654fcfcafbc7a4d389e5aae1f
kevinmenden <kevin.menden@t-online.de>
2021-06-15 08:57:55 +0200

    modules update initial commit

---
+++ nf_core/modules/update.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+The ModuleCreate class handles generating of module templates
+"""
+
+class ModuleUpdate(object):
+
+    def __init__(self, directory=".", tool="", author=None, process_label=None, has_meta=None, force=False, conda_name=None):
+        super().__init__()
