6632f3fa61c2eae2c3b346bce4d2f86b0cfe6008
kevinmenden <kevin.menden@t-online.de>
2021-06-16 10:28:50 +0200

    black

--- nf_core/modules/update.py
+++ nf_core/modules/update.py
@@ -3,7 +3,9 @@
 The ModuleCreate class handles generating of module templates
 """
 
-class ModuleUpdate(object):
 
-    def __init__(self, directory=".", tool="", author=None, process_label=None, has_meta=None, force=False, conda_name=None):
+class ModuleUpdate(object):
+    def __init__(
+        self, directory=".", tool="", author=None, process_label=None, has_meta=None, force=False, conda_name=None
+    ):
         super().__init__()
