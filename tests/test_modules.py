a3f63f7513a960e9a2051a4506c1d8b3993c3632
kevinmenden <kevin.menden@t-online.de>
2021-06-29 11:06:27 +0200

    black

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -241,7 +241,7 @@ class TestModules(unittest.TestCase):
     def test_modules_bump_versions_single_module(self):
         """ Test updating a single module """
         # Change the star/align version to an older version
-        main_nf_path = os.path.join(self.nfcore_modules, "software", "star", "align", "main.nf") 
+        main_nf_path = os.path.join(self.nfcore_modules, "software", "star", "align", "main.nf")
         with open(main_nf_path, "r") as fh:
             content = fh.read()
         new_content = re.sub(r"bioconda::star=\d.\d.\d\D?", r"bioconda::star=2.6.1d", content)
@@ -267,7 +267,7 @@ class TestModules(unittest.TestCase):
     def test_modules_bump_versions_fail_unknown_version(self):
         """ Fail because of an unknown version """
         # Change the star/align version to an older version
-        main_nf_path = os.path.join(self.nfcore_modules, "software", "star", "align", "main.nf") 
+        main_nf_path = os.path.join(self.nfcore_modules, "software", "star", "align", "main.nf")
         with open(main_nf_path, "r") as fh:
             content = fh.read()
         new_content = re.sub(r"bioconda::star=\d.\d.\d\D?", r"bioconda::star=xxx", content)
@@ -276,4 +276,3 @@ class TestModules(unittest.TestCase):
         version_bumper = nf_core.modules.ModuleVersionBumper(pipeline_dir=self.nfcore_modules)
         version_bumper.bump_versions(module="star/align")
         assert "Conda package had unknown version" in version_bumper.failed[0][0]
-    
