800a8dec2323e7240ffde01833c44a3281321f0b
Erik Danielsson <danielsson.erik.0@gmail.com>
2021-07-07 16:13:11 +0200

    remove tests with external modules source

--- tests/modules/remove.py
+++ tests/modules/remove.py
@@ -9,14 +9,15 @@ def test_modules_remove_trimgalore(self):
     assert os.path.exists(module_path) is False
 
 
-def test_modules_remove_trimgalore_alternative_source(self):
-    """Test removing TrimGalore! module after installing it from an alternative source"""
-    self.mods_install_alt.install("trimgalore")
-    module_path = os.path.join(self.mods_install.dir, "modules", "external", "trimgalore")
-    assert self.mods_remove_alt.remove("trimgalore")
-    assert os.path.exists(module_path) is False
-
-
 def test_modules_remove_trimgalore_uninstalled(self):
     """Test removing TrimGalore! module without installing it"""
     assert self.mods_remove.remove("trimgalore") is False
+
+
+# TODO Remove comments once external repository to have same structure as nf-core/modules
+# def test_modules_remove_trimgalore_alternative_source(self):
+#     """Test removing TrimGalore! module after installing it from an alternative source"""
+#     self.mods_install_alt.install("trimgalore")
+#     module_path = os.path.join(self.mods_install.dir, "modules", "external", "trimgalore")
+#     assert self.mods_remove_alt.remove("trimgalore")
+#     assert os.path.exists(module_path) is False
