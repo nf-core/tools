8bdef150d8b78e686c3a9446bec60e438e5211b3
Erik Danielsson <danielsson.erik.0@gmail.com>
2021-07-07 09:53:37 +0200

    Remove loading of 'environment.yml' file

--- nf_core/lint/__init__.py
+++ nf_core/lint/__init__.py
@@ -45,7 +45,6 @@ def run_linting(
     # Load the various pipeline configs
     lint_obj._load_lint_config()
     lint_obj._load_pipeline_config()
-    lint_obj._load_conda_environment()
     lint_obj._list_files()
 
     # Run the linting tests
