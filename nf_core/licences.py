bd1432b262189a7682b2dc3c3f258e28817d9c9e
Kevin Menden <kevin.menden@t-online.de>
2021-07-07 12:34:52 +0200

    Update nf_core/licences.py
    Co-authored-by: Erik Danielsson <53212377+ErikDanielsson@users.noreply.github.com>

--- nf_core/licences.py
+++ nf_core/licences.py
@@ -62,7 +62,7 @@ class WorkflowLicences(object):
             # Check that the pipeline exists
             if response.status_code == 404:
                 raise LookupError(
-                    f"Couldn't find pipeline conda file: {env_url}. (Note: DSL2 pipelines are currently not supported by this command."
+                    f"Couldn't find pipeline conda file: {env_url}. (Note: DSL2 pipelines are currently not supported by this command.)"
                 )
             self.conda_config = yaml.safe_load(response.text)
 
