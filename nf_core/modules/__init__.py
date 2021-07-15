2edc8a01ec9b1f53f8d054609931e2753a7e788a
Erik Danielsson <danielsson.erik.0@gmail.com>
2021-07-15 15:19:24 +0200

    Update __init__ and modules installed in pipeline

--- nf_core/modules/__init__.py
+++ nf_core/modules/__init__.py
@@ -6,4 +6,5 @@ from .bump_versions import ModuleVersionBumper
 from .module_utils import ModuleException
 from .list import ModuleList
 from .install import ModuleInstall
+from .update import ModuleUpdate
 from .remove import ModuleRemove
