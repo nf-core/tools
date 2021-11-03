b15c6c03e3947380a5b65b7808f3dc2bb7d609d0
Harshil Patel <drpatelhh@gmail.com>
2021-11-03 12:50:32 +0000

    Add a check to see if any custom configuration is provided

--- nf_core/pipeline-template/lib/NfcoreTemplate.groovy
+++ nf_core/pipeline-template/lib/NfcoreTemplate.groovy
@@ -19,6 +19,20 @@ class NfcoreTemplate {
     }
 
     //
+    //  Warn if a -profile or Nextflow config has not been provided to run the pipeline
+    //
+    public static void checkConfigProvided(workflow, log) {
+        if (workflow.profile == 'standard' && workflow.configFiles.size() <= 1) {
+            log.warn "[$workflow.manifest.name] You are attempting to run the pipeline without any custom configuration!\n\n" +
+                    "This will be dependent on your local compute enviroment but can be acheived by:\n" +
+                    "   (1) Using an existing pipeline profile e.g. `-profile docker` or `-profile singularity`\n" +
+                    "   (2) Using an existing nf-core/configs for your Institution e.g. `-profile crick` or `-profile awsbatch`\n" +
+                    "   (3) Using your own local custom config e.g. `-c /path/to/your/custom.config`\n\n" +
+                    "Please refer to the quick start section    and usage docs for the pipeline.\n "
+        }
+    }
+
+    //
     // Construct and send completion email
     //
     public static void email(workflow, params, summary_params, projectDir, log, multiqc_report=[]) {
