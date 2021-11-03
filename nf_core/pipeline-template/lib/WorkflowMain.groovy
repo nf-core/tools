bb5f4603e27010f2098179c29b7c17ec725d95f8
Harshil Patel <drpatelhh@gmail.com>
2021-11-03 11:21:16 +0000

    Remove params.hostnames from pipeline template

--- nf_core/pipeline-template/lib/WorkflowMain.groovy
+++ nf_core/pipeline-template/lib/WorkflowMain.groovy
@@ -69,9 +69,6 @@ class WorkflowMain {
         // Check AWS batch settings
         NfcoreTemplate.awsBatch(workflow, params)
 
-        // Check the hostnames against configured profiles
-        NfcoreTemplate.hostName(workflow, params, log)
-
         // Check input has been provided
         if (!params.input) {
             log.error "Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'"
