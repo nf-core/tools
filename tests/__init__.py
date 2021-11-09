880345107e6edf72ca11e9052fef24af335a54aa
Paolo Cozzi <bunop@libero.it>
2021-11-09 15:49:23 +0100

    start using context manager for temporary files and folders

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 13:46:10 2021
+
+@author: Paolo Cozzi <paolo.cozzi@ibba.cnr.it>
+"""
--- tests/test_download.py
+++ tests/test_download.py
@@ -14,6 +14,8 @@ import shutil
 import tempfile
 import unittest
 
+from .utils import with_temporary_folder, with_temporary_file
+
 
 class DownloadTest(unittest.TestCase):
 
@@ -73,8 +75,8 @@ class DownloadTest(unittest.TestCase):
     #
     # Tests for 'download_wf_files'
     #
-    def test_download_wf_files(self):
-        outdir = tempfile.mkdtemp()
+    @with_temporary_folder
+    def test_download_wf_files(self, outdir):
         download_obj = DownloadWorkflow(pipeline="nf-core/methylseq", revision="1.6")
         download_obj.outdir = outdir
         download_obj.wf_sha = "b3e5e3b95aaf01d98391a62a10a3990c0a4de395"
@@ -87,8 +89,8 @@ class DownloadTest(unittest.TestCase):
     #
     # Tests for 'download_configs'
     #
-    def test_download_configs(self):
-        outdir = tempfile.mkdtemp()
+    @with_temporary_folder
+    def test_download_configs(self, outdir):
         download_obj = DownloadWorkflow(pipeline="nf-core/methylseq", revision="1.6")
         download_obj.outdir = outdir
         download_obj.download_configs()
@@ -97,30 +99,32 @@ class DownloadTest(unittest.TestCase):
     #
     # Tests for 'wf_use_local_configs'
     #
-    def test_wf_use_local_configs(self):
+    @with_temporary_folder
+    def test_wf_use_local_configs(self, tmp_path):
         # Get a workflow and configs
-        test_pipeline_dir = os.path.join(tempfile.mkdtemp(), "nf-core-testpipeline")
+        test_pipeline_dir = os.path.join(tmp_path, "nf-core-testpipeline")
         create_obj = nf_core.create.PipelineCreate(
             "testpipeline", "This is a test pipeline", "Test McTestFace", outdir=test_pipeline_dir
         )
         create_obj.init_pipeline()
 
-        test_outdir = tempfile.mkdtemp()
-        download_obj = DownloadWorkflow(pipeline="dummy", revision="1.2.0", outdir=test_outdir)
-        shutil.copytree(test_pipeline_dir, os.path.join(test_outdir, "workflow"))
-        download_obj.download_configs()
+        with tempfile.TemporaryDirectory() as test_outdir:
+            download_obj = DownloadWorkflow(pipeline="dummy", revision="1.2.0", outdir=test_outdir)
+            shutil.copytree(test_pipeline_dir, os.path.join(test_outdir, "workflow"))
+            download_obj.download_configs()
 
-        # Test the function
-        download_obj.wf_use_local_configs()
-        wf_config = nf_core.utils.fetch_wf_config(os.path.join(test_outdir, "workflow"), cache_config=False)
-        assert wf_config["params.custom_config_base"] == f"'{test_outdir}/workflow/../configs/'"
+            # Test the function
+            download_obj.wf_use_local_configs()
+            wf_config = nf_core.utils.fetch_wf_config(os.path.join(test_outdir, "workflow"), cache_config=False)
+            assert wf_config["params.custom_config_base"] == f"'{test_outdir}/workflow/../configs/'"
 
     #
     # Tests for 'find_container_images'
     #
+    @with_temporary_folder
     @mock.patch("nf_core.utils.fetch_wf_config")
-    def test_find_container_images(self, mock_fetch_wf_config):
-        download_obj = DownloadWorkflow(pipeline="dummy", outdir=tempfile.mkdtemp())
+    def test_find_container_images(self, tmp_path, mock_fetch_wf_config):
+        download_obj = DownloadWorkflow(pipeline="dummy", outdir=tmp_path)
         mock_fetch_wf_config.return_value = {
             "process.mapping.container": "cutting-edge-container",
             "process.nocontainer": "not-so-cutting-edge",
@@ -132,60 +136,52 @@ class DownloadTest(unittest.TestCase):
     #
     # Tests for 'validate_md5'
     #
-    def test_matching_md5sums(self):
+    @with_temporary_file
+    def test_matching_md5sums(self, tmpfile):
         download_obj = DownloadWorkflow(pipeline="dummy")
         test_hash = hashlib.md5()
         test_hash.update(b"test")
         val_hash = test_hash.hexdigest()
-        tmpfilehandle, tmpfile = tempfile.mkstemp()
 
-        with open(tmpfile[1], "w") as f:
+        with open(tmpfile.name, "w") as f:
             f.write("test")
 
-        download_obj.validate_md5(tmpfile[1], val_hash)
+        download_obj.validate_md5(tmpfile.name, val_hash)
 
-        # Clean up
-        os.remove(tmpfile[1])
-
+    @with_temporary_file
     @pytest.mark.xfail(raises=IOError, strict=True)
-    def test_mismatching_md5sums(self):
+    def test_mismatching_md5sums(self, tmpfile):
         download_obj = DownloadWorkflow(pipeline="dummy")
         test_hash = hashlib.md5()
         test_hash.update(b"other value")
         val_hash = test_hash.hexdigest()
-        tmpfilehandle, tmpfile = tempfile.mkstemp()
 
-        with open(tmpfile, "w") as f:
+        with open(tmpfile.name, "w") as f:
             f.write("test")
 
-        download_obj.validate_md5(tmpfile[1], val_hash)
+        download_obj.validate_md5(tmpfile.name, val_hash)
 
-        # Clean up
-        os.remove(tmpfile)
 
     #
     # Tests for 'singularity_pull_image'
     #
     # If Singularity is not installed, will log an error and exit
     # If Singularity is installed, should raise an OSError due to non-existant image
+    @with_temporary_folder
     @pytest.mark.xfail(raises=OSError)
     @mock.patch("rich.progress.Progress.add_task")
-    def test_singularity_pull_image(self, mock_rich_progress):
-        tmp_dir = tempfile.mkdtemp()
+    def test_singularity_pull_image(self, tmp_dir, mock_rich_progress):
         download_obj = DownloadWorkflow(pipeline="dummy", outdir=tmp_dir)
         download_obj.singularity_pull_image("a-container", tmp_dir, None, mock_rich_progress)
 
-        # Clean up
-        shutil.rmtree(tmp_dir)
 
     #
     # Tests for the main entry method 'download_workflow'
     #
+    @with_temporary_folder
     @mock.patch("nf_core.download.DownloadWorkflow.singularity_pull_image")
     @mock.patch("shutil.which")
-    def test_download_workflow_with_success(self, mock_download_image, mock_singularity_installed):
-
-        tmp_dir = tempfile.mkdtemp()
+    def test_download_workflow_with_success(self, tmp_dir, mock_download_image, mock_singularity_installed):
         os.environ["NXF_SINGULARITY_CACHEDIR"] = "foo"
 
         download_obj = DownloadWorkflow(
@@ -197,6 +193,3 @@ class DownloadTest(unittest.TestCase):
         )
 
         download_obj.download_workflow()
-
-        # Clean up
-        shutil.rmtree(tmp_dir)
