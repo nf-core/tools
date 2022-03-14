85a048dbf2d37db5d985e9a0755e4a79d02c0862
Phil Ewels <phil.ewels@scilifelab.se>
2022-03-14 14:42:57 +0100

    Remove flag --repo-type
    Closes #1433

--- nf_core/__main__.py
+++ nf_core/__main__.py
@@ -526,8 +526,7 @@ def remove(ctx, dir, tool):
 @click.option("-f", "--force", is_flag=True, default=False, help="Overwrite any files if they already exist")
 @click.option("-c", "--conda-name", type=str, default=None, help="Name of the conda package to use")
 @click.option("-p", "--conda-package-version", type=str, default=None, help="Version of conda package to use")
-@click.option("-r", "--repo-type", type=click.Choice(["pipeline", "modules"]), default=None, help="Type of repository")
-def create_module(ctx, tool, dir, author, label, meta, no_meta, force, conda_name, conda_package_version, repo_type):
+def create_module(ctx, tool, dir, author, label, meta, no_meta, force, conda_name, conda_package_version):
     """
     Create a new DSL2 module from the nf-core template.
 
@@ -549,7 +548,7 @@ def create_module(ctx, tool, dir, author, label, meta, no_meta, force, conda_nam
     # Run function
     try:
         module_create = nf_core.modules.ModuleCreate(
-            dir, tool, author, label, has_meta, force, conda_name, conda_package_version, repo_type
+            dir, tool, author, label, has_meta, force, conda_name, conda_package_version
         )
         module_create.create()
     except UserWarning as e:
