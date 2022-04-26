2e2ea58479e3725b37877e5cbb7ae1e075fb8700
Júlia Mir Pedrol <julia@julias-mbp.am10.uni-tuebingen.de>
2022-04-26 13:10:10 +0200

    add code tests

---
+++ tests/modules/module_test.py
@@ -0,0 +1,15 @@
+"""Test the 'modules test' command which runs module pytests."""
+import os
+import pytest
+
+import nf_core.modules
+
+def test_modules_test_check_inputs(self):
+    """Test the check_inputs() function - raise UserWarning because module doesn't exist"""
+    cwd = os.getcwd()
+    os.chdir(self.nfcore_modules)
+    meta_builder = nf_core.modules.ModulesTest("none", True, "")
+    with pytest.raises(UserWarning) as excinfo:
+        meta_builder.check_inputs()
+    os.chdir(cwd)
+    assert "Cannot find directory" in str(excinfo.value)
