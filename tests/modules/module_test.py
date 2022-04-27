f3dc1875d61c4f7ef51019ec5c6f553d3faab265
Júlia Mir Pedrol <julia@julias-mbp.am10.uni-tuebingen.de>
2022-04-27 12:48:44 +0200

    fix test

--- tests/modules/module_test.py
+++ tests/modules/module_test.py
@@ -11,6 +11,6 @@ def test_modules_test_check_inputs(self):
     os.chdir(self.nfcore_modules)
     meta_builder = nf_core.modules.ModulesTest("none", True, "")
     with pytest.raises(UserWarning) as excinfo:
-        meta_builder.check_inputs()
+        meta_builder._check_inputs()
     os.chdir(cwd)
     assert "Cannot find directory" in str(excinfo.value)
