81a114771f63bf2e8512ede5317abcb43d629536
fabianegli <fabian.egli@protonmail.ch>
2022-04-29 10:48:38 +0200

    adopt isort

--- nf_core/modules/update.py
+++ nf_core/modules/update.py
@@ -4,17 +4,20 @@ import enum
 import json
 import logging
 import os
-import questionary
 import shutil
 import tempfile
+
+import questionary
 from rich.console import Console
 from rich.syntax import Syntax
 
-import nf_core.utils
 import nf_core.modules.module_utils
+import nf_core.utils
 
+from .module_utils import get_installed_modules
+from .module_utils import get_module_git_log
+from .module_utils import module_exist_in_repo
 from .modules_command import ModuleCommand
-from .module_utils import get_installed_modules, get_module_git_log, module_exist_in_repo
 from .modules_repo import ModulesRepo
 
 log = logging.getLogger(__name__)
