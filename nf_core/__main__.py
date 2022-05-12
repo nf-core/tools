6ef8255b9fb56dffadad04642cc462addec75021
fabianegli <fabian.egli@protonmail.ch>
2022-05-12 09:50:03 +0200

    Fix wrong merge conflict resolution

--- nf_core/__main__.py
+++ nf_core/__main__.py
@@ -706,25 +706,6 @@ def mulled(specifications, build_number):
             "If it does not, please add your desired combination as detailed at:\n"
             "https://github.com/BioContainers/multi-package-containers\n"
         )
-
-
-# nf-core modules test
-@modules.command("test")
-@click.pass_context
-@click.argument("tool", type=str, required=False, metavar="<tool> or <tool/subtool>")
-@click.option("-p", "--no-prompts", is_flag=True, default=False, help="Use defaults without prompting")
-@click.option("-a", "--pytest_args", type=str, required=False, multiple=True, help="Additional pytest arguments")
-def test_module(ctx, tool, no_prompts, pytest_args):
-    """
-    Run module tests locally.
-
-    Given the name of a module, runs the Nextflow test command.
-    """
-    try:
-        meta_builder = nf_core.modules.ModulesTest(tool, no_prompts, pytest_args)
-        meta_builder.run()
-    except UserWarning as e:
-        log.critical(e)
         sys.exit(1)
 
 
