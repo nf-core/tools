1aa6b66bdd50d5034e84c4905f64b499c9c954ed
Júlia Mir Pedrol <julia.mir-pedrol@qbic.uni-tuebingen.de>
2022-05-12 10:53:35 +0200

    modify regex based on PR review

--- nf_core/modules/lint/main_nf.py
+++ nf_core/modules/lint/main_nf.py
@@ -196,7 +196,7 @@ def check_process_section(self, lines):
     correct_process_labels = ["process_low", "process_medium", "process_high", "process_long"]
     process_label = [l for l in lines if "label" in l]
     if len(process_label) > 0:
-        process_label = re.search("process_[A-Za-z]*", process_label[0]).group(0)
+        process_label = re.search("process_[A-Za-z]+", process_label[0]).group(0)
         if not process_label in correct_process_labels:
             self.warned.append(
                 (
@@ -212,26 +212,18 @@ def check_process_section(self, lines):
 
     for l in lines:
         l = l.strip()
-        l = l.replace('"', "")
-        l = l.replace("'", "")
+        l = l.lstrip('"')
+        l = l.lstrip("'")
         if re.search("bioconda::", l):
             bioconda_packages = [b for b in l.split() if "bioconda::" in b]
         if l.startswith("https://containers") or l.startswith("https://depot"):
-            lspl = l.lstrip("https://").split(":")
-            if len(lspl) == 2:
-                # e.g. 'https://containers.biocontainers.pro/s3/SingImgsRepo/biocontainers/v1.2.0_cv1/biocontainers_v1.2.0_cv1.img' :
-                if "YOUR-TOOL-HERE" not in lspl[0]:
-                    # Get singularity container version when a container is provided
-                    singularity_tag = re.search("biocontainers_(.*).img", lspl[0]).group(1)
-            else:
-                # e.g. 'https://depot.galaxyproject.org/singularity/fastqc:0.11.9--0' :
-                singularity_tag = lspl[-2].strip()
+            # e.g. 'https://containers.biocontainers.pro/s3/SingImgsRepo/biocontainers/v1.2.0_cv1/biocontainers_v1.2.0_cv1.img' :
+            # e.g. 'https://depot.galaxyproject.org/singularity/fastqc:0.11.9--0' :
+            singularity_tag = re.search("(?:\/)?(?:biocontainers_)?(?::)?([A-Za-z\d\-_\.]+)(?:\.img)?['\"]", l).group(1)
         if l.startswith("biocontainers/") or l.startswith("quay.io/"):
             # e.g. 'quay.io/biocontainers/krona:2.7.1--pl526_5' }"
             # e.g. 'biocontainers/biocontainers:v1.2.0_cv1' }"
-            if "YOUR-TOOL-HERE" not in l:
-                # Get docker container version when a container is provided
-                docker_tag = re.search("\:(.*) \}", l).group(1)
+            docker_tag = re.search("(?:[\/])?(?::)?([A-Za-z\d\-_\.]+)['\"]", l).group(1)
 
     # Check that all bioconda packages have build numbers
     # Also check for newer versions
