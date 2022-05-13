99535d3a9eb98fd25fe30910b1cd54f666e61a33
Phil Ewels <phil.ewels@scilifelab.se>
2022-05-13 12:23:45 +0200

    Richer linting output for modules

--- nf_core/modules/lint/__init__.py
+++ nf_core/modules/lint/__init__.py
@@ -390,46 +390,59 @@ class ModuleLint(ModuleCommand):
                 return "s"
             return ""
 
-        # Print module linting results header
-        console.print(Panel("[magenta]Module lint results"))
+        # Print blank line for spacing
+        console.print("")
 
         # Table of passed tests
         if len(self.passed) > 0 and show_passed:
-            console.print(
-                rich.panel.Panel(r"[!] {} Test{} Passed".format(len(self.passed), _s(self.passed)), style="bold green")
-            )
-            table = Table(style="green", box=rich.box.ROUNDED)
+            table = Table(style="green", box=rich.box.MINIMAL, pad_edge=False, border_style="dim")
             table.add_column("Module name", width=max_mod_name_len)
             table.add_column("File path")
             table.add_column("Test message")
             table = format_result(self.passed, table)
-            console.print(table)
+            console.print(
+                rich.panel.Panel(
+                    table,
+                    title=r"[bold][✔] {} Module Test{} Passed".format(len(self.passed), _s(self.passed)),
+                    title_align="left",
+                    style="green",
+                    padding=0,
+                )
+            )
 
         # Table of warning tests
         if len(self.warned) > 0:
-            console.print(
-                rich.panel.Panel(
-                    r"[!] {} Test Warning{}".format(len(self.warned), _s(self.warned)), style="bold yellow"
-                )
-            )
-            table = Table(style="yellow", box=rich.box.ROUNDED)
+            table = Table(style="yellow", box=rich.box.MINIMAL, pad_edge=False, border_style="dim")
             table.add_column("Module name", width=max_mod_name_len)
             table.add_column("File path")
             table.add_column("Test message")
             table = format_result(self.warned, table)
-            console.print(table)
+            console.print(
+                rich.panel.Panel(
+                    table,
+                    title=r"[bold][!] {} Module Test Warning{}".format(len(self.warned), _s(self.warned)),
+                    title_align="left",
+                    style="yellow",
+                    padding=0,
+                )
+            )
 
         # Table of failing tests
         if len(self.failed) > 0:
-            console.print(
-                rich.panel.Panel(r"[!] {} Test{} Failed".format(len(self.failed), _s(self.failed)), style="bold red")
-            )
-            table = Table(style="red", box=rich.box.ROUNDED)
+            table = Table(style="red", box=rich.box.MINIMAL, pad_edge=False, border_style="dim")
             table.add_column("Module name", width=max_mod_name_len)
             table.add_column("File path")
             table.add_column("Test message")
             table = format_result(self.failed, table)
-            console.print(table)
+            console.print(
+                rich.panel.Panel(
+                    table,
+                    title=r"[bold][✗] {} Module Test{} Failed".format(len(self.failed), _s(self.failed)),
+                    title_align="left",
+                    style="red",
+                    padding=0,
+                )
+            )
 
     def print_summary(self):
         def _s(some_list):
