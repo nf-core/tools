7009037c9e7a38021dd07b418b309abda1fc1410
Erik Danielsson <53212377+ErikDanielsson@users.noreply.github.com>
2022-07-09 17:23:55 +0200

    Apply suggestions from code review
    Co-authored-by: Phil Ewels <phil@seqera.io>

--- nf_core/__main__.py
+++ nf_core/__main__.py
@@ -271,8 +271,8 @@ def validate_wf_name_prompt(ctx, opts, value):
 @click.option(
     "-o", "--outdir", type=str, default=None, help="Output directory for new pipeline (default: pipeline name)"
 )
-@click.option("-t", "--template-yaml", type=str, default=None, help="Pass a YAML file to customize the template")
-@click.option("--plain", is_flag=True, default=False, help="Use the standard nf-core template")
+@click.option("-t", "--template-yaml", help="Pass a YAML file to customize the template")
+@click.option("--plain", is_flag=True, help="Use the standard nf-core template")
 def create(name, description, author, version, no_git, force, outdir, template_yaml, plain):
     """
     Create a new pipeline using the nf-core template.
