100d311fe0b2cbdf8312659a8ee0f978e237dcd1
fabianegli <fabian.egli@protonmail.ch>
2022-07-20 13:57:26 +0200

    fix variable name

--- nf_core/modules/update.py
+++ nf_core/modules/update.py
@@ -45,7 +45,7 @@ class ModuleUpdate(ModuleCommand):
         self.save_diff_fn = save_diff_fn
         self.module = None
         self.update_config = None
-        self.module_json = None
+        self.modules_json = None
 
     class DiffEnum(enum.Enum):
         """
