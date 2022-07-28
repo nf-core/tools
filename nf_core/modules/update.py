9020ae18a7616fdc93582a1bb0f1ae2f74f465ae
Erik Danielsson <danielsson.erik.0@gmail.com>
2022-07-28 13:54:29 +0200

    Improve update

--- nf_core/modules/update.py
+++ nf_core/modules/update.py
@@ -478,40 +478,40 @@ class ModuleUpdate(ModuleCommand):
         """
         Try applying a patch file to the new module files
 
+
         Args:
             module (str): The name of the module
             repo_name (str): The name of the repository where the module resides
-            patch_file (Path | str): The patch file that should be applied
+            patch_relpath (Path | str): The path to patch file in the pipeline
             module_dir (Path | str): The module directory in the pipeline
             module_install_dir (Path | str): The directory where the new module
                                              file have been installed
+
+        Returns:
+            (bool): Whether the patch application was successful
         """
-        patch_path = Path(self.dir / patch_relpath)
         module_fullname = str(Path(repo_name, module))
         log.info(f"Found patch for  module '{module_fullname}'. Trying to apply it to new files")
 
-        # Copy the installed files to a new temporary directory to preserve them if the patch fails
+        patch_path = Path(self.dir / patch_relpath)
+        module_relpath = Path("modules", repo_name, module)
+
+        # Copy the installed files to a new temporary directory to save them for later use
         temp_dir = Path(tempfile.mkdtemp())
         temp_module_dir = temp_dir / module
         shutil.copytree(module_install_dir, temp_module_dir)
 
-        # Write the new diff to a temp file
-        temp_patch = Path(tempfile.mktemp())
-        shutil.copy(patch_path, temp_patch)
-
-        module_relpath = Path("modules", repo_name, module)
-
-        patches = ModulesDiffer.per_file_patch(temp_patch)
+        patches = ModulesDiffer.per_file_patch(patch_path)
         new_files = {}
         for file, patch in patches.items():
             try:
-                log.debug(f"Applying patch to {Path(repo_name, module, file)}")
+                log.debug(f"Applying patch to {Path(module_fullname, file)}")
                 file_relpath = Path(file).relative_to(module_relpath)
                 file_path = temp_module_dir / file_relpath
                 patched_new_lines = ModulesDiffer.try_apply_patch(file_path, patch)
                 new_files[file_relpath] = "".join(patched_new_lines)
             except LookupError as e:
-                # Save the patch file by moving to the install dir
+                # Patch failed. Save the patch file by moving to the install dir
                 shutil.move(patch_path, Path(module_install_dir, patch_path.relative_to(module_dir)))
                 log.warning(
                     f"Failed to apply patch for module '{module_fullname}'. You will have to apply the patch manually"
@@ -519,6 +519,7 @@ class ModuleUpdate(ModuleCommand):
                 return False
 
         # Write the patched files to a temporary directory
+        log.debug("Writing patched files")
         for file, new_content in new_files.items():
             fn = temp_module_dir / file
             with open(fn, "w") as fh:
