256678d192d83c625ad9db9a60a70317115be95a
fabianegli <fabian.egli@protonmail.ch>
2022-08-09 18:05:45 +0200

    remove superfluous arguments

--- nf_core/utils.py
+++ nf_core/utils.py
@@ -746,12 +746,12 @@ def custom_yaml_dumper():
             """
             return self.represent_dict(data.items())
 
-        def increase_indent(self, *args, flow=False, **kwargs):
+        def increase_indent(self, flow=False, indentless=False):
             """Indent YAML lists so that YAML validates with Prettier
 
             See https://github.com/yaml/pyyaml/issues/234#issuecomment-765894586
             """
-            return super().increase_indent(flow=flow, indentless=False)
+            return super().increase_indent(flow=flow, indentless=indentless)
 
         # HACK: insert blank lines between top-level objects
         # inspired by https://stackoverflow.com/a/44284819/3786245
