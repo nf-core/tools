c7ac551bd18a3150659ab3d7587aaecc82fa6eaf
fabianegli <fabian.egli@protonmail.ch>
2022-08-18 17:33:41 +0200

    remove unnecessary print statements form tests

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -47,7 +47,6 @@ class TestModules(unittest.TestCase):
             "mypipeline", "it is mine", "me", no_git=True, outdir=self.pipeline_dir, plain=True
         ).init_pipeline()
         # Set up install objects
-        print("Setting up install objects")
         self.mods_install = nf_core.modules.ModuleInstall(self.pipeline_dir, prompt=False, force=True)
         self.mods_install_alt = nf_core.modules.ModuleInstall(self.pipeline_dir, prompt=True, force=True)
         self.mods_install_old = nf_core.modules.ModuleInstall(
@@ -58,7 +57,6 @@ class TestModules(unittest.TestCase):
         )
 
         # Set up remove objects
-        print("Setting up remove objects")
         self.mods_remove = nf_core.modules.ModuleRemove(self.pipeline_dir)
         self.mods_remove_alt = nf_core.modules.ModuleRemove(self.pipeline_dir)
 
