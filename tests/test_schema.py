d7c145f4ad03999fd289b9a6f2df4258922d418c
mirpedrol <mirp.julia@gmail.com>
2022-09-08 16:12:09 +0200

    remove underscore from testpipeline name in tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -31,7 +31,7 @@ class TestSchema(unittest.TestCase):
         self.tmp_dir = tempfile.mkdtemp()
         self.template_dir = os.path.join(self.tmp_dir, "wf")
         create_obj = nf_core.create.PipelineCreate(
-            "test_pipeline", "", "", outdir=self.template_dir, no_git=True, plain=True
+            "testpipeline", "", "", outdir=self.template_dir, no_git=True, plain=True
         )
         create_obj.init_pipeline()
 
