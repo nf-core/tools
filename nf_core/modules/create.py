c1210efa887f6b32f5c7ef794e049471b7c25ae9
Harshil Patel <drpatelhh@gmail.com>
2022-10-04 12:29:45 +0100

    Make label:process_single default when creating a new module

--- nf_core/modules/create.py
+++ nf_core/modules/create.py
@@ -216,7 +216,7 @@ class ModuleCreate(ModuleCommand):
                 default=author_default,
             )
 
-        process_label_defaults = ["process_low", "process_medium", "process_high", "process_long"]
+        process_label_defaults = ["process_single", "process_low", "process_medium", "process_high", "process_long"]
         if self.process_label is None:
             log.info(
                 "Provide an appropriate resource label for the process, taken from the "
