dd5eb6f270801a1ccae5b7c0ab592c0e4e5ad55f
Júlia Mir Pedrol <mirp.julia@gmail.com>
2022-10-04 17:52:26 +0200

    Update comments from code review
    Co-authored-by: Phil Ewels <phil.ewels@scilifelab.se>

--- nf_core/modules/modules_command.py
+++ nf_core/modules/modules_command.py
@@ -151,7 +151,10 @@ class ModuleCommand:
     def check_modules_structure(self):
         """
         Check that the structure of the modules directory in a pipeline is the correct one:
-            'modules/nf-core/TOOL/SUBTOOL
+            modules/nf-core/TOOL/SUBTOOL
+
+        Prior to nf-core/tools release 2.6 the directory structure had an additional level of nesting:
+            modules/nf-core/modules/TOOL/SUBTOOL
         """
         if self.repo_type == "pipeline":
             wrong_location_modules = []
