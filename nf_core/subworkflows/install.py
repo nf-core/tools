c163bb3e8b5769e87a3248dfd0653ee71b737a21
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2022-10-11 12:53:02 +0200

    Move log statement to be after the subworkflow include log statement

--- nf_core/subworkflows/install.py
+++ nf_core/subworkflows/install.py
@@ -169,6 +169,11 @@ class SubworkflowInstall(object):
         log.info(
             f"Include statement: include {{ {subworkflow_name} }} from '.{os.path.join(install_folder, subworkflow)}/main'"
         )
+        subworkflow_config = os.path.join(install_folder, subworkflow, "nextflow.config")
+        if os.path.isfile(subworkflow_config):
+            log.info(
+                f"Subworkflow config include statement: includeConfig '{subworkflow_config}'"
+            )
 
         # Update module.json with newly installed subworkflow
         modules_json.update_subworkflow(self.modules_repo, subworkflow, version)
