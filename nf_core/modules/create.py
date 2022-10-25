2b164ed34c3e2cdbee61f3da3aed55c70d0ff846
mirpedrol <mirp.julia@gmail.com>
2022-10-25 16:27:29 +0200

    poc for shared functions in components commands

--- nf_core/modules/create.py
+++ nf_core/modules/create.py
@@ -5,20 +5,16 @@ The ModuleCreate class handles generating of module templates
 
 from __future__ import print_function
 
-import glob
-import json
 import logging
 import os
-import re
-import subprocess
 
-import jinja2
 import questionary
 import rich
 import yaml
 from packaging.version import parse as parse_version
 
 import nf_core
+import nf_core.components.components_create
 import nf_core.modules.module_utils
 import nf_core.utils
 
@@ -106,31 +102,9 @@ class ModuleCreate(ModuleCommand):
         )
 
         # Collect module info via prompt if empty or invalid
-        if self.tool is None:
-            self.tool = ""
-        while self.tool == "" or re.search(r"[^a-z\d/]", self.tool) or self.tool.count("/") > 0:
-
-            # Check + auto-fix for invalid chacters
-            if re.search(r"[^a-z\d/]", self.tool):
-                log.warning("Tool/subtool name must be lower-case letters only, with no punctuation")
-                tool_clean = re.sub(r"[^a-z\d/]", "", self.tool.lower())
-                if rich.prompt.Confirm.ask(f"[violet]Change '{self.tool}' to '{tool_clean}'?"):
-                    self.tool = tool_clean
-                else:
-                    self.tool = ""
-
-            # Split into tool and subtool
-            if self.tool.count("/") > 1:
-                log.warning("Tool/subtool can have maximum one '/' character")
-                self.tool = ""
-            elif self.tool.count("/") == 1:
-                self.tool, self.subtool = self.tool.split("/")
-            else:
-                self.subtool = None  # Reset edge case: entered '/subtool' as name and gone round loop again
-
-            # Prompt for new entry if we reset
-            if self.tool == "":
-                self.tool = rich.prompt.Prompt.ask("[violet]Name of tool/subtool").strip()
+        self.tool, self.subtool = nf_core.components.components_create.collect_name_prompt(
+            self.tool, self.component_type
+        )
 
         # Determine the tool name
         self.tool_name = self.tool
@@ -143,9 +117,58 @@ class ModuleCreate(ModuleCommand):
         self.tool_name_underscore = self.tool_name.replace("/", "_")
 
         # Check existence of directories early for fast-fail
-        self.file_paths = self.get_module_dirs()
+        self.file_paths = nf_core.components.components_create.get_component_dirs(
+            self.component_type,
+            self.repo_type,
+            self.directory,
+            self.tool_name,
+            self.tool,
+            self.subtool,
+            self.tool_dir,
+            self.force_overwrite,
+        )
 
         # Try to find a bioconda package for 'tool'
+        self._get_bioconda_tool()
+
+        # Prompt for GitHub username
+        nf_core.components.components_create.get_username(self.author)
+
+        self._get_module_structure_components()
+
+        # Create module template with jinja2
+        nf_core.components.components_create.render_template(self.component_type, vars(self), self.file_paths)
+
+        if self.repo_type == "modules":
+            # Add entry to pytest_modules.yml
+            try:
+                with open(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"), "r") as fh:
+                    pytest_modules_yml = yaml.safe_load(fh)
+                if self.subtool:
+                    pytest_modules_yml[self.tool_name] = [
+                        f"modules/nf-core/{self.tool}/{self.subtool}/**",
+                        f"tests/modules/nf-core/{self.tool}/{self.subtool}/**",
+                    ]
+                else:
+                    pytest_modules_yml[self.tool_name] = [
+                        f"modules/nf-core/{self.tool}/**",
+                        f"tests/modules/nf-core/{self.tool}/**",
+                    ]
+                pytest_modules_yml = dict(sorted(pytest_modules_yml.items()))
+                with open(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"), "w") as fh:
+                    yaml.dump(pytest_modules_yml, fh, sort_keys=True, Dumper=nf_core.utils.custom_yaml_dumper())
+            except FileNotFoundError as e:
+                raise UserWarning("Could not open 'tests/config/pytest_modules.yml' file!")
+
+        new_files = list(self.file_paths.values())
+        if self.repo_type == "modules":
+            new_files.append(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"))
+        log.info("Created / edited following files:\n  " + "\n  ".join(new_files))
+
+    def _get_bioconda_tool(self):
+        """
+        Try to find a bioconda package for 'tool'
+        """
         while True:
             try:
                 if self.tool_conda_name:
@@ -199,26 +222,7 @@ class ModuleCreate(ModuleCommand):
             except (ValueError, LookupError) as e:
                 log.info(f"Could not find a Docker/Singularity container ({e})")
 
-        # Prompt for GitHub username
-        # Try to guess the current user if `gh` is installed
-        author_default = None
-        try:
-            with open(os.devnull, "w") as devnull:
-                gh_auth_user = json.loads(subprocess.check_output(["gh", "api", "/user"], stderr=devnull))
-            author_default = f"@{gh_auth_user['login']}"
-        except Exception as e:
-            log.debug(f"Could not find GitHub username using 'gh' cli command: [red]{e}")
-
-        # Regex to valid GitHub username: https://github.com/shinnn/github-username-regex
-        github_username_regex = re.compile(r"^@[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38}$")
-        while self.author is None or not github_username_regex.match(self.author):
-            if self.author is not None and not github_username_regex.match(self.author):
-                log.warning("Does not look like a valid GitHub username (must start with an '@')!")
-            self.author = rich.prompt.Prompt.ask(
-                f"[violet]GitHub Username:[/]{' (@author)' if author_default is None else ''}",
-                default=author_default,
-            )
-
+    def _get_module_structure_components(self):
         process_label_defaults = ["process_single", "process_low", "process_medium", "process_high", "process_long"]
         if self.process_label is None:
             log.info(
@@ -245,124 +249,3 @@ class ModuleCreate(ModuleCommand):
             self.has_meta = rich.prompt.Confirm.ask(
                 "[violet]Will the module require a meta map of sample information?", default=True
             )
-
-        # Create module template with jinja
-        self.render_template()
-
-        if self.repo_type == "modules":
-            # Add entry to pytest_modules.yml
-            try:
-                with open(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"), "r") as fh:
-                    pytest_modules_yml = yaml.safe_load(fh)
-                if self.subtool:
-                    pytest_modules_yml[self.tool_name] = [
-                        f"modules/nf-core/{self.tool}/{self.subtool}/**",
-                        f"tests/modules/nf-core/{self.tool}/{self.subtool}/**",
-                    ]
-                else:
-                    pytest_modules_yml[self.tool_name] = [
-                        f"modules/nf-core/{self.tool}/**",
-                        f"tests/modules/nf-core/{self.tool}/**",
-                    ]
-                pytest_modules_yml = dict(sorted(pytest_modules_yml.items()))
-                with open(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"), "w") as fh:
-                    yaml.dump(pytest_modules_yml, fh, sort_keys=True, Dumper=nf_core.utils.custom_yaml_dumper())
-            except FileNotFoundError as e:
-                raise UserWarning("Could not open 'tests/config/pytest_modules.yml' file!")
-
-        new_files = list(self.file_paths.values())
-        if self.repo_type == "modules":
-            new_files.append(os.path.join(self.directory, "tests", "config", "pytest_modules.yml"))
-        log.info("Created / edited following files:\n  " + "\n  ".join(new_files))
-
-    def render_template(self):
-        """
-        Create new module files with Jinja2.
-        """
-        # Run jinja2 for each file in the template folder
-        env = jinja2.Environment(loader=jinja2.PackageLoader("nf_core", "module-template"), keep_trailing_newline=True)
-        for template_fn, dest_fn in self.file_paths.items():
-            log.debug(f"Rendering template file: '{template_fn}'")
-            j_template = env.get_template(template_fn)
-            object_attrs = vars(self)
-            object_attrs["nf_core_version"] = nf_core.__version__
-            rendered_output = j_template.render(object_attrs)
-
-            # Write output to the target file
-            os.makedirs(os.path.dirname(dest_fn), exist_ok=True)
-            with open(dest_fn, "w") as fh:
-                log.debug(f"Writing output to: '{dest_fn}'")
-                fh.write(rendered_output)
-
-            # Mirror file permissions
-            template_stat = os.stat(os.path.join(os.path.dirname(nf_core.__file__), "module-template", template_fn))
-            os.chmod(dest_fn, template_stat.st_mode)
-
-    def get_module_dirs(self):
-        """Given a directory and a tool/subtool, set the file paths and check if they already exist
-
-        Returns dict: keys are relative paths to template files, vals are target paths.
-        """
-
-        file_paths = {}
-
-        if self.repo_type == "pipeline":
-            local_modules_dir = os.path.join(self.directory, "modules", "local")
-
-            # Check whether module file already exists
-            module_file = os.path.join(local_modules_dir, f"{self.tool_name}.nf")
-            if os.path.exists(module_file) and not self.force_overwrite:
-                raise UserWarning(f"Module file exists already: '{module_file}'. Use '--force' to overwrite")
-
-            # If a subtool, check if there is a module called the base tool name already
-            if self.subtool and os.path.exists(os.path.join(local_modules_dir, f"{self.tool}.nf")):
-                raise UserWarning(f"Module '{self.tool}' exists already, cannot make subtool '{self.tool_name}'")
-
-            # If no subtool, check that there isn't already a tool/subtool
-            tool_glob = glob.glob(f"{local_modules_dir}/{self.tool}_*.nf")
-            if not self.subtool and tool_glob:
-                raise UserWarning(
-                    f"Module subtool '{tool_glob[0]}' exists already, cannot make tool '{self.tool_name}'"
-                )
-
-            # Set file paths
-            file_paths[os.path.join("modules", "main.nf")] = module_file
-
-        if self.repo_type == "modules":
-            software_dir = os.path.join(self.directory, self.default_modules_path, self.tool_dir)
-            test_dir = os.path.join(self.directory, self.default_tests_path, self.tool_dir)
-
-            # Check if module directories exist already
-            if os.path.exists(software_dir) and not self.force_overwrite:
-                raise UserWarning(f"Module directory exists: '{software_dir}'. Use '--force' to overwrite")
-
-            if os.path.exists(test_dir) and not self.force_overwrite:
-                raise UserWarning(f"Module test directory exists: '{test_dir}'. Use '--force' to overwrite")
-
-            # If a subtool, check if there is a module called the base tool name already
-            parent_tool_main_nf = os.path.join(self.directory, self.default_modules_path, self.tool, "main.nf")
-            parent_tool_test_nf = os.path.join(self.directory, self.default_tests_path, self.tool, "main.nf")
-            if self.subtool and os.path.exists(parent_tool_main_nf):
-                raise UserWarning(
-                    f"Module '{parent_tool_main_nf}' exists already, cannot make subtool '{self.tool_name}'"
-                )
-            if self.subtool and os.path.exists(parent_tool_test_nf):
-                raise UserWarning(
-                    f"Module '{parent_tool_test_nf}' exists already, cannot make subtool '{self.tool_name}'"
-                )
-
-            # If no subtool, check that there isn't already a tool/subtool
-            tool_glob = glob.glob(f"{os.path.join(self.directory, self.default_modules_path, self.tool)}/*/main.nf")
-            if not self.subtool and tool_glob:
-                raise UserWarning(
-                    f"Module subtool '{tool_glob[0]}' exists already, cannot make tool '{self.tool_name}'"
-                )
-
-            # Set file paths - can be tool/ or tool/subtool/ so can't do in template directory structure
-            file_paths[os.path.join("modules", "main.nf")] = os.path.join(software_dir, "main.nf")
-            file_paths[os.path.join("modules", "meta.yml")] = os.path.join(software_dir, "meta.yml")
-            file_paths[os.path.join("tests", "main.nf")] = os.path.join(test_dir, "main.nf")
-            file_paths[os.path.join("tests", "test.yml")] = os.path.join(test_dir, "test.yml")
-            file_paths[os.path.join("tests", "nextflow.config")] = os.path.join(test_dir, "nextflow.config")
-
-        return file_paths
