f289a69f508ef36c851ed41ac4f9952bfb59b45b
mashehu <mashehu3@gmail.com>
2022-11-03 17:30:53 +0100

    add test for sw list

--- tests/test_subworkflows.py
+++ tests/test_subworkflows.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
 """ Tests covering the subworkflows commands
 """
 
 import os
-import shutil
 import tempfile
 import unittest
 
@@ -13,10 +11,10 @@ import nf_core.create
 import nf_core.modules
 import nf_core.subworkflows
 
-from .utils import GITLAB_URL, mock_api_calls
+from .utils import GITLAB_SUBWORKFLOWS_BRANCH, GITLAB_URL, mock_api_calls
 
 
-def create_modules_repo_dummy(tmp_dir):
+def create_subworkflows_repo_dummy(tmp_dir):
     """Create a dummy copy of the nf-core/modules repo"""
 
     root_dir = os.path.join(tmp_dir, "modules")
@@ -32,10 +30,8 @@ def create_modules_repo_dummy(tmp_dir):
         fh.writelines(["repository_type: modules", "\n"])
 
     with requests_mock.Mocker() as mock:
-        mock_api_calls(mock, "bpipe", "0.9.11")
-        # bpipe is a valid package on bioconda that is very unlikely to ever be added to nf-core/modules
-        module_create = nf_core.modules.ModuleCreate(root_dir, "bpipe/test", "@author", "process_medium", False, False)
-        module_create.create()
+        subworkflow_create = nf_core.subworkflows.SubworkflowCreate(root_dir, "test_subworkflow", "@author", True)
+        subworkflow_create.create()
 
     return root_dir
 
@@ -56,7 +52,13 @@ class TestSubworkflows(unittest.TestCase):
         ).init_pipeline()
 
         # Set up the nf-core/modules repo dummy
-        self.nfcore_modules = create_modules_repo_dummy(self.tmp_dir)
+        self.nfcore_modules = create_subworkflows_repo_dummy(self.tmp_dir)
+
+        # Set up install objects
+        self.sw_install = nf_core.subworkflows.SubworkflowInstall(self.pipeline_dir, prompt=False, force=False)
+        self.sw_install_gitlab = nf_core.subworkflows.SubworkflowInstall(
+            self.pipeline_dir, prompt=False, force=False, remote_url=GITLAB_URL, branch=GITLAB_SUBWORKFLOWS_BRANCH
+        )
 
     ############################################
     # Test of the individual modules commands. #
@@ -66,6 +68,12 @@ class TestSubworkflows(unittest.TestCase):
         test_subworkflows_create_fail_exists,
         test_subworkflows_create_nfcore_modules,
         test_subworkflows_create_succeed,
+    )
+    from .subworkflows.list import (
+        test_subworkflows_install_and_list_subworkflows,
+        test_subworkflows_install_gitlab_and_list_subworkflows,
+        test_subworkflows_list_remote,
+        test_subworkflows_list_remote_gitlab,
     )
     from .subworkflows.subworkflows_test import (
         test_subworkflows_test_check_inputs,
