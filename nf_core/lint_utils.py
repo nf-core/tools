a2dd8032bb8269cd1292e809593e3042d8f9a57c
fabianegli <fabian.egli@protonmail.ch>
2022-12-03 18:47:46 +0100

    add else after changing from raise to loggging

--- nf_core/lint_utils.py
+++ nf_core/lint_utils.py
@@ -73,7 +73,8 @@ def run_prettier_on_file(file):
     except subprocess.CalledProcessError as e:
         if ": SyntaxError: " in e.stdout.decode():
             log.critical(f"Can't format {file} because it has a syntax error.\n{e.stdout.decode()}")
-        log.warning(
-            "There was an error running the prettier pre-commit hook.\n"
-            f"STDOUT: {e.stdout.decode()}\nSTDERR: {e.stderr.decode()}"
-        )
+        else:
+            log.warning(
+                "There was an error running the prettier pre-commit hook.\n"
+                f"STDOUT: {e.stdout.decode()}\nSTDERR: {e.stderr.decode()}"
+            )
