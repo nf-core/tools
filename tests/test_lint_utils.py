c69583580e4aa26347dcd9306b636f765357aed8
Fabian Egli <fabianegli@users.noreply.github.com>
2022-12-05 13:49:56 +0100

    fix typo

--- tests/test_lint_utils.py
+++ tests/test_lint_utils.py
@@ -60,6 +60,6 @@ def test_run_prettier_on_malformed_file(malformed_json):
 
 
 def test_run_prettier_on_syntax_error_file(syntax_error_json, caplog):
-    nf_core.lint_utils.run_prettier_on_file(synthax_error_json)
+    nf_core.lint_utils.run_prettier_on_file(syntax_error_json)
     expected_critical_log = "SyntaxError: Unexpected token (1:10)"
     assert expected_critical_log in caplog.text
