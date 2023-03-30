f9ea217544cf6942fa39d8b15b702b1c061e0f7f
Anne Marie Noronha <anoronh4@users.noreply.github.com>
2023-03-30 12:57:17 -0400

    Simplified logic for returning True/False in check

--- nf_core/components/install.py
+++ nf_core/components/install.py
@@ -279,13 +279,9 @@ class ComponentInstall(ComponentCommand):
             True: if problematic components are found
             False: if problematic components are not found
         """
-        alternate_remotes = False
         modules_json.load()
         for repo_url, repo_content in modules_json.modules_json.get("repos", dict()).items():
             for dir in repo_content.get(self.component_type, dict()).keys():
                 if dir == self.org and repo_url != self.modules_repo.remote_url:
-                    alternate_remotes = True
-        if alternate_remotes:
-            return True
-        else:
-            return False
+                    return True
+        return False
