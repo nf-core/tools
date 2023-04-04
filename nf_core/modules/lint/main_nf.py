2915f2ebd4353b4be5f67e4a1c1b0a98c55234d9
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2023-04-04 12:15:34 +0100

    Fix indentation so that warning logic is outside loop and not repeated for every label

--- nf_core/modules/lint/main_nf.py
+++ nf_core/modules/lint/main_nf.py
@@ -246,30 +246,30 @@ def check_process_section(self, lines, fix_version, progress_bar):
                 bad_labels.append(label)
             else:
                 good_labels.append(label)
-            if len(good_labels) > 1:
-                self.warned.append(
-                    (
-                        "process_standard_label",
-                        f"Conflicting process labels found: `{'`,`'.join(good_labels)}`",
-                        self.main_nf,
-                    )
+        if len(good_labels) > 1:
+            self.warned.append(
+                (
+                    "process_standard_label",
+                    f"Conflicting process labels found: `{'`,`'.join(good_labels)}`",
+                    self.main_nf,
                 )
-            elif len(good_labels) == 1:
-                self.passed.append(("process_standard_label", "Correct process label", self.main_nf))
-            else:
-                self.warned.append(("process_standard_label", "Standard process label not found", self.main_nf))
-            if len(bad_labels) > 0:
-                self.warned.append(
-                    ("process_standard_label", f"Non-standard labels found: `{'`,`'.join(good_labels)}`", self.main_nf)
+            )
+        elif len(good_labels) == 1:
+            self.passed.append(("process_standard_label", "Correct process label", self.main_nf))
+        else:
+            self.warned.append(("process_standard_label", "Standard process label not found", self.main_nf))
+        if len(bad_labels) > 0:
+            self.warned.append(
+                ("process_standard_label", f"Non-standard labels found: `{'`,`'.join(good_labels)}`", self.main_nf)
+            )
+        if len(all_labels) > len(set(all_labels)):
+            self.warned.append(
+                (
+                    "process_standard_label",
+                    f"Duplicate labels found: `{'`,`'.join(sorted(all_labels))}`",
+                    self.main_nf,
                 )
-            if len(all_labels) > len(set(all_labels)):
-                self.warned.append(
-                    (
-                        "process_standard_label",
-                        f"Duplicate labels found: `{'`,`'.join(sorted(all_labels))}`",
-                        self.main_nf,
-                    )
-                )
+            )
 
     else:
         self.warned.append(("process_standard_label", "Process label not specified", self.main_nf))
