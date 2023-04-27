e21f2a23800f3f7b293e2ff07842bf9ead560ac4
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-04-27 20:12:15 +0200

    Writing additional tests for the --tower download functionality.

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,16 +3,20 @@
 
 import hashlib
 import os
+import re
 import shutil
 import tempfile
 import unittest
+from pathlib import Path
 from unittest import mock
 
 import pytest
 
 import nf_core.create
 import nf_core.utils
-from nf_core.download import DownloadWorkflow
+from nf_core.download import DownloadWorkflow, WorkflowRepo
+from nf_core.synced_repo import SyncedRepo
+from nf_core.utils import NFCORE_CACHE_DIR, NFCORE_DIR
 
 from .utils import with_temporary_file, with_temporary_folder
 
@@ -170,6 +174,32 @@ class DownloadTest(unittest.TestCase):
             download_obj.singularity_pull_image("a-container", tmp_dir, None, mock_rich_progress)
 
     #
+    # Test for '--singularity-cache remote --singularity-cache-index'. Provide a list of containers already available in a remote location.
+    #
+    @with_temporary_folder
+    def test_remote_container_functionality(self, tmp_dir):
+        os.environ["NXF_SINGULARITY_CACHEDIR"] = "foo"
+
+        download_obj = DownloadWorkflow(
+            pipeline="nf-core/rnaseq",
+            outdir=os.path.join(tmp_dir, "new"),
+            revision="3.9",
+            compress_type="none",
+            singularity_cache_index=Path(__file__).resolve().parent / "data/testdata_remote_containers.txt",
+        )
+
+        download_obj.include_configs = False  # suppress prompt, because stderr.is_interactive doesn't.
+
+        # test if settings are changed accordingly.
+        assert download_obj.singularity_cache == "remote" and download_obj.container == "singularity"
+        assert isinstance(download_obj.containers_remote, list) and len(download_obj.containers_remote) == 0
+        # read in the file
+        download_obj.read_remote_containers()
+        assert len(download_obj.containers_remote) == 33
+        assert "depot.galaxyproject.org-singularity-salmon-1.5.2--h84f40af_0.img" in download_obj.containers_remote
+        assert "MV Rena" not in download_obj.containers_remote  # decoy in test file
+
+    #
     # Tests for the main entry method 'download_workflow'
     #
     @with_temporary_folder
@@ -184,6 +214,65 @@ class DownloadTest(unittest.TestCase):
             container="singularity",
             revision="1.6",
             compress_type="none",
+            singularity_cache="copy",
         )
 
+        download_obj.include_configs = True  # suppress prompt, because stderr.is_interactive doesn't.
         download_obj.download_workflow()
+
+    #
+    # Test Download for Tower
+    #
+    @with_temporary_folder
+    def test_download_workflow_for_tower(self, tmp_dir):
+        download_obj = DownloadWorkflow(
+            pipeline="nf-core/rnaseq",
+            revision=("3.7", "3.9"),
+            compress_type="none",
+            tower=True,
+        )
+
+        download_obj.include_configs = False  # suppress prompt, because stderr.is_interactive doesn't.
+
+        assert isinstance(download_obj.revision, list) and len(download_obj.revision) == 2
+        assert isinstance(download_obj.wf_sha, dict) and len(download_obj.wf_sha) == 0
+        assert isinstance(download_obj.wf_download_url, dict) and len(download_obj.wf_download_url) == 0
+
+        wfs = nf_core.list.Workflows()
+        wfs.get_remote_workflows()
+        (
+            download_obj.pipeline,
+            download_obj.wf_revisions,
+            download_obj.wf_branches,
+        ) = nf_core.utils.get_repo_releases_branches(download_obj.pipeline, wfs)
+
+        download_obj.get_revision_hash()
+
+        # download_obj.wf_download_url is not set for tower downloads, but the sha values are
+        assert isinstance(download_obj.wf_sha, dict) and len(download_obj.wf_sha) == 2
+        assert isinstance(download_obj.wf_download_url, dict) and len(download_obj.wf_download_url) == 0
+
+        # The outdir for multiple revisions is the pipeline name and date: e.g. nf-core-rnaseq_2023-04-27_18-54
+        assert bool(re.search(r"nf-core-rnaseq_\d{4}-\d{2}-\d{1,2}_\d{1,2}-\d{1,2}", download_obj.outdir, re.S))
+
+        download_obj.output_filename = f"{download_obj.outdir}.git"
+        download_obj.download_workflow_tower(location=tmp_dir)
+
+        assert download_obj.workflow_repo
+        assert isinstance(download_obj.workflow_repo, WorkflowRepo)
+        assert issubclass(type(download_obj.workflow_repo), SyncedRepo)
+        # corroborate that the other revisions are inaccessible to the user.
+        assert len(download_obj.workflow_repo.tags) == len(download_obj.revision)
+
+        # manually test container image detection for 3.7 revision
+        download_obj.workflow_repo.checkout(download_obj.wf_sha["3.7"])
+        assert isinstance(download_obj.containers, list) and len(download_obj.containers) == 0
+        download_obj.find_container_images(download_obj.workflow_repo.access())
+        assert len(download_obj.containers) == 30  # 30 containers for 3.7
+        assert (
+            "https://depot.galaxyproject.org/singularity/bbmap:38.93--he522d1c_0" in download_obj.containers
+        )  # direct definition
+        assert (
+            "https://depot.galaxyproject.org/singularity/mulled-v2-1fa26d1ce03c295fe2fdcf85831a92fbcbd7e8c2:59cdd445419f14abac76b31dd0d71217994cbcc9-0"
+            in download_obj.containers
+        )  # indirect definition via $container variable.
