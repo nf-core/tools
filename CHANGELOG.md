6a2adc15c72c500fa3297a1553d633d2bffc3eb6
Adam Talbot <adam.talbot@seqera.io>
2023-06-13 16:42:06 +0100

    feat (2311): Infer docker.registry value from nextflow.config
    Fixes #2311

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@
 - Fix link in the MultiQC report to point to exact version of output docs ([#2298](https://github.com/nf-core/tools/pull/2298))
 - Fix parsing of container directive when it is not typical nf-core format ([#2306](https://github.com/nf-core/tools/pull/2306))
 - Add ability to specify custom registry for linting modules, defaults to quay.io ([#2313](https://github.com/nf-core/tools/pull/2313))
+- Add ability to interpret `docker.registry` from `nextflow.config` file. If not found defaults to quay.io.
 
 ### Download
 
--- nf_core/__main__.py
+++ nf_core/__main__.py
@@ -808,7 +808,12 @@ def create_test_yml(ctx, tool, run_tests, output, force, no_prompts):
 @click.argument("tool", type=str, required=False, metavar="<tool> or <tool/subtool>")
 @click.option("-d", "--dir", type=click.Path(exists=True), default=".", metavar="<pipeline/modules directory>")
 @click.option(
-    "-r", "--registry", type=str, metavar="<registry>", default="quay.io", help="Registry to use for containers"
+    "-r",
+    "--registry",
+    type=str,
+    metavar="<registry>",
+    default=None,
+    help="Registry to use for containers. If not specified will interpret it from nextflow.config file at attribute docker.registry",
 )
 @click.option("-k", "--key", type=str, metavar="<test>", multiple=True, help="Run only these lint tests")
 @click.option("-a", "--all", is_flag=True, help="Run on all modules")
@@ -842,6 +847,7 @@ def lint(
         module_lint = ModuleLint(
             dir,
             fail_warned=fail_warned,
+            registry=ctx.params["registry"],
             remote_url=ctx.obj["modules_repo_url"],
             branch=ctx.obj["modules_repo_branch"],
             no_pull=ctx.obj["modules_repo_no_pull"],
@@ -849,7 +855,6 @@ def lint(
         )
         module_lint.lint(
             module=tool,
-            registry=registry,
             key=key,
             all_modules=all,
             print_results=True,
