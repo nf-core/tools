91a3b654010946a8d927540e6a9a68f5e9e6dde9
James A. Fellows Yates <jfy133@gmail.com>
2023-07-13 11:01:06 +0200

    Fix link to point to new website page

--- nf_core/sync.py
+++ nf_core/sync.py
@@ -338,7 +338,7 @@ class PipelineSync:
             f"resolving any merge conflicts in the `{self.merge_branch}` branch (or your own fork, if you prefer). "
             "Once complete, make a new minor release of your pipeline.\n\n"
             "For instructions on how to merge this PR, please see "
-            "[https://nf-co.re/developers/sync](https://nf-co.re/developers/sync#merging-automated-prs).\n\n"
+            "[https://nf-co.re/docs/contributing/sync/](https://nf-co.re/docs/contributing/sync/#merging-automated-prs).\n\n"
             "For more information about this release of [nf-core/tools](https://github.com/nf-core/tools), "
             "please see the `v{tag}` [release page](https://github.com/nf-core/tools/releases/tag/{tag})."
         ).format(tag=nf_core.__version__)
