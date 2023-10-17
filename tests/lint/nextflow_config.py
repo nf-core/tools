0e33bf0d524cf26ea66bb9692650e53fdd771ca4
Marius Bjørnstad <pmb@fa2k.net>
2023-10-17 14:10:09 +0200

    Add a test for the test

--- tests/lint/nextflow_config.py
+++ tests/lint/nextflow_config.py
@@ -1,6 +1,7 @@
 import nf_core.create
 import nf_core.lint
-
+import subprocess
+from unittest import mock
 
 def test_nextflow_config_example_pass(self):
     """Tests that config variable existence test works with good pipeline example"""
@@ -31,5 +32,20 @@ def test_nextflow_config_dev_in_release_mode_failed(self):
     lint_obj.release_mode = True
     lint_obj.nf_config["manifest.version"] = "dev_is_bad_name"
     result = lint_obj.nextflow_config()
+    assert len(result["failed"]) > 0
+    assert len(result["warned"]) == 0
+
+@mock.patch("subprocess.run")
+def test_nextflow_config_missing_test_profile_failed(self, mock_subprocess):
+    """Tests that the test fails if nextflow command with `-profile test` exits
+    with exit code 1."""
+    self.lint_obj._load_pipeline_config()
+    with mock.patch("subprocess.run") as mock_subprocess:
+        mock_subprocess.return_value = mock.Mock(returncode=1)
+        result = self.lint_obj.nextflow_config()
+        mock_subprocess.assert_called_once_with(
+            ["nextflow", "config", "-flat", "-profile", "test"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+
     assert len(result["failed"]) > 0
     assert len(result["warned"]) == 0
