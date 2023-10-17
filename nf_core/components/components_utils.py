7edf84fa278be7880e8a535591c1e3d344e57bf4
Alfred Kedhammar <89784800+kedhammar@users.noreply.github.com>
2023-10-17 16:04:57 +0200

    Update nf_core/components/components_utils.py
    Change typing based on nf_core.utils.determine_base_dir() outputs. A bit messy, but let's not increase the scope further.
    
    Co-authored-by: Matthias Hörtenhuber <mashehu@users.noreply.github.com>

--- nf_core/components/components_utils.py
+++ nf_core/components/components_utils.py
@@ -23,7 +23,7 @@ def get_repo_info(directory: str, use_prompt: Optional[bool] = True) -> Tuple[st
         raise UserWarning(f"Could not find directory: {directory}")
 
     # Try to find the root directory
-    base_dir: str = nf_core.utils.determine_base_dir(directory)
+    base_dir: Union[str, Path] = nf_core.utils.determine_base_dir(directory)
 
     # Figure out the repository type from the .nf-core.yml config file if we can
     config_fn, tools_config = nf_core.utils.load_tools_config(base_dir)
