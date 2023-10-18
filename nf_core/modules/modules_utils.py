72b46715bd73a7d51a570f7b028160ec3373dcf8
kedhammar <alfred.kedhammar@scilifelab.se>
2023-10-18 11:41:52 +0200

    isort

--- nf_core/modules/modules_utils.py
+++ nf_core/modules/modules_utils.py
@@ -1,8 +1,8 @@
 import logging
 import os
-from urllib.parse import urlparse
 from pathlib import Path
 from typing import List, Optional, Tuple
+from urllib.parse import urlparse
 
 from ..components.nfcore_component import NFCoreComponent
 
