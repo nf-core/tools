6b0ff4992a9ce9baec1dac1ff548282ea222d42c
mashehu <mashehu3@gmail.com>
2023-11-02 09:36:27 +0100

    remove more mentions of test_yml_builder

--- nf_core/modules/__init__.py
+++ nf_core/modules/__init__.py
@@ -10,5 +10,4 @@ from .modules_test import ModulesTest
 from .modules_utils import ModuleException
 from .patch import ModulePatch
 from .remove import ModuleRemove
-from .test_yml_builder import ModulesTestYmlBuilder
 from .update import ModuleUpdate
