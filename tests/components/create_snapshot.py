f42fe20c916e381f862a2d83560fe1e923a58855
mirpedrol <mirp.julia@gmail.com>
2023-11-07 14:37:18 +0100

    run create-snapshot tests from modules root directory

--- tests/components/create_snapshot.py
+++ tests/components/create_snapshot.py
@@ -8,34 +8,36 @@ from ..utils import GITLAB_NFTEST_BRANCH, GITLAB_URL
 
 def test_generate_snapshot_module(self):
     """Generate the snapshot for a module in nf-core/modules clone"""
-    for root, directories, files in os.walk(self.nfcore_modules):
-        print(f"Directory: {root}")
-        for file in files:
-            print(f"  File: {file}")
-
+    os.chdir(self.nfcore_modules)
     snap_generator = ComponentTestSnapshotGenerator(
         component_type="modules",
         component_name="fastqc",
         no_prompts=True,
-        directory=self.nfcore_modules,
         remote_url=GITLAB_URL,
         branch=GITLAB_NFTEST_BRANCH,
     )
-    assert snap_generator.run()
+    try:
+        snap_generator.run()
+    except UserWarning as e:
+        assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
 
-    assert os.path.exists(Path(self.nfcore_modules, "modules", "nf-core-test", "fastqc", "tests", "main.nf.test.snap"))
+    assert Path("modules", "nf-core-test", "fastqc", "tests", "main.nf.test.snap").exists()
 
 
 def test_update_snapshot_module(self):
     """Update the snapshot of a module in nf-core/modules clone"""
+    os.chdir(self.nfcore_modules)
     snap_generator = ComponentTestSnapshotGenerator(
         component_type="modules",
         component_name="bwa/mem",
         no_prompts=True,
+        remote_url=GITLAB_URL,
+        branch=GITLAB_NFTEST_BRANCH,
         update=True,
     )
-    assert snap_generator.run()
+    try:
+        snap_generator.run()
+    except UserWarning as e:
+        assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
 
-    assert os.path.exists(
-        Path(self.nfcore_modules, "modules", "nf-core-test", "bwa", "mem", "tests", "main.nf.test.snap")
-    )
+    assert Path("modules", "nf-core-test", "bwa", "mem", "tests", "main.nf.test.snap").exists()
