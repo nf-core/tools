53b23b7253b78e5d6d5cacf1b8c730e6577e6e70
mirpedrol <mirp.julia@gmail.com>
2023-11-08 08:59:12 +0100

    use set_wd for tests

--- tests/components/create_snapshot.py
+++ tests/components/create_snapshot.py
@@ -3,41 +3,41 @@ from pathlib import Path
 
 from nf_core.components.snapshot_generator import ComponentTestSnapshotGenerator
 
-from ..utils import GITLAB_NFTEST_BRANCH, GITLAB_URL
+from ..utils import GITLAB_NFTEST_BRANCH, GITLAB_URL, set_wd
 
 
 def test_generate_snapshot_module(self):
     """Generate the snapshot for a module in nf-core/modules clone"""
-    os.chdir(self.nfcore_modules)
-    snap_generator = ComponentTestSnapshotGenerator(
-        component_type="modules",
-        component_name="fastqc",
-        no_prompts=True,
-        remote_url=GITLAB_URL,
-        branch=GITLAB_NFTEST_BRANCH,
-    )
-    try:
-        snap_generator.run()
-    except UserWarning as e:
-        assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
+    with set_wd(self.nfcore_modules):
+        snap_generator = ComponentTestSnapshotGenerator(
+            component_type="modules",
+            component_name="fastqc",
+            no_prompts=True,
+            remote_url=GITLAB_URL,
+            branch=GITLAB_NFTEST_BRANCH,
+        )
+        try:
+            snap_generator.run()
+        except UserWarning as e:
+            assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
 
-    assert Path("modules", "nf-core-test", "fastqc", "tests", "main.nf.test.snap").exists()
+        assert Path("modules", "nf-core-test", "fastqc", "tests", "main.nf.test.snap").exists()
 
 
 def test_update_snapshot_module(self):
     """Update the snapshot of a module in nf-core/modules clone"""
-    os.chdir(self.nfcore_modules)
-    snap_generator = ComponentTestSnapshotGenerator(
-        component_type="modules",
-        component_name="bwa/mem",
-        no_prompts=True,
-        remote_url=GITLAB_URL,
-        branch=GITLAB_NFTEST_BRANCH,
-        update=True,
-    )
-    try:
-        snap_generator.run()
-    except UserWarning as e:
-        assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
+    with set_wd(self.nfcore_modules):
+        snap_generator = ComponentTestSnapshotGenerator(
+            component_type="modules",
+            component_name="bwa/mem",
+            no_prompts=True,
+            remote_url=GITLAB_URL,
+            branch=GITLAB_NFTEST_BRANCH,
+            update=True,
+        )
+        try:
+            snap_generator.run()
+        except UserWarning as e:
+            assert False, f"'ComponentTestSnapshotGenerator' raised an exception {e}"
 
-    assert Path("modules", "nf-core-test", "bwa", "mem", "tests", "main.nf.test.snap").exists()
+        assert Path("modules", "nf-core-test", "bwa", "mem", "tests", "main.nf.test.snap").exists()
