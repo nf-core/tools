9ed053a8c276fdfaedcfff72a27a5d6a8deecace
mashehu <mashehu3@gmail.com>
2024-01-24 17:47:06 +0100

    restructure code and add tests

--- nf_core/ro_crate.py
+++ nf_core/ro_crate.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-""" Code to deal with pipeline RO (Research Object) Crates """
-
+"""Code to deal with pipeline RO (Research Object) Crates"""
 
 import logging
 import tempfile
@@ -10,6 +9,7 @@ from typing import Union
 import requests
 import rocrate.model.entity
 import rocrate.rocrate
+from git import GitCommandError, InvalidGitRepositoryError, Repo
 from rocrate.model.person import Person
 
 from nf_core.utils import Pipeline
@@ -18,10 +18,17 @@ log = logging.getLogger(__name__)
 
 
 class RoCrate:
-    """Class to generate an RO Crate for a pipeline"""
+    """
+    Class to generate an RO Crate for a pipeline
+
+    Args:
+        pipeline_dir (Path): Path to the pipeline directory
+        version (str): Version of the pipeline to use
+
+    """
 
     def __init__(self, pipeline_dir: Path, version=""):
-        from nf_core.utils import is_pipeline_directory
+        from nf_core.utils import is_pipeline_directory, setup_requests_cachedir
 
         is_pipeline_directory(pipeline_dir)
         self.pipeline_dir = pipeline_dir
@@ -30,13 +37,55 @@ class RoCrate:
         self.pipeline_obj = Pipeline(str(self.pipeline_dir))
         self.pipeline_obj._load()
 
-    def create_ro_crate(self, outdir: Path, metadata_fn="", zip_fn=""):
-        """Create an RO Crate for the pipeline"""
+        setup_requests_cachedir()
+
+    def create_ro_crate(
+        self, outdir: Path, metadata_fn: Union[str, None, Path] = None, zip_fn: Union[str, None] = None
+    ) -> None:
+        """
+        Create an RO Crate for a pipeline
+
+        Args:
+            outdir (Path): Path to the output directory
+            metadata_fn (str): Filename for the metadata file
+            zip_fn (str): Filename for the zip file
+
+        """
+        import os
 
         # Set input paths
-        self.get_crate_paths(outdir)
+        try:
+            self.set_crate_paths(outdir)
+        except OSError as e:
+            log.error(e)
+            sys.exit(1)
 
-        self.make_workflow_ro_crate(self.pipeline_dir)
+        # Change to the pipeline directory, because the RO Crate doesn't handle relative paths well
+        current_path = Path.cwd()
+        os.chdir(self.pipeline_dir)
+
+        # Check that the checkout pipeline version is the same as the requested version
+        if self.version:
+            if self.version != self.pipeline_obj.nf_config.get("manifest.version"):
+                # using git checkout to get the requested version
+                log.info(f"Checking out pipeline version {self.version}")
+                try:
+                    self.repo = Repo(self.pipeline_dir)
+                    self.repo.git.checkout(self.version)
+                    self.pipeline_obj = Pipeline(str(self.pipeline_dir))
+                    self.pipeline_obj._load()
+                except InvalidGitRepositoryError:
+                    log.error(f"Could not find a git repository in {self.pipeline_dir}")
+                    sys.exit(1)
+                except GitCommandError:
+                    log.error(f"Could not checkout version {self.version}")
+                    sys.exit(1)
+
+        try:
+            self.make_workflow_ro_crate()
+        except Exception as e:
+            log.error(e)
+            sys.exit(1)
 
         # Save just the JSON metadata file
         if metadata_fn is not None:
@@ -53,16 +102,21 @@ class RoCrate:
             log.info(f"Saving zip file '{zip_fn}'")
             self.crate.write_zip(zip_fn)
 
-    def make_workflow_ro_crate(self, path: Path):
-        import nf_core.utils
+        # Change back to the original directory
+        os.chdir(current_path)
 
+    def make_workflow_ro_crate(self) -> None:
+        """
+        Create an RO Crate for a pipeline
+
+        Args:
+            path (Path): Path to the pipeline directory
+        """
         if self.pipeline_obj is None:
             raise ValueError("Pipeline object not loaded")
 
-        # Create the RO Crate
+        # Create the RO Crate object
         self.crate = rocrate.rocrate.ROCrate()
-
-        # Conform to RO-Crate 1.1 and workflowhub-ro-crate
 
         # Set language type
         programming_language = rocrate.model.entity.Entity(
@@ -77,6 +131,8 @@ class RoCrate:
             },
         )
         self.crate.add(programming_language)
+
+        # Conform to RO-Crate 1.1 and workflowhub-ro-crate
         self.crate.update_jsonld(
             {
                 "@id": "ro-crate-metadata.json",
@@ -88,26 +144,19 @@ class RoCrate:
         )
 
         # Set main entity file
-        wf_file = self.crate.add_jsonld(
-            {
-                "@id": "main.nf",
-                "@type": ["File", "SoftwareSourceCode", "ComputationalWorkflow"],
-            },
-        )
-        self.crate.mainEntity = wf_file
-        # self.crate.update_jsonld({"@id": "main.nf", "@type": ["File", "SoftwareSourceCode", "ComputationalWorkflow"]})
-
-        self.add_authors(wf_file)
-        wf_file.append_to("programmingLanguage", programming_language)
+        self.set_main_entity("main.nf")
 
         # add readme as description
-        readme = Path(self.pipeline_dir, "README.md")
-        self.crate.description = readme.read_text()
+        readme = Path("README.md")
+
+        try:
+            self.crate.description = readme.read_text()
+        except FileNotFoundError:
+            log.error(f"Could not find README.md in {self.pipeline_dir}")
 
         self.crate.license = "MIT"
 
         # add doi as identifier
-        # self.crate.identifier = self.pipeline_obj.get("manifest", {}).get("doi", "")
         self.crate.name = f'Research Object Crate for {self.pipeline_obj.nf_config.get("manifest.name")}'
 
         if "dev" in self.pipeline_obj.nf_config.get("manifest.version", ""):
@@ -116,32 +165,35 @@ class RoCrate:
             self.crate.CreativeWorkStatus = "Stable"
 
         # Add all other files
-        wf_filenames = nf_core.utils.get_wf_files(self.pipeline_dir)
-        log.debug(f"Adding {len(wf_filenames)} workflow files")
-        for fn in wf_filenames:
-            # check if it wasn't already added
-            if fn == "main.nf":
-                continue
-            # add nextflow language to .nf and .config files
-            if fn.endswith(".nf") or fn.endswith(".config"):
-                log.debug(f"Adding workflow file: {fn}")
-                self.crate.add_file(fn, properties={"programmingLanguage": {"@id": "#nextflow"}})
-            if fn.endswith(".png"):
-                log.debug(f"Adding workflow file: {fn}")
-                self.crate.add_file(fn, properties={"@type": ["File", "ImageObject"]})
-                if "metro_map" in fn:
-                    log.info(f"Setting main entity image to: {fn}")
-                    wf_file.append_to("image", {"@id": fn})
-            if fn.endswith(".md"):
-                log.debug(f"Adding workflow file: {fn}")
-                self.crate.add_file(fn, properties={"encodingFormat": "text/markdown"})
-            else:
-                log.debug(f"Adding workflow file: {fn}")
-                self.crate.add_file(fn)
+        self.add_workflow_files()
 
-        # Add keywords from github topics
+    def set_main_entity(self, main_entity_filename: str):
+        """
+        Set the main.nf as the main entity of the crate and add necessary metadata
+        """
 
-    def add_authors(self, wf_file):
+        wf_file = self.crate.add_jsonld(
+            {
+                "@id": main_entity_filename,
+                "@type": ["File", "SoftwareSourceCode", "ComputationalWorkflow"],
+            },
+        )
+        self.crate.mainEntity = wf_file
+        self.add_main_authors(wf_file)
+        wf_file.append_to("programmingLanguage", {"@id": "#nextflow"})
+        # get keywords from nf-core website
+        remote_workflows = requests.get("https://nf-co.re/pipelines.json").json()["remote_workflows"]
+        # go through all remote workflows and find the one that matches the pipeline name
+        topics = ["nf-core", "nextflow"]
+        for remote_wf in remote_workflows:
+            if remote_wf["name"] == self.pipeline_obj.pipeline_name.replace("nf-core/", ""):
+                topics = topics + remote_wf["topics"]
+                break
+
+        log.debug(f"Adding topics: {topics}")
+        wf_file.append_to("keywords", topics)
+
+    def add_main_authors(self, wf_file):
         """
         Add workflow authors to the crate
         NB: We don't have much metadata here - scope to improve in the future
@@ -159,7 +211,42 @@ class RoCrate:
             author_entitity = self.crate.add(Person(self.crate, orcid, properties={"name": author}))
             wf_file.append_to("author", author_entitity)
 
-    def get_crate_paths(self, path):
+    def add_workflow_files(self):
+        """
+        Add workflow files to the RO Crate
+        """
+        import nf_core.utils
+
+        wf_filenames = nf_core.utils.get_wf_files(Path.cwd())
+        # exclude github action files
+        wf_filenames = [fn for fn in wf_filenames if not fn.startswith(".github/")]
+        log.debug(f"Adding {len(wf_filenames)} workflow files")
+        for fn in wf_filenames:
+            # skip main.nf
+            if fn == "main.nf":
+                continue
+            # add nextflow language to .nf and .config files
+            if fn.endswith(".nf") or fn.endswith(".config") or fn.endswith(".nf.test"):
+                log.debug(f"Adding workflow file: {fn}")
+                self.crate.add_file(fn, properties={"programmingLanguage": {"@id": "#nextflow"}})
+                continue
+            if fn.endswith(".png"):
+                log.debug(f"Adding workflow image file: {fn}")
+                self.crate.add_jsonld({"@id": Path(fn).name, "@type": ["File", "ImageObject"]})
+                if "metro_map" in fn:
+                    log.info(f"Setting main entity image to: {fn}")
+                    self.crate.mainEntity.append_to("image", {"@id": Path(fn).name})
+                continue
+            if fn.endswith(".md"):
+                log.debug(f"Adding workflow file: {fn}")
+                self.crate.add_file(fn, properties={"encodingFormat": "text/markdown"})
+                continue
+            else:
+                log.debug(f"Adding workflow file: {fn}")
+                self.crate.add_file(fn)
+                continue
+
+    def set_crate_paths(self, path: Path) -> None:
         """Given a pipeline name, directory, or path, set wf_crate_filename"""
 
         path = Path(path)
@@ -177,6 +264,15 @@ class RoCrate:
 
 
 def get_orcid(name: str) -> Union[str, None]:
+    """
+    Get the ORCID for a given name
+
+    Args:
+        name (str): Name of the author
+
+    Returns:
+        str: ORCID URI or None
+    """
     base_url = "https://pub.orcid.org/v3.0/search/"
     headers = {
         "Accept": "application/json",
---
+++ tests/test_rocrate.py
@@ -0,0 +1,86 @@
+""" Test the nf-core rocrate command """
+
+
+import shutil
+import tempfile
+import unittest
+from pathlib import Path
+
+import rocrate.rocrate
+from git import Repo
+
+import nf_core.create
+import nf_core.rocrate
+import nf_core.utils
+
+
+class TestROCrate(unittest.TestCase):
+    """Class for lint tests"""
+
+    def setUp(self):
+        """Function that runs at start of tests for common resources
+
+        Use nf_core.create() to make a pipeline that we can use for testing
+        """
+
+        self.tmp_dir = Path(tempfile.mkdtemp())
+        self.test_pipeline_dir = Path(self.tmp_dir, "nf-core-testpipeline")
+        self.create_obj = nf_core.create.PipelineCreate(
+            name="testpipeline",
+            description="This is a test pipeline",
+            author="Test McTestFace",
+            outdir=self.test_pipeline_dir,
+            version="1.0.0",
+            no_git=False,
+            force=True,
+            plain=True,
+        )
+        self.create_obj.init_pipeline()
+
+        # add fake metro map
+        Path(self.test_pipeline_dir, "docs", "images", "nf-core-testpipeline_metro_map.png").touch()
+        # commit the changes
+        repo = Repo(self.test_pipeline_dir)
+        repo.git.add(A=True)
+        repo.index.commit("Initial commit")
+
+    def tearDown(self):
+        """Clean up temporary files and folders"""
+
+        if self.tmp_dir.exists():
+            shutil.rmtree(self.tmp_dir)
+
+    def test_rocrate_creation(self):
+        """Run the nf-core rocrate command"""
+
+        # Run the command
+        self.rocrate_obj = nf_core.rocrate.RoCrate(self.test_pipeline_dir)
+        self.rocrate_obj.create_ro_crate(
+            self.test_pipeline_dir, metadata_fn=Path(self.test_pipeline_dir, "ro-crate-metadata.json")
+        )
+
+        # Check that the crate was created
+        self.assertTrue(Path(self.test_pipeline_dir, "ro-crate-metadata.json").exists())
+
+        # Check that the entries in the crate are correct
+        crate = rocrate.rocrate.ROCrate(str(self.test_pipeline_dir))
+        entities = crate.get_entities()
+
+        # Check if the correct entities are set:
+        for entity in entities:
+            entity_json = entity.as_jsonld()
+            if entity_json["@id"] == "./":
+                self.assertEqual(entity_json.get("name"), "Research Object Crate for nf-core/testpipeline")
+                self.assertEqual(entity_json["mainEntity"], {"@id": "#main.nf"})
+            elif entity_json["@id"] == "#main.nf":
+                self.assertEqual(entity_json["programmingLanguage"], [{"@id": "#nextflow"}])
+                self.assertEqual(entity_json["image"], [{"@id": "nf-core-testpipeline_metro_map.png"}])
+            # assert there is a metro map
+            # elif entity_json["@id"] == "nf-core-testpipeline_metro_map.png": # FIXME waiting for https://github.com/ResearchObject/ro-crate-py/issues/174
+            # self.assertEqual(entity_json["@type"], ["File", "ImageObject"])
+            # assert that author is set as a person
+            elif "name" in entity_json and entity_json["name"] == "Test McTestFace":
+                self.assertEqual(entity_json["@type"], "Person")
+                # check that it is set as author of the main entity
+                if crate.mainEntity is not None:
+                    self.assertEqual(crate.mainEntity["author"][0].id, entity_json["@id"])
