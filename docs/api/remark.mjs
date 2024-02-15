fac45e72258199c4a52fc25e187cdffd7b549fa2
mashehu <mashehu3@gmail.com>
2024-02-15 17:35:27 +0100

    use different remark logic

--- docs/api/remark.mjs
+++ docs/api/remark.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import { remark } from "remark";
 import { visit } from "unist-util-visit";
 
-const remarkDirectives = () => {
+function remarkDirectives() {
     return transformer;
 
     function transformer(tree) {
@@ -12,23 +12,66 @@ const remarkDirectives = () => {
 
     function visitor(node, index, parent) {
         if (node.depth === 4) {
-            const firstChild = node.children[0];
-            if (["note", "warning"].includes(firstChild?.value?.toLowerCase())) {
-                handleNoteOrWarning(node, index, parent, firstChild);
-            } else if (firstChild?.type === "emphasis") {
-                handleEmphasis(node, firstChild);
-            } else if (firstChild?.type !== "inlineCode") {
-                handleInlineCode(node, firstChild);
+            if (["note", "warning"].includes(node.children[0].value?.toLowerCase())) {
+                const type = node.children[0].value.toLowerCase();
+                parent.children.splice(index, 1);
+                parent.children[index].children[0].value = `:::${type}\n${parent.children[index].children[0].value}`;
+                // if second to list parent.children[index].children ends with ":", check if the next node is a code block, if so, add the code block as a child to the current node
+                if (parent.children[index].children.slice(-1)[0]?.value?.trim().endsWith(":")) {
+                    if (parent.children[index + 1].type === "code") {
+                        parent.children[index].children.slice(-1)[0].value += "\n";
+                        parent.children[index].children.push(parent.children[index + 1]);
+                        parent.children.splice(index + 1, 1);
+                    }
+                }
+                parent.children[index].children.push({ type: "text", value: "\n:::" });
+            } else if (node.children[0].type === "emphasis") {
+                node.children[0].children.map((child) => {
+                    if (child.type === "text") {
+                        child.type = "inlineCode";
+                        child.value = child.value?.trim() + "{:python}";
+                    }
+                });
+                // convert the rest of the heading to inline code
+                node.children.slice(1).map((child) => {
+                    if (child.type === "text") {
+                        child.type = "inlineCode";
+                        child.value = child.value?.trim() + "{:python}";
+                    }
+                    if (child.type === "link") {
+                        child.children.map((child) => {
+                            if (child.type === "text") {
+                                child.type = "inlineCode";
+                                child.value = child.value?.trim() + "{:python}";
+                            }
+                        });
+                    }
+                });
+            } else if (node.children[0].type !== "inlineCode") {
+                node.children[0] = {
+                    type: "inlineCode",
+                    value: node.children[0].value?.trim() + "{:python}",
+                };
             }
         } else if (node.depth === 3) {
-            node.children.forEach((child) => {
-                if (child.type === "text" || child.type === "emphasis") {
-                    handleInlineCode(node, child);
+            node.children.map((child) => {
+                if (child.type === "text") {
+                    child.type = "inlineCode";
+                    child.value = child.value?.trim() + "{:python}";
                 }
                 if (child.type === "link") {
-                    child.children.forEach((child) => {
+                    child.children.map((child) => {
                         if (child.type === "text") {
-                            handleInlineCode(node, child);
+                            child.type = "inlineCode";
+                            child.value = child.value?.trim() + "{:python}";
+                        }
+                    });
+                }
+                if (child.type === "emphasis") {
+                    child.children.map((child) => {
+                        if (child.type === "text") {
+                            child.type = "inlineCode";
+                            child.value = child.value?.trim() + "{:python}";
                         }
                     });
                 }
@@ -38,59 +81,13 @@ const remarkDirectives = () => {
             node.url = node.url.replace(".md", "");
         }
     }
+}
 
-    function handleNoteOrWarning(node, index, parent, firstChild) {
-        const type = firstChild.value.toLowerCase();
-        parent.children.splice(index, 1);
-        parent.children[index].children[0].value = `:::${type}\n${parent.children[index].children[0].value}`;
-
-        const lastChildValue = parent.children[index].children.slice(-1)[0]?.value?.trim();
-        if (lastChildValue?.endsWith(":") && parent.children[index + 1]?.type === "code") {
-            parent.children[index].children.slice(-1)[0].value += "\n";
-            parent.children[index].children.push(parent.children[index + 1]);
-            parent.children.splice(index + 1, 1);
-        }
-
-        parent.children[index].children.push({ type: "text", value: "\n:::" });
-    }
-
-    function handleEmphasis(node, firstChild) {
-        firstChild.children.forEach((child) => {
-            if (child.type === "text") {
-                child.type = "inlineCode";
-                child.value = `${child.value?.trim()}{:python}`;
-            }
-        });
-
-        node.children.slice(1).forEach((child) => {
-            if (child.type === "text") {
-                child.type = "inlineCode";
-                child.value = `${child.value?.trim()}{:python}`;
-            }
-            if (child.type === "link") {
-                child.children.forEach((child) => {
-                    if (child.type === "text") {
-                        child.type = "inlineCode";
-                        child.value = `${child.value?.trim()}{:python}`;
-                    }
-                });
-            }
-        });
-    }
-
-    function handleInlineCode(node, firstChild) {
-        node.children[0] = {
-            type: "inlineCode",
-            value: `${firstChild.value?.trim()}{:python}`,
-        };
-    }
-};
-
-const markdown = fs.readFileSync(process.argv[2]);
+let markdown = fs.readFileSync(process.argv[2]);
 
 remark()
     .use(remarkDirectives)
-    .process(markdown, (err, file) => {
+    .process(markdown, function (err, file) {
         if (err) throw err;
         fs.writeFileSync(process.argv[2], String(file));
     });
