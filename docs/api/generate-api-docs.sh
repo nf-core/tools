eeca1b20d86d7dd83aaa39e517f7f5eaebdd87e9
mashehu <mashehu3@gmail.com>
2024-02-19 13:47:09 +0100

    fix generate api docs script, gsed-> sed

--- docs/api/generate-api-docs.sh
+++ docs/api/generate-api-docs.sh
@@ -48,7 +48,7 @@ for release in "${releases[@]}"; do
     git checkout docs/api
     pip install -r docs/api/requirements.txt --quiet
     # add the napoleon extension to the sphinx conf.py
-    gsed -i 's/^extensions = \[/extensions = \[\n    "sphinx_markdown_builder",/' docs/api/_src/conf.py
+    sed -i 's/^extensions = \[/extensions = \[\n    "sphinx_markdown_builder",/' docs/api/_src/conf.py
 
     # run docs/api/make_lint_md.py if it exists
     # if [[ -f "docs/api/make_lint_md.py" ]]; then
@@ -59,12 +59,12 @@ for release in "${releases[@]}"; do
         # echo "Processing $file"
 
         # replace ..tip:: with note in the python docstrings due to missing directive in the markdown builder
-        gsed -i 's/^\(\s*\)\.\. tip::/\1\.\. note::/g' "$file"
+        sed -i 's/^\(\s*\)\.\. tip::/\1\.\. note::/g' "$file"
 
     done
 
     # fix syntax in lint/merge_markers.py
-    gsed -i 's/>>>>>>> or <<<<<<</``>>>>>>>`` or ``<<<<<<<``/g' nf_core/lint/merge_markers.py
+    sed -i 's/>>>>>>> or <<<<<<</``>>>>>>>`` or ``<<<<<<<``/g' nf_core/lint/merge_markers.py
     # remove markdown files if --force is set
     if [[ "$force" = true ]]; then
         echo -e "\n\e[31mRemoving $output_dir/$release because of '--force'\e[0m"
@@ -77,7 +77,7 @@ for release in "${releases[@]}"; do
 
     git checkout -
     # replace :::{seealso} with :::tip in the markdown files
-    find "$output_dir/$release" -name "*.md" -exec gsed -i 's/:::{seealso}/:::tip/g' {} \;
+    find "$output_dir/$release" -name "*.md" -exec sed -i 's/:::{seealso}/:::tip/g' {} \;
     i=1
     sp="/-\|" # spinner
     find "$output_dir/$release" -name "*.md" | while IFS= read -r file; do
