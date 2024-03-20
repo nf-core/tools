37a2ed7b9a40f7570498dbc006c8121ed3b2b439
James A. Fellows Yates <jfy133@gmail.com>
2024-03-20 15:42:26 +0100

    Add a self reminder here

--- nf_core/configs/create/create.py
+++ nf_core/configs/create/create.py
@@ -7,6 +7,8 @@ class ConfigCreate:
     def __init__(self, template_config: CreateConfig):
         self.template_config = template_config
 
+    ## TODO make this into a proper function here so that the exported `dict`
+    ## is an actual nf-core config
     def write_to_file(self):
         with open("file.txt", "w+") as file:
             file.write(json.dumps(dict(self.template_config)))
