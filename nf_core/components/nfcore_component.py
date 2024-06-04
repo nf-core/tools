970b4a0e09249476eaf2153ff57a0aad061d6750
mirpedrol <mirp.julia@gmail.com>
2024-06-04 11:51:25 +0200

    get correct format of inputs and outputs

--- nf_core/components/nfcore_component.py
+++ nf_core/components/nfcore_component.py
@@ -174,7 +174,7 @@ class NFCoreComponent:
             return inputs
         input_data = data.split("input:")[1].split("output:")[0]
         for line in input_data.split("\n"):
-            theseinputs = []
+            channel_elements = []
             regex = r"(val|path)\s*(\(([^)]+)\)|\s*([^)\s,]+))"
             matches = re.finditer(regex, line)
             for _, match in enumerate(matches, start=1):
@@ -187,13 +187,9 @@ class NFCoreComponent:
                 elif match.group(4):
                     input_val = match.group(4).split(",")[0]  # handle `files, stageAs: "inputs/*"` cases
                 if input_type and input_val:
-                    theseinputs.append({
-                        input_val: {
-                            "type": input_type
-                        }
-                    })
-            if len(theseinputs) > 0:
-                inputs.append(theseinputs)
+                    channel_elements.append({input_val: {"type": input_type}})
+            if len(channel_elements) > 0:
+                inputs.append(channel_elements)
         log.debug(f"Found {len(inputs)} inputs in {self.main_nf}")
         self.inputs = inputs
 
@@ -206,9 +202,25 @@ class NFCoreComponent:
             log.debug(f"Could not find any outputs in {self.main_nf}")
             return outputs
         output_data = data.split("output:")[1].split("when:")[0]
-        regex = r"emit:\s*([^)\s,]+)"
-        matches = re.finditer(regex, output_data, re.MULTILINE)
-        for _, match in enumerate(matches, start=1):
-            outputs.append(match.group(1))
+        regex_emit = r"emit:\s*([^)\s,]+)"
+        regex_elements = r"(val|path|env|stdout)\s*(\(([^)]+)\)|\s*([^)\s,]+))"
+        for line in output_data.split("\n"):
+            match_emit = re.search(regex_emit, line)
+            matches_elements = re.finditer(regex_elements, line)
+            if not match_emit:
+                continue
+            output_channel = {match_emit.group(1): []}
+            for _, match_element in enumerate(matches_elements, start=1):
+                output_type = None
+                output_val = None
+                if match_element.group(1):
+                    output_type = match_element.group(1)
+                if match_element.group(3):
+                    output_val = match_element.group(3).split(",")[0]  # handle `files, stageAs: "inputs/*"` cases
+                elif match_element.group(4):
+                    output_val = match_element.group(4).split(",")[0]  # handle `files, stageAs: "inputs/*"` cases
+                if output_type and output_val:
+                    output_channel[match_emit.group(1)].append({output_val: {"type": output_type}})
+            outputs.append(output_channel)
         log.debug(f"Found {len(outputs)} outputs in {self.main_nf}")
         self.outputs = outputs
