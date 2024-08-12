4888d3251982337ba31a4b73de960cac7a3d8f3b
jvfe <jvfecav@gmail.com>
2024-08-12 08:12:22 -0300

    fix: Use subworkflow name in recreate deps

--- nf_core/modules/modules_json.py
+++ nf_core/modules/modules_json.py
@@ -1183,7 +1183,8 @@ class ModulesJson:
         i.e., no module or subworkflow has been installed by the user in the meantime
         """
 
-        sw_path = Path(self.subworkflows_dir, org, subworkflow)
+        sw_name = subworkflow["name"] if isinstance(subworkflow, dict) else subworkflow
+        sw_path = Path(self.subworkflows_dir, org, sw_name)
         dep_mods, dep_subwfs = get_components_to_install(sw_path)
 
         for dep_mod in dep_mods:
