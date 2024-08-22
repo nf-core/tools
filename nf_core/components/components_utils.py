2b9a573c4b12d6e7961969615283ee0c9aa9bf96
João Cavalcante <jvfecav@gmail.com>
2024-08-22 17:31:43 -0300

    refact: Change UserWarning message
    Co-authored-by: Matthieu Muffato <mm49@sanger.ac.uk>

--- nf_core/components/components_utils.py
+++ nf_core/components/components_utils.py
@@ -185,7 +185,7 @@ def get_components_to_install(
                         if org_path_match:
                             org_path = org_path_match.group(1)
                         else:
-                            raise UserWarning(f"Organisation path could not be established for {component_name}")
+                            raise UserWarning(f"The organisation path of {component_name} could not be established from '{git_remote}'")
                         current_comp_dict = subworkflows if component_name in subworkflows else modules
 
                         component_dict = {
