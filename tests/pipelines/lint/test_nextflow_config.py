559903976a9dd5d81ed58df81f13bd8523de3618
mirpedrol <mirp.julia@gmail.com>
2024-09-16 16:49:39 +0200

    add tests back

--- tests/pipelines/lint/test_nextflow_config.py
+++ tests/pipelines/lint/test_nextflow_config.py
@@ -2,8 +2,11 @@ import os
 import re
 from pathlib import Path
 
+import yaml
+
 import nf_core.pipelines.create.create
 import nf_core.pipelines.lint
+from nf_core.utils import NFCoreYamlConfig
 
 from ..test_lint import TestLint
 
@@ -64,6 +67,81 @@ class TestLintNextflowConfig(TestLint):
         result = lint_obj.nextflow_config()
         assert len(result["failed"]) > 0
         assert len(result["warned"]) == 0
+
+    def test_default_values_fail(self):
+        """Test linting fails if the default values in nextflow.config do not match the ones defined in the nextflow_schema.json."""
+        # Change the default value of max_multiqc_email_size in nextflow.config
+        nf_conf_file = Path(self.new_pipeline) / "nextflow.config"
+        with open(nf_conf_file) as f:
+            content = f.read()
+            fail_content = re.sub(r"\bmax_multiqc_email_size\s*=\s*'25.MB'", "max_multiqc_email_size = '0'", content)
+        with open(nf_conf_file, "w") as f:
+            f.write(fail_content)
+        # Change the default value of custom_config_version in nextflow_schema.json
+        nf_schema_file = Path(self.new_pipeline) / "nextflow_schema.json"
+        with open(nf_schema_file) as f:
+            content = f.read()
+            fail_content = re.sub(r'"default": "master"', '"default": "main"', content)
+        with open(nf_schema_file, "w") as f:
+            f.write(fail_content)
+        lint_obj = nf_core.pipelines.lint.PipelineLint(self.new_pipeline)
+        lint_obj.load_pipeline_config()
+        result = lint_obj.nextflow_config()
+        assert len(result["failed"]) == 2
+        assert (
+            "Config default value incorrect: `params.max_multiqc_email_size` is set as `25.MB` in `nextflow_schema.json` but is `0` in `nextflow.config`."
+            in result["failed"]
+        )
+        assert (
+            "Config default value incorrect: `params.custom_config_version` is set as `main` in `nextflow_schema.json` but is `master` in `nextflow.config`."
+            in result["failed"]
+        )
+
+    def test_catch_params_assignment_in_main_nf(self):
+        """Test linting fails if main.nf contains an assignment to a parameter from nextflow_schema.json."""
+        # Add parameter assignment in main.nf
+        main_nf_file = Path(self.new_pipeline) / "main.nf"
+        with open(main_nf_file, "a") as f:
+            f.write("params.custom_config_base = 'test'")
+        lint_obj = nf_core.pipelines.lint.PipelineLint(self.new_pipeline)
+        lint_obj.load_pipeline_config()
+        result = lint_obj.nextflow_config()
+        assert len(result["failed"]) == 1
+        assert (
+            result["failed"][0]
+            == "Config default value incorrect: `params.custom_config_base` is set as `https://raw.githubusercontent.com/nf-core/configs/master` in `nextflow_schema.json` but is `null` in `nextflow.config`."
+        )
+
+    def test_allow_params_reference_in_main_nf(self):
+        """Test linting allows for references like `params.aligner == 'bwa'` in main.nf. The test will detect if the bug mentioned in GitHub-issue #2833 reemerges."""
+        # Add parameter reference in main.nf
+        main_nf_file = Path(self.new_pipeline) / "main.nf"
+        with open(main_nf_file, "a") as f:
+            f.write("params.custom_config_version == 'main'")
+        lint_obj = nf_core.pipelines.lint.PipelineLint(self.new_pipeline)
+        lint_obj.load_pipeline_config()
+        result = lint_obj.nextflow_config()
+        assert len(result["failed"]) == 0
+
+    def test_default_values_ignored(self):
+        """Test ignoring linting of default values."""
+        # Add custom_config_version to the ignore list
+        nf_core_yml_path = Path(self.new_pipeline) / ".nf-core.yml"
+        nf_core_yml = NFCoreYamlConfig(
+            repository_type="pipeline",
+            lint={"nextflow_config": [{"config_defaults": ["params.custom_config_version"]}]},
+        )
+        with open(nf_core_yml_path, "w") as f:
+            yaml.dump(nf_core_yml.model_dump(), f)
+
+        lint_obj = nf_core.pipelines.lint.PipelineLint(self.new_pipeline)
+        lint_obj.load_pipeline_config()
+        lint_obj._load_lint_config()
+        result = lint_obj.nextflow_config()
+        assert len(result["failed"]) == 0
+        assert len(result["ignored"]) == 1
+        assert "Config default value correct: params.custom_config_version" not in str(result["passed"])
+        assert "Config default ignored: params.custom_config_version" in str(result["ignored"])
 
     def test_default_values_float(self):
         """Test comparing two float values."""
