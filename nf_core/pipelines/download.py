ae5084038ac5712e12566eb39ebd0aab593942be
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2024-09-23 19:18:21 +0200

    Refactor CLI commands for Download.

--- nf_core/pipelines/download.py
+++ nf_core/pipelines/download.py
@@ -133,10 +133,8 @@ class DownloadWorkflow:
         self.force = force
         self.platform = platform
         self.fullname: Optional[str] = None
-        # if flag is not specified, do not assume deliberate choice and prompt config inclusion interactively.
-        # this implies that non-interactive "no" choice is only possible implicitly (e.g. with --platform or if prompt is suppressed by !stderr.is_interactive).
-        # only alternative would have been to make it a parameter with argument, e.g. -d="yes" or -d="no".
-        self.include_configs = True if download_configuration else False if bool(platform) else None
+        # downloading configs is not supported for Seqera Platform downloads.
+        self.include_configs = True if download_configuration == "yes" and not bool(platform) else False
         # Additional tags to add to the downloaded pipeline. This enables to mark particular commits or revisions with
         # additional tags, e.g. "stable", "testing", "validated", "production" etc. Since this requires a git-repo, it is only
         # available for the bare / Seqera Platform download.
