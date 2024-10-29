8a1d44a09941e040b5b6c0a2961ccb97f00c9a53
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2024-10-29 15:35:33 +0100

    feat(1929): local linting option now *only* lints local modules. Add tests

--- tests/subworkflows/test_lint.py
+++ tests/subworkflows/test_lint.py
@@ -31,7 +31,6 @@ class TestSubworkflowsLint(TestSubworkflows):
         subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.nfcore_modules)
         subworkflow_lint.lint(print_results=True, all_subworkflows=True)
         assert len(subworkflow_lint.failed) == 0
-
         assert len(subworkflow_lint.passed) > 0
         assert len(subworkflow_lint.warned) >= 0
 
@@ -397,3 +396,41 @@ class TestSubworkflowsLint(TestSubworkflows):
         # reset the file
         with open(snap_file, "w") as fh:
             fh.write(content)
+
+    def test_subworkflows_lint_local(self):
+        assert self.subworkflow_install.install("fastq_align_bowtie2")
+        installed = Path(self.pipeline_dir, "subworkflows", "nf-core", "fastq_align_bowtie2")
+        local = Path(self.pipeline_dir, "subworkflows", "local", "fastq_align_bowtie2")
+        shutil.move(installed, local)
+        subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.pipeline_dir)
+        subworkflow_lint.lint(print_results=False, local=True, all_subworkflows=True)
+        assert len(subworkflow_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
+        assert len(subworkflow_lint.passed) > 0
+        assert len(subworkflow_lint.warned) >= 0
+
+    def test_subworkflows_lint_local_missing_files(self):
+        assert self.subworkflow_install.install("fastq_align_bowtie2")
+        installed = Path(self.pipeline_dir, "subworkflows", "nf-core", "fastq_align_bowtie2")
+        local = Path(self.pipeline_dir, "subworkflows", "local", "fastq_align_bowtie2")
+        shutil.move(installed, local)
+        Path(self.pipeline_dir, "subworkflows", "local", "fastq_align_bowtie2", "meta.yml").unlink()
+        subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.pipeline_dir)
+        subworkflow_lint.lint(print_results=False, local=True, all_subworkflows=True)
+        assert len(subworkflow_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
+        assert len(subworkflow_lint.passed) > 0
+        assert len(subworkflow_lint.warned) >= 0
+        warnings = [x.message for x in subworkflow_lint.warned]
+        assert "Subworkflow `meta.yml` does not exist" in warnings
+
+    def test_subworkflows_lint_local_old_format(self):
+        assert self.subworkflow_install.install("fastq_align_bowtie2")
+        installed = Path(self.pipeline_dir, "subworkflows", "nf-core", "fastq_align_bowtie2", "main.nf")
+        Path(self.pipeline_dir, "subworkflows", "local").mkdir(exist_ok=True)
+        local = Path(self.pipeline_dir, "subworkflows", "local", "fastq_align_bowtie2.nf")
+        shutil.copy(installed, local)
+        self.subworkflow_remove.remove("fastq_align_bowtie2", force=True)
+        subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.pipeline_dir)
+        subworkflow_lint.lint(print_results=False, local=True, all_subworkflows=True)
+        assert len(subworkflow_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
+        assert len(subworkflow_lint.passed) > 0
+        assert len(subworkflow_lint.warned) >= 0
