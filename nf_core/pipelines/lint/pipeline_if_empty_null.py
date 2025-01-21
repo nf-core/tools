e832a97ed8bdab07f35d1ae965beda041bc4f1bf
lmReef <ReefMatson@gmail.com>
2025-01-21 15:31:48 +1300

    comment formatting

--- nf_core/pipelines/lint/pipeline_if_empty_null.py
+++ nf_core/pipelines/lint/pipeline_if_empty_null.py
@@ -14,7 +14,7 @@ def pipeline_if_empty_null(self, root_dir=None):
         1. `ifEmpty( [ ] )` to ensure a process executes, for example when an input file is optional (although this can be replaced by `toList()`).
         2. When a channel should not be empty and throws an error `ifEmpty { error ... }`, e.g. reading from an empty samplesheet.
 
-    There are multiple examples of workflows that inject null objects into channels using ifEmpty(null), which can cause unhandled null pointer exceptions.
+    There are multiple examples of workflows that inject null objects into channels using `ifEmpty(null)`, which can cause unhandled null pointer exceptions.
     This lint test throws warnings for those instances.
     """
     passed = []
--- nf_core/subworkflows/lint/subworkflow_if_empty_null.py
+++ nf_core/subworkflows/lint/subworkflow_if_empty_null.py
@@ -12,7 +12,7 @@ def subworkflow_if_empty_null(_, subworkflow):
         1. `ifEmpty( [ ] )` to ensure a process executes, for example when an input file is optional (although this can be replaced by `toList()`).
         2. When a channel should not be empty and throws an error `ifEmpty { error ... }`, e.g. reading from an empty samplesheet.
 
-    There are multiple examples of workflows that inject null objects into channels using ifEmpty(null), which can cause unhandled null pointer exceptions.
+    There are multiple examples of workflows that inject null objects into channels using `ifEmpty(null)`, which can cause unhandled null pointer exceptions.
     This lint test throws warnings for those instances.
     """
 
