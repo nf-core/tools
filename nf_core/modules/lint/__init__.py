8a6d45fd6985caec2cc2a1f0167e39170f2e3a61
mirpedrol <mirp.julia@gmail.com>
2025-02-10 16:10:47 +0100

    add edam comments to hardocded template example and when linting

--- nf_core/modules/lint/__init__.py
+++ nf_core/modules/lint/__init__.py
@@ -398,10 +398,13 @@ class ModuleLint(ComponentLint):
                     log.debug(f"current ontologies for input: {current_ontologies_i}")
                     for ontology, ext in expected_ontologies_i:
                         if ontology not in current_ontologies_i:
-                            corrected_meta_yml["input"][i][j][element_name]["ontologies"].append({"edam": ontology})
-                            corrected_meta_yml["input"][i][j][element_name]["ontologies"].yaml_add_eol_comment(
-                                f"{edam_formats[ext][1]}", -1
+                            corrected_meta_yml["input"][i][j][element_name]["ontologies"].append(
+                                ruamel.yaml.comments.CommentedMap({"edam": ontology})
                             )
+                            corrected_meta_yml["input"][i][j][element_name]["ontologies"][-1].yaml_add_eol_comment(
+                                f"{edam_formats[ext][1]}", "edam"
+                            )
+                            print(f"added comment {edam_formats[ext][1]}")
         if "output" in meta_yml:
             for i, channel in enumerate(corrected_meta_yml["output"]):
                 ch_name = list(channel.keys())[0]
