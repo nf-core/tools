33650677847f49eb4654c9d963ab5844e36337d5
Louis Le Nezet <louislenezet@gmail.com>
2025-03-06 16:30:52 +0100

    Fix

--- nf_core/pipelines/lint/__init__.py
+++ nf_core/pipelines/lint/__init__.py
@@ -599,10 +599,7 @@ def run_linting(
     lint_obj._load_lint_config()
     lint_obj.load_pipeline_config()
 
-    if (
-        lint_obj.lint_config
-        and lint_obj.lint_config["nfcore_components"] is not None
-    ):
+    if lint_obj.lint_config and lint_obj.lint_config["nfcore_components"] is not None:
         module_lint_obj = None
         subworkflow_lint_obj = None
     else:
