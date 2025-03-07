822fe08ce2d76146e108ee3aca649c568ff175ee
Júlia Mir Pedrol <mirp.julia@gmail.com>
2025-03-07 15:09:08 +0100

    fix typing for python 3.9

--- nf_core/modules/modules_json.py
+++ nf_core/modules/modules_json.py
@@ -1043,7 +1043,7 @@ class ModulesJson:
         component_type,
         name,
         dependent_components,
-    ) -> dict[str, Tuple[str, str, str]]:
+    ) -> Dict[str, Tuple[str, str, str]]:
         """
         Retrieves all pipeline modules/subworkflows that are reported in the modules.json
         as being installed by the given component
