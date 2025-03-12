660d734bb8613637d2d2052d5362d354db881a92
Júlia Mir Pedrol <mirp.julia@gmail.com>
2025-03-12 13:01:46 +0100

    remove not available branches on pull_request_review

--- nf_core/pipelines/lint/actions_awsfulltest.py
+++ nf_core/pipelines/lint/actions_awsfulltest.py
@@ -42,8 +42,6 @@ def actions_awsfulltest(self) -> Dict[str, List[str]]:
 
         # Check that the action is only turned on for published releases
         try:
-            if wf[True]["pull_request_review"]["branches"] != ["main", "master"]:
-                raise AssertionError()
             if wf[True]["pull_request_review"]["types"] != ["submitted"]:
                 raise AssertionError()
             if "workflow_dispatch" not in wf[True]:
