9ba70bbe41523ce96118c62563fc86e151597c39
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2025-03-17 12:53:00 +0100

    Adds list and search with concurrent http requests

--- nf_core/commands_test_datasets.py
+++ nf_core/commands_test_datasets.py
@@ -1,31 +1,43 @@
 import logging
+import os
 import sys
+import time
 
 import rich
 
-from nf_core.test_datasets.test_datasets_utils import list_files_by_branch
+from nf_core.test_datasets.test_datasets_utils import list_files_by_branch, list_files_by_branch_async
 from nf_core.utils import rich_force_colors
 
 log = logging.getLogger(__name__)
 stdout = rich.console.Console(force_terminal=rich_force_colors())
 
+
 def test_datasets_list_remote(ctx, branch):
 
-    tree = list_files_by_branch(branch)
+    #tree = list_files_by_branch(branch)
+    tree = list_files_by_branch_async(branch)
 
+    out = ""
     for b in tree.keys():
         files = sorted(tree[b])
         for f in files:
-            stdout.print(f"(Branch: {b}) {f}")
+            out += f"(Branch: {b}) {f}" + os.linesep
+
+    stdout.print(out)
 
 
 def test_datasets_search(ctx, query, branch):
     log.debug(f"test-datasets search query: {query}")
-    tree = list_files_by_branch(branch)
+    #tree = list_files_by_branch(branch)
+    # concurrent requests for speed-up
+    tree = list_files_by_branch_async(branch)
     log.debug(f"Searching the tree of {len(tree.keys())} remote branches ...")
 
+    out = ""
     for b in tree.keys():
         files = sorted(tree[b])
         for f in files:
             if query in f:
-                stdout.print(f"(Branch: {b}) {f}")
+                out += f"(Branch: {b}) {f}" + os.linesep
+
+    stdout.print(out)
--- nf_core/test_datasets/test_datasets_utils.py
+++ nf_core/test_datasets/test_datasets_utils.py
@@ -1,17 +1,17 @@
+import asyncio
 import json
 import logging
 from dataclasses import dataclass
 
+import aiohttp
 import requests
-
 from utils import gh_api
-
 
 log = logging.getLogger(__name__)
 
 @dataclass
 class GithubApiEndpoints():
-    gh_api_base_url = "https://api.github.com/"
+    gh_api_base_url = "https://api.github.com"
     gh_orga: str = "nf-core"
     gh_repo: str = "test-datasets"
 
@@ -23,6 +23,28 @@ class GithubApiEndpoints():
     def get_remote_tree_url_for_branch(self, branch, recursive=1):
         url = f"{self.gh_api_base_url}/repos/{self.gh_orga}/{self.gh_repo}/git/trees/{branch}?recursive=1"
         return url
+
+class GithubApiSessionAsync(aiohttp.ClientSession):
+    def __init__(self, gh_api, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gh_api = gh_api
+        self.setup_github_auth(self)
+
+    def setup_github_auth(self, auth=None):
+
+        if self.gh_api.auth is None:
+            self.gh_api.setup_github_auth()
+
+        if isinstance(self.gh_api.auth, requests.auth.HTTPBasicAuth):
+            self.auth = aiohttp.BasicAuth(self.gh_api.auth.login, self.gh_api.auth.password)
+        elif isinstance(self.gh_api.auth, requests.auth.AuthBase):
+            if self.headers is None:
+                self.headers = dict()
+
+            self.headers["authorization"] = f"Bearer {self.gh_api.auth.token}"
+
+        else:
+            log.debug("Auth could not be transfered to GithubApiSessionAsync")
 
 
 def get_remote_branches():
@@ -119,5 +141,73 @@ def list_files_by_branch(branch, ignored_file_prefixes=[".", "CITATION", "LICENS
     tree = dict()
     for b in branches:
         tree[b] = get_remote_tree_for_branch(b, only_files=True, ignored_prefixes=ignored_file_prefixes)
+
+    return tree
+
+
+async def get_remote_tree_for_branch_async(session, branch, only_files=True, ignored_prefixes=[]):
+    """
+    For a given branch name, return the file tree by querying the github API
+    at the endpoint at `/repos/nf-core/test-datasets/git/trees/`
+    """
+
+    gh_filetree_file_value = "blob"    # value in nodes used to refer to "files"
+    gh_response_filetree_key = "tree"  # key in response to refer to the filetree
+    gh_filetree_type_key = "type"      # key in filetree nodes used to refer to their type
+    gh_filetree_name_key = "path"      # key in filetree nodes used to refer to their name
+
+    gh_api_url = GithubApiEndpoints(gh_repo="test-datasets")
+    async with session.get(gh_api_url.get_remote_tree_url_for_branch(branch)) as response:
+        response_json = await response.json()
+
+        if not response.ok:
+            log.error(f"Error status code {response.status} received while fetching the repository filetree at url {response.url}")
+            return []
+
+        repo_tree = response_json[gh_response_filetree_key]
+
+    if only_files:
+        repo_tree = [node for node in repo_tree if node[gh_filetree_type_key] == gh_filetree_file_value]
+
+    # filter by ignored_prefixes and extract names
+    repo_files = []
+    for node in repo_tree:
+        for prefix in ignored_prefixes:
+            if node[gh_filetree_name_key].startswith(prefix):
+                break
+        else:
+            repo_files.append(node[gh_filetree_name_key])
+
+    return repo_files
+
+
+async def list_files_payload_wrapper(async_loop, branches, ignored_file_prefixes):
+
+    async with GithubApiSessionAsync(gh_api, loop=async_loop) as session:
+        to_execute = [get_remote_tree_for_branch_async(session, b, ignored_file_prefixes) for b in branches]
+        tree_list = await asyncio.gather(*to_execute)
+        tree = dict(zip(branches, tree_list))
+
+    return tree
+
+
+def list_files_by_branch_async(branch, ignored_file_prefixes=[".", "CITATION", "LICENSE", "README", "docs", ]):
+    """
+    Lists files for all branches in the test-datasets github repo concurrently.
+    Returns dictionary with branchnames as keys and file-lists as values
+    """
+
+    # Fetch list of branches frorm GitHub API
+    log.debug("Fetching list of remote branches")
+    branches = get_remote_branches()
+
+    if branch:
+        branches = list(filter(lambda b: b == branch, branches))
+        if len(branches) == 0:
+            log.error(f"No branches matching '{branch}'")
+
+    log.debug("Fetching remote trees")
+    event_loop = asyncio.get_event_loop()
+    tree = event_loop.run_until_complete(list_files_payload_wrapper(event_loop, branches, ignored_file_prefixes))
 
     return tree
