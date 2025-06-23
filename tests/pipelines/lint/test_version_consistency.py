a137f9b08f61cd7331f0bf20f31818e28543105c
yuxinNing <ningyuxin1999@126.com>
2025-06-23 19:43:26 +0200

    pipeline version check

--- tests/pipelines/lint/test_version_consistency.py
+++ tests/pipelines/lint/test_version_consistency.py
@@ -17,3 +17,20 @@ class TestLintVersionConsistency(TestLint):
             "Version tags are numeric and consistent between container, release tag and config."
         ]
         assert result["failed"] == ["manifest.version was not numeric: 1.0.0dev!"]
+
+    def test_version_consistency_fail(self):
+        """Tests that config variable existence test fails with bad pipeline name"""
+        new_pipeline = self._make_pipeline_copy()
+        lint_obj = nf_core.pipelines.lint.PipelineLint(new_pipeline)
+        lint_obj.load_pipeline_config()
+        lint_obj.nextflow_config()
+        # Set a bad version number
+        lint_obj.nf_config["manifest.version"] = "1.0.0dev"
+        lint_obj.nf_config["process.container"] = "nfcore/pipeline:latest"
+        result = lint_obj.version_consistency()
+        assert len(result["passed"]) == 0
+        assert result["failed"] == [
+            "manifest.version was not numeric: 1.0.0dev!",
+            "process.container was not numeric: latest!",
+            "The versioning is not consistent between container, release tag and config. Found manifest.version = 1.0.0dev, process.container = latest",
+        ]
