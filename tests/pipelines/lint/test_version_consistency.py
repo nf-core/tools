7ba605ba8dc17a653716e3380981a727ce5b5541
yuxinNing <ningyuxin1999@126.com>
2025-06-30 17:03:16 +0200

    update tests

--- tests/pipelines/lint/test_version_consistency.py
+++ tests/pipelines/lint/test_version_consistency.py
@@ -6,13 +6,16 @@ from ..test_lint import TestLint
 
 class TestLintVersionConsistency(TestLint):
     def test_version_consistency_pass(self):
-        """Tests that config variable existence test fails with bad pipeline name"""
+        """Tests that pipeline version consistency test passes with good pipeline name"""
         new_pipeline = self._make_pipeline_copy()
         lint_obj = nf_core.pipelines.lint.PipelineLint(new_pipeline)
         lint_obj.load_pipeline_config()
         lint_obj.nextflow_config()
 
+        # Set the version numbers to be consistent
         lint_obj.nf_config["manifest.version"] = "1.0.0"
+        lint_obj.nf_config["config_yml.template.version"] = "1.0.0"
+
         result = lint_obj.version_consistency()
         assert result["passed"] == [
             "Version tags are consistent between container, release tag, config and nfcore yaml."
@@ -20,7 +23,7 @@ class TestLintVersionConsistency(TestLint):
         assert result["failed"] == []
 
     def test_version_consistency_not_numeric(self):
-        """Tests that config variable existence test fails with bad pipeline name"""
+        """Tests that pipeline version consistency test fails with non-numeric version numbers"""
         new_pipeline = self._make_pipeline_copy()
         lint_obj = nf_core.pipelines.lint.PipelineLint(new_pipeline)
         lint_obj.load_pipeline_config()
@@ -32,19 +35,41 @@ class TestLintVersionConsistency(TestLint):
         ]
         assert result["failed"] == ["manifest.version was not numeric: 1.0.0dev!"]
 
-    def test_version_consistency_not_consistent(self):
-        """Tests that config variable existence test fails with bad pipeline name"""
+    def test_version_consistency_container_not_consistent(self):
+        """Tests that pipeline version consistency test fails with inconsistent version numbers"""
         new_pipeline = self._make_pipeline_copy()
         lint_obj = nf_core.pipelines.lint.PipelineLint(new_pipeline)
         lint_obj.load_pipeline_config()
         lint_obj.nextflow_config()
 
-        # Set a bad version number
+        # Set a bad version number for the container
         lint_obj.nf_config["process.container"] = "nfcore/pipeline:latest"
+        # Set the version numbers to be consistent
+        lint_obj.nf_config["manifest.version"] = "1.0.0"
+        lint_obj.nf_config["config_yml.template.version"] = "1.0.0"
+
         result = lint_obj.version_consistency()
         assert len(result["passed"]) == 0
         assert result["failed"] == [
-            "manifest.version was not numeric: 1.0.0dev!",
             "process.container was not numeric: latest!",
-            "The versioning is not consistent between container, release tag and config. Found manifest.version = 1.0.0dev, process.container = latest",
+            "The versioning is not consistent between container, release tag and config. Found manifest.version = 1.0.0, process.container = latest, nfcore_yml.version = 1.0.0",
+        ]
+
+    def test_version_consistency_yml_not_consistent(self):
+        """Tests that pipeline version consistency test fails with inconsistent version numbers"""
+        new_pipeline = self._make_pipeline_copy()
+        lint_obj = nf_core.pipelines.lint.PipelineLint(new_pipeline)
+        lint_obj.load_pipeline_config()
+        lint_obj.nextflow_config()
+
+        # Set a bad version number for the YAML file
+        lint_obj.nf_config["config_yml.template.version"] = "0.0.0"
+        # Set the version numbers to be consistent
+        lint_obj.nf_config["process.container"] = "nfcore/pipeline:1.0.0"
+        lint_obj.nf_config["manifest.version"] = "1.0.0"
+
+        result = lint_obj.version_consistency()
+        assert len(result["passed"]) == 0
+        assert result["failed"] == [
+            "The versioning is not consistent between container, release tag and config. Found manifest.version = 1.0.0, process.container = 1.0.0, nfcore_yml.version = 0.0.0",
         ]
