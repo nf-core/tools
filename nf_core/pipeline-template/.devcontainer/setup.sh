491ec957b41cb54bca3b3b2b677679105e2f3036
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2025-07-18 15:03:20 +0200

    Change to using prebuild image. Remove local feature installs. Simplify setup.sh

--- nf_core/pipeline-template/.devcontainer/setup.sh
+++ nf_core/pipeline-template/.devcontainer/setup.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-# Customise the terminal command prompt
-echo "export PROMPT_DIRTRIM=2" >> $HOME/.bashrc
-echo "export PS1='\[\e[3;36m\]\w ->\[\e[0m\\] '" >> $HOME/.bashrc
-export PS1='\[\e[3;36m\]\w ->\[\e[0m\\] '
-
-# Update Nextflow
-nextflow self-update
-
-# Install nf-core tools
-python -m pip install nf-core
-
-# Install pre-commit hooks
-pre-commit install --install-hooks
-
 # Update welcome message
 echo "Welcome to the nf-core pipeline devcontainer!" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
