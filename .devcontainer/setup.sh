11a79b35d28023e332f3284b6368d939e684c478
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2025-07-21 14:18:39 +0200

    Don't run any commands for testing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Update welcome message
-echo "Welcome to the nf-core/tools devcontainer!" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+#echo "Welcome to the nf-core/tools devcontainer!" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
