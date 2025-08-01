303d6bc65c28e67c66c469af69ddf30d9ff3b4c1
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2025-08-01 13:45:49 +0000

    Add jinja variable for pipelien name to greeting

--- nf_core/pipeline-template/.devcontainer/setup.sh
+++ nf_core/pipeline-template/.devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Update welcome message
-echo "Welcome to the nf-core pipeline devcontainer!" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+echo "Welcome to the {{ name }} devcontainer!" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
