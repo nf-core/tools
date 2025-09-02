6b713933fefb19a900b1bca92e66a099b312682d
yuxinNing <ningyuxin1999@126.com>
2025-09-02 10:56:39 +0200

    update

--- nf_core/pipelines/lint/rocrate_readme_sync.py
+++ nf_core/pipelines/lint/rocrate_readme_sync.py
@@ -13,7 +13,6 @@ def rocrate_readme_sync(self):
 
     passed = []
     ignored = []
-    failed = []
     fixed = []
 
     # Check if the file exists before trying to load it
@@ -26,7 +25,7 @@ def rocrate_readme_sync(self):
             ignored.append("`ro-crate-metadata.json` not found")
         if not readme_file.exists():
             ignored.append("`README.md` not found")
-        return {"passed": passed, "failed": failed, "ignored": ignored}
+        return {"passed": passed, "fixed": fixed, "ignored": ignored}
 
     try:
         metadata_content = metadata_file.read_text(encoding="utf-8")
@@ -34,40 +33,27 @@ def rocrate_readme_sync(self):
     except json.JSONDecodeError as e:
         log.error("Failed to decode JSON from `ro-crate-metadata.json`: %s", e)
         ignored.append("Invalid JSON in `ro-crate-metadata.json`")
-        return {"passed": passed, "failed": failed, "ignored": ignored}
+        return {"passed": passed, "fixed": fixed, "ignored": ignored}
     readme_content = readme_file.read_text(encoding="utf-8")
     graph = metadata_dict.get("@graph")
-
-    # Ensure the fix rocrate_readme_sync is in the fix list
-    if "rocrate_readme_sync" not in self.fix:
-        self.fix += ("rocrate_readme_sync",)
 
     if not graph or not isinstance(graph, list) or not graph[0] or not isinstance(graph[0], dict):
         ignored.append("Invalid RO-Crate metadata structure.")
     else:
         # Check if the 'description' key is present
         if "description" not in graph[0]:
-            if "rocrate_readme_sync" in self.fix:
-                metadata_dict.get("@graph")[0]["description"] = readme_content
-                fixed.append("Fixed: add the same description from `README.md` to the RO-Crate metadata.")
-            else:
-                ignored.append("No description found in `ro-crate-metadata.json`.")
-                return {"passed": passed, "failed": failed, "ignored": ignored}
+            metadata_dict.get("@graph")[0]["description"] = readme_content
+            fixed.append("Fixed: add the same description from `README.md` to the RO-Crate metadata.")
 
     rc_description_graph = metadata_dict.get("@graph", [{}])[0].get("description")
 
     # Compare the two strings and add a linting error if they don't match
     if readme_content != rc_description_graph:
-        if "rocrate_readme_sync" in self.fix:
-            metadata_dict.get("@graph")[0]["description"] = readme_content
-            with metadata_file.open("w", encoding="utf-8") as f:
-                json.dump(metadata_dict, f, indent=4)
-            passed.append("RO-Crate description matches the `README.md`.")
-            fixed.append("Mismatch fixed: RO-Crate description updated from `README.md`.")
-        else:
-            failed.append(
-                "The RO-Crate descriptions do not match the README.md content. Use `nf-core pipelines lint --fix rocrate_readme_sync` to update."
-            )
+        metadata_dict.get("@graph")[0]["description"] = readme_content
+        with metadata_file.open("w", encoding="utf-8") as f:
+            json.dump(metadata_dict, f, indent=4)
+        passed.append("RO-Crate description matches the `README.md`.")
+        fixed.append("Mismatch fixed: RO-Crate description updated from `README.md`.")
     else:
         passed.append("RO-Crate descriptions are in sync with `README.md`.")
-    return {"passed": passed, "failed": failed, "ignored": ignored, "fixed": fixed}
+    return {"passed": passed, "fixed": fixed, "ignored": ignored}
