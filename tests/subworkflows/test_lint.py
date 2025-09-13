f6fa9d5244464750050c5c78bb1a1cb97c024cc0
Matthieu Muffato <mm49@sanger.ac.uk>
2025-09-13 21:42:57 +0100

    Added linting tests for patched subworkflows

--- tests/subworkflows/test_lint.py
+++ tests/subworkflows/test_lint.py
@@ -1,5 +1,6 @@
 import json
 import shutil
+import subprocess
 from pathlib import Path
 
 import nf_core.subworkflows
@@ -434,3 +435,60 @@ class TestSubworkflowsLint(TestSubworkflows):
         assert len(subworkflow_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
         assert len(subworkflow_lint.passed) > 0
         assert len(subworkflow_lint.warned) >= 0
+
+
+class TestSubworkflowsLintPatch(TestSubworkflows):
+    def setup_patch(self, pipeline_dir):
+        # Install the subworkflow bam_sort_stats_samtools
+        self.subworkflow_install.install("bam_sort_stats_samtools")
+
+        # Modify the subworkflow by inserting a new input channel
+        new_line = "    ch_dummy // channel: [ path ]\n"
+
+        subworkflow_path = Path(pipeline_dir, "subworkflows", "nf-core", "bam_sort_stats_samtools", "main.nf")
+
+        with open(subworkflow_path) as fh:
+            lines = fh.readlines()
+        for line_index in range(len(lines)):
+            if "take:" in lines[line_index]:
+                lines.insert(line_index + 1, new_line)
+        with open(subworkflow_path, "w") as fh:
+            fh.writelines(lines)
+
+    def test_lint_clean_patch(self):
+        """Test linting a patched subworkflow"""
+        self.setup_patch(self.pipeline_dir)
+
+        # Create a patch file
+        patch_obj = nf_core.subworkflows.SubworkflowPatch(self.pipeline_dir)
+        patch_obj.patch("bam_sort_stats_samtools")
+
+        subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.pipeline_dir)
+        subworkflow_lint.lint(print_results=False, subworkflow="bam_sort_stats_samtools")
+
+        assert len(subworkflow_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
+        assert len(subworkflow_lint.passed) > 0
+        assert len(subworkflow_lint.warned) == 0, f"Linting warned with {[x.__dict__ for x in subworkflow_lint.warned]}"
+
+    def test_lint_broken_patch(self):
+        """Test linting a patched subworkflow when the patch is broken"""
+        self.setup_patch(self.pipeline_dir)
+
+        # Create a patch file
+        patch_obj = nf_core.subworkflows.SubworkflowPatch(self.pipeline_dir)
+        patch_obj.patch("bam_sort_stats_samtools")
+
+        # Now modify the diff
+        diff_file = Path(
+            self.pipeline_dir, "subworkflows", "nf-core", "bam_sort_stats_samtools", "bam_sort_stats_samtools.diff"
+        )
+        subprocess.check_call(["sed", "-i''", "4,6d", str(diff_file)])
+
+        subworkflow_lint = nf_core.subworkflows.SubworkflowLint(directory=self.pipeline_dir)
+        subworkflow_lint.lint(print_results=False, subworkflow="bam_sort_stats_samtools")
+
+        assert len(subworkflow_lint.failed) == 1, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
+        errors = [x.message for x in subworkflow_lint.failed]
+        assert "Subworkflow patch cannot be cleanly applied" in errors
+        assert len(subworkflow_lint.passed) > 0
+        assert len(subworkflow_lint.warned) == 0, f"Linting warned with {[x.__dict__ for x in subworkflow_lint.warned]}"
