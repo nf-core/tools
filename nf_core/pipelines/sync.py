2a7923f70e3f063760cbc05c6c1971199fa06199
René Rahn <1529364+rrahn@users.noreply.github.com>
2025-11-03 15:56:40 +0100

    Force git add to exclude certain files
    When some files are already globally ignored via .gitignore, git add
    --all will not add them even if we want to force add all files except
    some specific ones. Not using force will cause git to error out since
    the files with the exclude pattern are already ignored.

--- nf_core/pipelines/sync.py
+++ nf_core/pipelines/sync.py
@@ -345,7 +345,7 @@ class PipelineSync:
         try:
             # add and commit all files except self.ignored_files
             # :! syntax to exclude files using git pathspec
-            self.repo.git.add([f":!{f}" for f in self.ignored_files], all=True)
+            self.repo.git.add([f":!{f}" for f in self.ignored_files], all=True, force=True)
             self.repo.index.commit(f"Template update for nf-core/tools version {nf_core.__version__}")
             self.made_changes = True
             log.info("Committed changes to 'TEMPLATE' branch")
