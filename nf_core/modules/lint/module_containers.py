b754b77846d713230b73167b1a91416ebb281b93
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-03-11 10:24:45 +0100

    Add module_containers dummy

---
+++ nf_core/modules/lint/module_containers.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def lint_meta_yml_containers(meta_yml: dict):
+    pass
+
+
+def lint_main_nf_container(main_nf: Path):
+    pass
+
+
+def lint_conda_lock_files(meta_yml: dict):
+    pass
