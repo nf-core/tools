a06323f774978118203d894edf4e79a91068d297
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-03-11 15:57:22 +0100

    Fix command creation and path for nextflow inspect

--- nf_core/utils.py
+++ nf_core/utils.py
@@ -399,12 +399,13 @@ def nextflow_inspect(main_nf: Path, format: str = "json", profile: str = "docker
     if format and format.lower() not in valid_formats:
         raise ValueError(f"Invalid format: {format} Must be one of ({','.join(valid_formats)})")
 
+    main_nf = Path(main_nf).absolute()
     with set_wd_tempdir():
         executable = "nextflow"
-        cmd_params = "inspect"
-        cmd_params += f"-format {format}" if format else ""
-        cmd_params += f"-profile {profile}" if profile else ""
-        cmd_params += str(main_nf.absolute())
+        cmd_params = " inspect "
+        cmd_params += f" -format {format} " if format else ""
+        cmd_params += f" -profile {profile} " if profile else ""
+        cmd_params += str(main_nf)
 
         log.debug("Running nextflow inspect to extract docker container")
         cmd_out = run_cmd(executable, cmd_params)
