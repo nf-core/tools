87f4d30f08cbf8843932de7fce8b721b0c6af821
mashehu <mashehu3@gmail.com>
2026-03-19 14:21:15 +0100

    switch to gitlab module to test with old version syntax

--- tests/modules/lint/test_main_nf.py
+++ tests/modules/lint/test_main_nf.py
@@ -137,23 +137,23 @@ class TestMainNfLinting(TestModules):
     def test_topics_and_emits_version_check(self):
         """Test that main_nf version emit and topics check works correctly"""
 
-        self.mods_install.install("bioawk")
-        # Lint a module known to have versions YAML in main.nf (for now)
+        self.mods_install_gitlab_nftest.install("fastqc")
+        # Lint a module installed from the gitlab test branch; gitlab test modules
         module_lint = nf_core.modules.lint.ModuleLint(directory=self.pipeline_dir)
-        module_lint.lint(print_results=False, module="bioawk")
-        assert len(module_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in module_lint.failed]}"
-        assert len(module_lint.warned) == 2, (
-            f"Linting warned with {[x.__dict__ for x in module_lint.warned]}, expected 2 warnings"
-        )
+        module_lint.lint(print_results=False, module="fastqc")
+        assert len(module_lint.failed) == 0
         assert len(module_lint.passed) > 0
+
+        assert any(w.lint_test in ("main_nf_version_emit", "main_nf_version_topic") for w in module_lint.warned), (
+            f"Expected warning about missing version topic, got {[w.message for w in module_lint.warned]}"
+        )
 
         # Lint a module known to have topics as output in main.nf
         module_lint = nf_core.modules.lint.ModuleLint(directory=self.pipeline_dir)
         module_lint.lint(print_results=False, module="bamstats/generalstats")
         assert len(module_lint.failed) == 0, f"Linting failed with {[x.__dict__ for x in module_lint.failed]}"
-        assert len(module_lint.warned) == 0, (
-            f"Linting warned with {[x.__dict__ for x in module_lint.warned]}, expected 1 warning"
-        )
+        assert len(module_lint.warned) == 0, f"Expected 0 warnings, got {[x.__dict__ for x in module_lint.warned]}"
+
         assert len(module_lint.passed) > 0
 
 
