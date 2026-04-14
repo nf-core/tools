aae824221e06dc936e1e3999eda235f22ea67774
Matthias Hörtenhuber <mashehu@users.noreply.github.com>
2026-04-14 12:02:25 +0000

    update assertions

--- tests/subworkflows/lint/test_main_nf.py
+++ tests/subworkflows/lint/test_main_nf.py
@@ -64,7 +64,7 @@ class TestMainNf(TestSubworkflows):
         subworkflow_lint.lint(print_results=False, subworkflow="bam_stats_samtools")
         assert len(subworkflow_lint.failed) >= 0, f"Linting failed with {[x.__dict__ for x in subworkflow_lint.failed]}"
         assert len(subworkflow_lint.passed) > 0
-        assert len(subworkflow_lint.warned) == 5
+        assert len(subworkflow_lint.warned) == 1
         assert any(
             [x.message == "Included component 'SAMTOOLS_STATS_1' used in main.nf" for x in subworkflow_lint.passed]
         )
