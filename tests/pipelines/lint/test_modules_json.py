35b1d5435d8f157b609f3f22150024f8ad749ed7
beatrizsavinhas <beatriz.sa.vinhas@gmail.com>
2026-05-21 12:01:17 +0200

    Add test_modules_json_with_repo_not_starting_with_http_fail and test_modules_json_with_repo_with_no_modules_or_subworkflows_fail and refactor test_modules_json_with_only_subworkflow_entries.

--- tests/pipelines/lint/test_modules_json.py
+++ tests/pipelines/lint/test_modules_json.py
@@ -31,11 +31,25 @@ class TestLintModulesJson(TestLint):
         assert len(results.get("failed", [])) == 0
         assert len(results.get("passed", [])) > 0
 
-    def test_modules_json_with_only_subworkflow_entries(self):
-        # Create a new_modules_json with only "subworkflows" component for all repos
+    def test_modules_json_with_repo_not_starting_with_http_fail(self):
+        # Change the link of the first repo in modules_json to not start with "http"
         new_modules_json = self.modules_json
-        for repo in new_modules_json["repos"]:
-            new_modules_json["repos"][repo] = {"subworkflows": new_modules_json["repos"][repo]["subworkflows"]}
+        first_repo = next(iter(new_modules_json["repos"]))
+        new_modules_json["repos"]["incorrect_repo_link"] = new_modules_json["repos"].pop(first_repo)
+
+        self._update_modules_json(new_modules_json)
+
+        # Check linting results
+        results = self.lint_obj.modules_json()
+        assert len(results.get("warned", [])) == 0
+        assert len(results.get("failed", [])) == 1
+        assert len(results.get("passed", [])) == 1  # The repository still contains only installed modules
+
+    def test_modules_json_with_only_subworkflow_entries(self):
+        # Keep only "subworkflows" in the first repo in modules_json
+        new_modules_json = self.modules_json
+        first_repo = next(iter(new_modules_json["repos"]))
+        new_modules_json["repos"][first_repo] = {"subworkflows": new_modules_json["repos"][first_repo]["subworkflows"]}
 
         self._update_modules_json(new_modules_json)
 
@@ -43,4 +57,18 @@ class TestLintModulesJson(TestLint):
         results = self.lint_obj.modules_json()
         assert len(results.get("warned", [])) == 0
         assert len(results.get("failed", [])) == 0
+        assert len(results.get("passed", [])) == 0  # There is no subworkflow linting so no "passed" is returned
+
+    def test_modules_json_with_repo_with_no_modules_or_subworkflows_fail(self):
+        # Change the first repo in modules_json to have no modules or subworkflows
+        new_modules_json = self.modules_json
+        first_repo = next(iter(new_modules_json["repos"]))
+        new_modules_json["repos"][first_repo] = {}
+
+        self._update_modules_json(new_modules_json)
+
+        # Check linting results
+        results = self.lint_obj.modules_json()
+        assert len(results.get("warned", [])) == 0
+        assert len(results.get("failed", [])) == 1
         assert len(results.get("passed", [])) == 0
