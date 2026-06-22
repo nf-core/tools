da3754ae62a58dcc4d9864350963606507b3b787
mashehu <mashehu3@gmail.com>
2026-06-22 16:34:57 +0200

    fix test_meta_yml

--- nf_core/modules/lint/__init__.py
+++ nf_core/modules/lint/__init__.py
@@ -332,7 +332,7 @@ class ModuleLint(ComponentLint):
         """
         Update the meta.yml file with the correct inputs, outputs, topics and containers
         """
-        meta_yml = self.read_meta_yml(mod)
+        meta_yml = self.read_meta_yml_patched(mod)
         if meta_yml is None:
             log.warning(f"Could not read meta.yml for {mod.component_name}, skipping update")
             return
