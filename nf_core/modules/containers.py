01d57408fcb1c9be187314b27f2baa5249c86e0b
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2026-08-19 11:16:15 +0200

    Add basePackages to container metadata

--- nf_core/modules/containers.py
+++ nf_core/modules/containers.py
@@ -703,6 +703,7 @@ class ModuleContainers:
             "containerPlatform": platform,
             "freeze": True,
             "buildTemplate": "conda/pixi:v1",
+            "basePackages": "littler r-cran-docopt",
             "format": WAVE_FORMAT[container_system],
             "nameStrategy": "imageSuffix",
         }
